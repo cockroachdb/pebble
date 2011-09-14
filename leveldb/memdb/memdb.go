61d267b43dc9a3301b52b088bd97776565b40444
Nigel Tao <nigeltao@golang.org>
2011-09-14 22:00:49 +1000

    leveldb/memdb: change node representation from a pointer-to-struct to a slice offset.
    Printing runtime.MemStats.Mallocs at the end of memdb_test.go drops from
    around 22000 to around 20000. I suspect that this 10% ratio underestimates
    the magnitude of improvement because, IIUC, the compiler does not yet
    eliminate many string -> []byte conversions in the test code.
    
    R=bradfitz, dvyukov
    CC=golang-dev
    http://codereview.appspot.com/4983051

--- leveldb/memdb/memdb.go
+++ leveldb/memdb/memdb.go
@@ -20,76 +20,87 @@ import (
 	"snappy-go.googlecode.com/hg/varint"
 )
 
-const (
-	// maxHeight is the maximum height of a MemDB's skiplist.
-	maxHeight = 12
+// maxHeight is the maximum height of a MemDB's skiplist.
+const maxHeight = 12
 
-	// Nodes hold offsets into a MemDB's data slice that stores varint-prefixed
-	// strings: the node's key and value. A negative offset means a zero-length
-	// string, whether explicitly set to empty or implicitly set by deletion.
-	offsetEmptySlice  = -1
-	offsetDeletedNode = -2
+// A MemDB's skiplist consists of a number of nodes, and each node is
+// represented by a variable number of ints: a key-offset, a value-offset, and
+// between 1 and maxHeight next nodes. The key-offset and value-offset encode
+// the node's key/value pair and are offsets into a MemDB's kvData slice.
+// The remaining ints, for the next nodes in the skiplist's linked lists, are
+// offsets into a MemDB's nodeData slice.
+//
+// The fXxx constants represent how to find the Xxx field of a node in the
+// nodeData. For example, given an int 30 representing a node, and given
+// nodeData[30:36] that looked like [60, 71, 82, 83, 84, 85], then
+// nodeData[30 + fKey] = 60 would be the node's key-offset,
+// nodeData[30 + fVal] = 71 would be the node's value-offset, and
+// nodeData[30 + fNxt + 0] = 82 would be the next node at the height-0 list,
+// nodeData[30 + fNxt + 1] = 83 would be the next node at the height-1 list,
+// and so on. A node's height is implied by the skiplist construction: a node
+// of height x appears in the height-h list iff 0 <= h && h < x.
+const (
+	fKey = iota
+	fVal
+	fNxt
 )
 
-// node is a node in a skiplist. It holds a key/value pair (as offsets into
-// a MemDB's data field) and a variable-length list of next nodes.
-//
-// TODO: instead of allocating lots of little node structs (whose next field
-// is mostly empty), a MemDB could have an []int buffer (similar to its "data
-// []byte" buffer) and a node could just be an offset into this buffer.
-// A node would be between 2+1 and 2+maxHeight ints: kOff, vOff and a variable
-// number of next node offsets. Note that a node's height is unmodified after
-// construction (other than the artificial head node), and a node of a given
-// height will never appear in a list of higher height.
-type node struct {
-	// kOff is the data offset of the node's key.
-	kOff int
-	// vOff is the data offset of the node's value. A value of offsetDeletedNode
-	// means that the key has been deleted from the skiplist.
-	vOff int
-	// next[i] is the next node in the linked list at height i.
-	next [maxHeight]*node
-}
+const (
+	// zeroNode represents the end of a linked list.
+	zeroNode = 0
+	// headNode represents the start of the linked list. It is equal to -fNxt
+	// so that the next nodes at height-h are at nodeData[h].
+	// The head node is an artificial node and has no key or value.
+	headNode = -fNxt
+)
+
+// A node's key-offset and value-offset fields are offsets into a MemDB's
+// kvData slice that stores varint-prefixed strings: the node's key and value.
+// A negative offset means a zero-length string, whether explicitly set to
+// empty or implicitly set by deletion.
+const (
+	kvOffsetEmptySlice  = -1
+	kvOffsetDeletedNode = -2
+)
 
 // MemDB is a memory-backed implementation of the db.DB interface.
 //
 // It is safe to call Get, Set, Delete and Find concurrently.
 type MemDB struct {
 	mutex sync.RWMutex
-	// head is an artificial node that holds the start of each layer of
-	// a skiplist.
-	head node
 	// height is the number of such lists, which can increase over time.
 	height int
 	// cmp defines an ordering on keys.
 	cmp db.Comparer
-	// data is an append-only buffer that holds varint-prefixed strings.
-	data []byte
+	// kvData is an append-only buffer that holds varint-prefixed strings.
+	kvData []byte
+	// nodeData is an append-only buffer that holds a node's fields.
+	nodeData []int
 }
 
 // MemDB implements the db.DB interface.
 var _ db.DB = &MemDB{}
 
-// load loads a []byte from m.data.
-func (m *MemDB) load(offset int) (b []byte) {
-	if offset < 0 {
+// load loads a []byte from m.kvData.
+func (m *MemDB) load(kvOffset int) (b []byte) {
+	if kvOffset < 0 {
 		return nil
 	}
-	bLen, n := varint.Decode(m.data[offset:])
-	return m.data[offset+n : offset+n+int(bLen)]
+	bLen, n := varint.Decode(m.kvData[kvOffset:])
+	return m.kvData[kvOffset+n : kvOffset+n+int(bLen)]
 }
 
-// save saves a []byte to m.data.
-func (m *MemDB) save(b []byte) (offset int) {
+// save saves a []byte to m.kvData.
+func (m *MemDB) save(b []byte) (kvOffset int) {
 	if len(b) == 0 {
-		return offsetEmptySlice
+		return kvOffsetEmptySlice
 	}
-	offset = len(m.data)
+	kvOffset = len(m.kvData)
 	var buf [varint.MaxLen]byte
 	length := varint.Encode(buf[:], uint64(len(b)))
-	m.data = append(m.data, buf[:length]...)
-	m.data = append(m.data, b...)
-	return offset
+	m.kvData = append(m.kvData, buf[:length]...)
+	m.kvData = append(m.kvData, b...)
+	return kvOffset
 }
 
 // findNode returns the first node n whose key is >= the given key (or nil if
@@ -97,23 +108,24 @@ func (m *MemDB) save(b []byte) (offset int) {
 // solely on the contents of a node's key. Whether or not that key was
 // previously deleted from the MemDB is not relevant.
 //
-// If prev is non-nil, it also sets the first s.height elements of prev to the
+// If prev is non-nil, it also sets the first m.height elements of prev to the
 // preceding node at each height.
-func (m *MemDB) findNode(key []byte, prev *[maxHeight]*node) (n *node, exactMatch bool) {
-	for h, p := m.height-1, &m.head; h >= 0; h-- {
-		// Walk the skiplist at height h until we find either a nil node
+func (m *MemDB) findNode(key []byte, prev *[maxHeight]int) (n int, exactMatch bool) {
+	for h, p := m.height-1, headNode; h >= 0; h-- {
+		// Walk the skiplist at height h until we find either a zero node
 		// or one whose key is >= the given key.
-		n = p.next[h]
+		n = m.nodeData[p+fNxt+h]
 		for {
-			if n == nil {
+			if n == zeroNode {
 				exactMatch = false
 				break
 			}
-			if c := m.cmp.Compare(m.load(n.kOff), key); c >= 0 {
+			kOff := m.nodeData[n+fKey]
+			if c := m.cmp.Compare(m.load(kOff), key); c >= 0 {
 				exactMatch = c == 0
 				break
 			}
-			p, n = n, n.next[h]
+			p, n = n, m.nodeData[n+fNxt+h]
 		}
 		if prev != nil {
 			(*prev)[h] = p
@@ -127,10 +139,11 @@ func (m *MemDB) Get(key []byte) (value []byte, err os.Error) {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 	n, exactMatch := m.findNode(key, nil)
-	if !exactMatch || n.vOff == offsetDeletedNode {
+	vOff := m.nodeData[n+fVal]
+	if !exactMatch || vOff == kvOffsetDeletedNode {
 		return nil, db.ErrNotFound
 	}
-	return m.load(n.vOff), nil
+	return m.load(vOff), nil
 }
 
 // Set implements DB.Set, as documented in the leveldb/db package.
@@ -138,10 +151,10 @@ func (m *MemDB) Set(key, value []byte) os.Error {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	// Find the node, and its predecessors at all heights.
-	var prev [maxHeight]*node
+	var prev [maxHeight]int
 	n, exactMatch := m.findNode(key, &prev)
 	if exactMatch {
-		n.vOff = m.save(value)
+		m.nodeData[n+fVal] = m.save(value)
 		return nil
 	}
 	// Choose the new node's height, branching with 25% probability.
@@ -152,19 +165,21 @@ func (m *MemDB) Set(key, value []byte) os.Error {
 	// Raise the skiplist's height to the node's height, if necessary.
 	if m.height < h {
 		for i := m.height; i < h; i++ {
-			prev[i] = &m.head
+			prev[i] = headNode
 		}
 		m.height = h
 	}
 	// Insert the new node.
-	n1 := &node{
-		kOff: m.save(key),
-		vOff: m.save(value),
-	}
+	var x [fNxt + maxHeight]int
+	n1 := len(m.nodeData)
+	x[fKey] = m.save(key)
+	x[fVal] = m.save(value)
 	for i := 0; i < h; i++ {
-		n1.next[i] = prev[i].next[i]
-		prev[i].next[i] = n1
+		j := prev[i] + fNxt + i
+		x[fNxt+i] = m.nodeData[j]
+		m.nodeData[j] = n1
 	}
+	m.nodeData = append(m.nodeData, x[:fNxt+h]...)
 	return nil
 }
 
@@ -173,10 +188,10 @@ func (m *MemDB) Delete(key []byte) os.Error {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	n, exactMatch := m.findNode(key, nil)
-	if !exactMatch || n.vOff == offsetDeletedNode {
+	if !exactMatch || m.nodeData[n+fVal] == kvOffsetDeletedNode {
 		return db.ErrNotFound
 	}
-	n.vOff = offsetDeletedNode
+	m.nodeData[n+fVal] = kvOffsetDeletedNode
 	return nil
 }
 
@@ -185,12 +200,12 @@ func (m *MemDB) Find(key []byte) db.Iterator {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 	n, _ := m.findNode(key, nil)
-	for n != nil && n.vOff == offsetDeletedNode {
-		n = n.next[0]
+	for n != zeroNode && m.nodeData[n+fVal] == kvOffsetDeletedNode {
+		n = m.nodeData[n+fNxt]
 	}
 	t := &iterator{
-		m:       m,
-		restart: n,
+		m:           m,
+		restartNode: n,
 	}
 	t.fill()
 	// The iterator is positioned at the first node >= key. The iterator API
@@ -208,30 +223,29 @@ func (m *MemDB) Close() os.Error {
 func (m *MemDB) ApproximateMemoryUsage() int {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
-	return len(m.data)
+	return len(m.kvData)
 }
 
 // New returns a new MemDB.
 func New(o *db.Options) *MemDB {
 	return &MemDB{
-		head: node{
-			kOff: offsetEmptySlice,
-			vOff: offsetEmptySlice,
-		},
 		height: 1,
 		cmp:    o.GetComparer(),
-		data:   make([]byte, 0, 4096),
+		kvData: make([]byte, 0, 4096),
+		// The first maxHeight values of nodeData are the next nodes after the
+		// head node at each possible height. Their initial value is zeroNode.
+		nodeData: make([]int, maxHeight, 256),
 	}
 }
 
 // iterator is a MemDB iterator that buffers upcoming results, so that it does
 // not have to acquire the MemDB's mutex on each Next call.
 type iterator struct {
-	m       *MemDB
-	// restart is the node to start refilling the buffer from.
-	restart *node
+	m *MemDB
+	// restartNode is the node to start refilling the buffer from.
+	restartNode int
 	// i0 is the current iterator position with respect to buf. A value of -1
-	// means that the iterator is either at the start or end of the iteration.
+	// means that the iterator is at the start, end or both of the iteration.
 	// i1 is the number of buffered entries.
 	// Invariant: -1 <= i0 && i0 < i1 && i1 <= len(buf).
 	i0, i1 int
@@ -244,24 +258,24 @@ var _ db.Iterator = &iterator{}
 
 // fill fills the iterator's buffer with key/value pairs from the MemDB.
 func (t *iterator) fill() {
-	i, n := 0, t.restart
-	for i < len(t.buf) && n != nil {
-		if n.vOff != offsetDeletedNode {
-			t.buf[i][0] = t.m.load(n.kOff)
-			t.buf[i][1] = t.m.load(n.vOff)
+	i, n := 0, t.restartNode
+	for i < len(t.buf) && n != zeroNode {
+		if t.m.nodeData[n+fVal] != kvOffsetDeletedNode {
+			t.buf[i][fKey] = t.m.load(t.m.nodeData[n+fKey])
+			t.buf[i][fVal] = t.m.load(t.m.nodeData[n+fVal])
 			i++
 		}
-		n = n.next[0]
+		n = t.m.nodeData[n+fNxt]
 	}
 	if i == 0 {
-		// There were no non-deleted nodes on or after t.restart.
+		// There were no non-deleted nodes on or after t.restartNode.
 		// The iterator is exhausted.
 		t.i0 = -1
 	} else {
 		t.i0 = 0
 	}
 	t.i1 = i
-	t.restart = n
+	t.restartNode = n
 }
 
 // Next implements Iterator.Next, as documented in the leveldb/db package.
@@ -270,7 +284,7 @@ func (t *iterator) Next() bool {
 	if t.i0 < t.i1 {
 		return true
 	}
-	if t.restart == nil {
+	if t.restartNode == zeroNode {
 		t.i0 = -1
 		t.i1 = 0
 		return false
@@ -286,7 +300,7 @@ func (t *iterator) Key() []byte {
 	if t.i0 < 0 {
 		return nil
 	}
-	return t.buf[t.i0][0]
+	return t.buf[t.i0][fKey]
 }
 
 // Value implements Iterator.Value, as documented in the leveldb/db package.
@@ -294,7 +308,7 @@ func (t *iterator) Value() []byte {
 	if t.i0 < 0 {
 		return nil
 	}
-	return t.buf[t.i0][1]
+	return t.buf[t.i0][fVal]
 }
 
 // Close implements Iterator.Close, as documented in the leveldb/db package.
