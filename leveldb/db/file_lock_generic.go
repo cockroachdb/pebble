594f03d500957cce7bccb1cda533873f7a4df318
ChaiShushan <chaishushan@gmail.com>
2013-12-03 12:59:00 +1100

    leveldb-go: implement file lock on windows.
    R=nigeltao, alex.brainman
    CC=golang-dev
    https://codereview.appspot.com/35380043
    
    Committer: Nigel Tao <nigeltao@golang.org>

--- leveldb/db/file_lock_generic.go
+++ leveldb/db/file_lock_generic.go
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !linux,!darwin !amd64
+// +build !linux,!amd64
+// +build !darwin,!amd64
+// +build !windows
 
 package db
 
