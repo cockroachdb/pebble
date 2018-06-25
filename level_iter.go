d8674bd49cec47d239177cae3ebd89414fd0c080
Peter Mattis <petermattis@gmail.com>
2018-06-24 21:36:58 -0400

    Flesh out levelIter

--- level_iter.go
+++ level_iter.go
@@ -1,18 +1,40 @@
 package pebble
 
-import "github.com/petermattis/pebble/db"
+import (
+	"sort"
+
+	"github.com/petermattis/pebble/db"
+)
 
 type levelIter struct {
-	files      []fileMetadata
-	tableCache *tableCache
-	err        error
+	cmp     db.Compare
+	titer   db.InternalIterator
+	tiMaker tableIterMaker
+	files   []fileMetadata
+	err     error
 }
 
 // levelIter implements the db.InternalIterator interface.
 var _ db.InternalIterator = (*levelIter)(nil)
 
+func (l *levelIter) findFileGE(key *db.InternalKey) *fileMetadata {
+	// Find the earliest file at that level whose largest key is >= ikey.
+	index := sort.Search(len(l.files), func(i int) bool {
+		return db.InternalCompare(l.cmp, l.files[i].largest, *key) >= 0
+	})
+	if index == len(l.files) {
+		return &l.files[len(l.files)-1]
+	}
+	return &l.files[index]
+}
+
 func (l *levelIter) SeekGE(key *db.InternalKey) {
-	panic("pebble: SeekGE unimplemented")
+	f := l.findFileGE(key)
+	l.titer, l.err = l.tiMaker.newIter(f.fileNum)
+	if l.err != nil {
+		return
+	}
+	l.titer.SeekGE(key)
 }
 
 func (l *levelIter) SeekLE(key *db.InternalKey) {
@@ -20,37 +42,83 @@ func (l *levelIter) SeekLE(key *db.InternalKey) {
 }
 
 func (l *levelIter) First() {
-	panic("pebble: First unimplemented")
+	f := &l.files[0]
+	l.titer, l.err = l.tiMaker.newIter(f.fileNum)
+	if l.err != nil {
+		return
+	}
+	l.titer.First()
 }
 
 func (l *levelIter) Last() {
-	panic("pebble: Last unimplemented")
+	f := &l.files[len(l.files)-1]
+	l.titer, l.err = l.tiMaker.newIter(f.fileNum)
+	if l.err != nil {
+		return
+	}
+	l.titer.Last()
 }
 
 func (l *levelIter) Next() bool {
+	if l.err != nil {
+		return false
+	}
+	if l.titer != nil {
+		if l.titer.Next() {
+			return true
+		}
+	}
+
+	// TODO(peter): find the next file.
 	panic("pebble: Next unimplemented")
 }
 
 func (l *levelIter) Prev() bool {
+	if l.err != nil {
+		return false
+	}
+	if l.titer != nil {
+		if l.titer.Prev() {
+			return true
+		}
+	}
+
+	// TODO(peter): find the prev file.
 	panic("pebble: Prev unimplemented")
 }
 
 func (l *levelIter) Key() *db.InternalKey {
-	return nil
+	if l.titer == nil {
+		return nil
+	}
+	return l.titer.Key()
 }
 
 func (l *levelIter) Value() []byte {
-	return nil
+	if l.titer == nil {
+		return nil
+	}
+	return l.titer.Value()
 }
 
 func (l *levelIter) Valid() bool {
-	return false
+	if l.titer == nil {
+		return false
+	}
+	return l.titer.Valid()
 }
 
 func (l *levelIter) Error() error {
-	return l.err
+	if l.err != nil {
+		return l.err
+	}
+	return l.titer.Error()
 }
 
 func (l *levelIter) Close() error {
+	if l.titer != nil {
+		l.err = l.titer.Close()
+		l.titer = nil
+	}
 	return l.err
 }
---
+++ level_iter_test.go
@@ -0,0 +1,4 @@
+package pebble
+
+// TODO(peter): Set up a fake environment with multiple tables and test various
+// options for iteration.
