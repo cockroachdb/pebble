8dd6b3a43cdb2927b98399caf2a4498bebf8b1b4
Peter Mattis <petermattis@gmail.com>
2018-08-11 14:36:33 -0400

    Convert dbIter tests to be data-driven

--- db_iter_test.go
+++ db_iter_test.go
@@ -8,94 +8,28 @@ import (
 	"testing"
 	"time"
 
+	"github.com/petermattis/pebble/datadriven"
 	"github.com/petermattis/pebble/db"
 )
 
-func TestDBIterNextPrev(t *testing.T) {
-	testCases := []struct {
-		seek         string
-		keys         string
-		expectedNext string
-		expectedPrev string
-	}{
-		{
-			"a:1",
-			"a.SET.1:b",
-			"<a:b>.",
-			".",
-		},
-		{
-			"a:1",
-			"a.SET.2:c a.SET.1:b",
-			"<a:b>.",
-			".",
-		},
-		{
-			"a:2",
-			"a.SET.2:c a.SET.1:b",
-			"<a:c>.",
-			".",
-		},
-		{
-			"a:2",
-			"a.DEL.2: a.SET.1:b",
-			".",
-			".",
-		},
-		{
-			"a:3",
-			"a.DEL.2: a.SET.1:b b.SET.3:c",
-			"<b:c>.",
-			".",
-		},
-		{
-			"a:3",
-			"a.SET.1:a b.SET.2:b c.SET.3:c",
-			"<a:a><b:b><c:c>.",
-			".",
-		},
-		{
-			"b:3",
-			"a.SET.1:a b.SET.2:b c.SET.3:c",
-			"<b:b><c:c>.",
-			"<a:a>.",
-		},
-		{
-			"c:3",
-			"a.SET.1:a b.SET.2:b c.SET.3:c",
-			"<c:c>.",
-			"<b:b><a:a>.",
-		},
-		{
-			"c:3",
-			"a.SET.1:a b.SET.2:b c.SET.3:c",
-			"<c:c>.",
-			"<b:b><a:a>.",
-		},
-		// Batch sequence numbers are not skipped.
-		{
-			"a:1",
-			"a.SET.b2:b b.SET.2:c",
-			"<a:b>.",
-			".",
-		},
-		{
-			"b:1",
-			"a.SET.b2:b b.SET.2:c",
-			".",
-			"<a:b>.",
-		},
-	}
-	for _, c := range testCases {
-		t.Run("", func(t *testing.T) {
-			var keys []db.InternalKey
-			var vals [][]byte
-			for _, key := range strings.Split(c.keys, " ") {
+func TestDBIter(t *testing.T) {
+	var keys []db.InternalKey
+	var vals [][]byte
+
+	datadriven.RunTest(t, "testdata/db_iter", func(d *datadriven.TestData) string {
+		switch d.Cmd {
+		case "define":
+			keys = nil
+			vals = nil
+			for _, key := range strings.Split(d.Input, "\n") {
 				j := strings.Index(key, ":")
 				keys = append(keys, makeIkey(key[:j]))
 				vals = append(vals, []byte(key[j+1:]))
 			}
-			seek := fakeIkey(c.seek)
+			return ""
+
+		case "next":
+			seek := fakeIkey(strings.TrimSpace(d.Input))
 			iter := &dbIter{
 				cmp:    db.DefaultComparer.Compare,
 				iter:   &fakeIter{keys: keys, vals: vals},
@@ -104,31 +38,33 @@ func TestDBIterNextPrev(t *testing.T) {
 
 			var b bytes.Buffer
 			for iter.SeekGE([]byte(seek.UserKey)); iter.Valid(); iter.Next() {
-				fmt.Fprintf(&b, "<%s:%s>", iter.Key(), iter.Value())
+				fmt.Fprintf(&b, "%s:%s\n", iter.Key(), iter.Value())
 			}
 			if err := iter.Error(); err != nil {
-				fmt.Fprintf(&b, "err=%v", err)
-			} else {
-				b.WriteByte('.')
+				fmt.Fprintf(&b, "err=%v\n", err)
 			}
-			if got := b.String(); got != c.expectedNext {
-				t.Errorf("got  %q\nwant %q", got, c.expectedNext)
+			return b.String()
+
+		case "prev":
+			seek := fakeIkey(strings.TrimSpace(d.Input))
+			iter := &dbIter{
+				cmp:    db.DefaultComparer.Compare,
+				iter:   &fakeIter{keys: keys, vals: vals},
+				seqNum: seek.SeqNum(),
 			}
 
-			b.Reset()
+			var b bytes.Buffer
 			for iter.SeekLT([]byte(seek.UserKey)); iter.Valid(); iter.Prev() {
-				fmt.Fprintf(&b, "<%s:%s>", iter.Key(), iter.Value())
+				fmt.Fprintf(&b, "%s:%s\n", iter.Key(), iter.Value())
 			}
-			if err := iter.Close(); err != nil {
-				fmt.Fprintf(&b, "err=%v", err)
-			} else {
-				b.WriteByte('.')
+			if err := iter.Error(); err != nil {
+				fmt.Fprintf(&b, "err=%v\n", err)
 			}
-			if got := b.String(); got != c.expectedPrev {
-				t.Errorf("got  %q\nwant %q", got, c.expectedPrev)
-			}
-		})
-	}
+			return b.String()
+		}
+
+		return ""
+	})
 }
 
 func BenchmarkDBIterSeekGE(b *testing.B) {
