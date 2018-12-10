6f27c45b1fae608d3764a86b50d3fc4de305620b
Peter Mattis <petermattis@gmail.com>
2018-12-09 22:10:21 -0500

    Update benchmark numbers

--- bench.sh
+++ bench.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# pushd internal/arenaskl
-# go test -cpu 1,8 -run - -bench . -count 10 -timeout 1h 2>&1 | tee bench.txt.new
-# popd
+pushd internal/arenaskl
+go test -cpu 1,8 -run - -bench . -count 10 -timeout 1h 2>&1 | tee bench.txt.new
+popd
 
-# pushd internal/batchskl
-# go test -run - -bench . -count 10 -timeout 1h 2>&1 | tee bench.txt.new
-# popd
+pushd internal/batchskl
+go test -run - -bench . -count 10 -timeout 1h 2>&1 | tee bench.txt.new
+popd
 
 pushd internal/record
 go test -run - -bench . -count 10 -timeout 1h 2>&1 | tee bench.txt.new
