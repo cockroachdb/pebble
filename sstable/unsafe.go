651ae1b91f2dea240c8d9b95f0d131c44577f450
Peter Mattis <petermattis@gmail.com>
2019-04-13 13:31:28 -0400

    Add prefix varint routines

--- sstable/unsafe.go
+++ sstable/unsafe.go
@@ -4,7 +4,10 @@
 
 package sstable
 
-import "unsafe"
+import (
+	"math/bits"
+	"unsafe"
+)
 
 func getBytes(ptr unsafe.Pointer, length int) []byte {
 	return (*[1 << 30]byte)(ptr)[:length:length]
@@ -29,4 +32,40 @@ func decodeVarint(ptr unsafe.Pointer) (uint32, unsafe.Pointer) {
 		return uint32(e)<<28 | uint32(d)<<21 | uint32(c)<<14 | uint32(b)<<7 | uint32(a),
 			unsafe.Pointer(uintptr(ptr) + 5)
 	}
+}
+
+// xxxxxxx1  7 bits in 1 byte
+// xxxxxx10 14 bits in 2 bytes
+// xxxxx100 21 bits in 3 bytes
+// xxxx1000 28 bits in 4 bytes
+// xxx10000 35 bits in 5 bytes
+
+func encodePrefixVarint(buf []byte, v uint32) int {
+	bits := 32 - bits.LeadingZeros32(v)
+	bytes := 1 + (bits-1)/7
+	_ = buf[4]
+	buf[0] = byte(((v << 1) + 1) << uint(bytes-1))
+	v >>= uint(8 - bytes)
+	buf[1] = byte(v)
+	buf[2] = byte(v >> 8)
+	buf[3] = byte(v >> 16)
+	buf[4] = byte(v >> 24)
+	return bytes
+}
+
+var mask = [6]uint32{
+	0,
+	(1 << 7) - 1,
+	(1 << 14) - 1,
+	(1 << 21) - 1,
+	(1 << 28) - 1,
+	(1 << 32) - 1,
+}
+
+func decodePrefixVarint(ptr unsafe.Pointer) (uint32, unsafe.Pointer) {
+	src := (*[5]uint8)(ptr)
+	bytes := uint(1 + bits.TrailingZeros8(src[0]))
+	v := uint64(src[0]) | uint64(src[1])<<8 | uint64(src[2])<<16 | uint64(src[3])<<24 | uint64(src[4])<<32
+	v >>= bytes
+	return uint32(v) & mask[bytes], unsafe.Pointer(uintptr(ptr) + uintptr(bytes))
 }
--- sstable/unsafe_test.go
+++ sstable/unsafe_test.go
@@ -39,27 +39,133 @@ func TestDecodeVarint(t *testing.T) {
 	}
 }
 
+func TestPrefixVarint(t *testing.T) {
+	buf := make([]byte, 5)
+	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+	for i := 0; i < 100000; i++ {
+		v := rng.Uint32()
+		n := encodePrefixVarint(buf, v)
+		d, _ := decodePrefixVarint(unsafe.Pointer(&buf[0]))
+		if v != d {
+			fmt.Printf("%d != %d: %08b\n", v, d, buf[:n])
+		}
+	}
+}
+
+func BenchmarkEncodeVarint(b *testing.B) {
+	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+
+	for _, bytes := range []int{1, 2, 3, 4, 5} {
+		b.Run(fmt.Sprintf("bytes=%d", bytes), func(b *testing.B) {
+			vals := make([]uint32, 10000)
+			for i := range vals {
+				vals[i] = rng.Uint32() % mask[bytes]
+			}
+			buf := make([]byte, 5)
+
+			b.ResetTimer()
+			var t int
+			for i, n := 0, 0; i < b.N; i += n {
+				n = len(vals)
+				if n > b.N-i {
+					n = b.N - i
+				}
+				for j := 0; j < n; j++ {
+					t = binary.PutUvarint(buf, uint64(vals[j]))
+				}
+			}
+			if testing.Verbose() {
+				fmt.Fprint(ioutil.Discard, t)
+			}
+		})
+	}
+}
+
 func BenchmarkDecodeVarint(b *testing.B) {
 	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
-	vals := make([]unsafe.Pointer, 10000)
-	for i := range vals {
-		buf := make([]byte, 5)
-		binary.PutUvarint(buf, uint64(rng.Uint32()))
-		vals[i] = unsafe.Pointer(&buf[0])
-	}
 
-	b.ResetTimer()
-	var ptr unsafe.Pointer
-	for i, n := 0, 0; i < b.N; i += n {
-		n = len(vals)
-		if n > b.N-i {
-			n = b.N - i
-		}
-		for j := 0; j < n; j++ {
-			_, ptr = decodeVarint(vals[j])
-		}
+	for _, bytes := range []int{1, 2, 3, 4, 5} {
+		b.Run(fmt.Sprintf("bytes=%d", bytes), func(b *testing.B) {
+			vals := make([]unsafe.Pointer, 10000)
+			for i := range vals {
+				buf := make([]byte, 5)
+				binary.PutUvarint(buf, uint64(rng.Uint32()&mask[bytes]))
+				vals[i] = unsafe.Pointer(&buf[0])
+			}
+
+			b.ResetTimer()
+			var ptr unsafe.Pointer
+			for i, n := 0, 0; i < b.N; i += n {
+				n = len(vals)
+				if n > b.N-i {
+					n = b.N - i
+				}
+				for j := 0; j < n; j++ {
+					_, ptr = decodeVarint(vals[j])
+				}
+			}
+			if testing.Verbose() {
+				fmt.Fprint(ioutil.Discard, ptr)
+			}
+		})
 	}
-	if testing.Verbose() {
-		fmt.Fprint(ioutil.Discard, ptr)
+}
+
+func BenchmarkEncodePrefixVarint(b *testing.B) {
+	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+
+	for _, bytes := range []int{1, 2, 3, 4, 5} {
+		b.Run(fmt.Sprintf("bytes=%d", bytes), func(b *testing.B) {
+			vals := make([]uint32, 10000)
+			for i := range vals {
+				vals[i] = rng.Uint32() % mask[bytes]
+			}
+			buf := make([]byte, 5)
+
+			b.ResetTimer()
+			var t int
+			for i, n := 0, 0; i < b.N; i += n {
+				n = len(vals)
+				if n > b.N-i {
+					n = b.N - i
+				}
+				for j := 0; j < n; j++ {
+					t = encodePrefixVarint(buf, vals[j])
+				}
+			}
+			if testing.Verbose() {
+				fmt.Fprint(ioutil.Discard, t)
+			}
+		})
+	}
+}
+
+func BenchmarkDecodePrefixVarint(b *testing.B) {
+	rng := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+
+	for _, bytes := range []int{1, 2, 3, 4, 5} {
+		b.Run(fmt.Sprintf("bytes=%d", bytes), func(b *testing.B) {
+			vals := make([]unsafe.Pointer, 10000)
+			for i := range vals {
+				buf := make([]byte, 5)
+				encodePrefixVarint(buf, rng.Uint32()%mask[bytes])
+				vals[i] = unsafe.Pointer(&buf[0])
+			}
+
+			b.ResetTimer()
+			var v uint32
+			for i, n := 0, 0; i < b.N; i += n {
+				n = len(vals)
+				if n > b.N-i {
+					n = b.N - i
+				}
+				for j := 0; j < n; j++ {
+					v, _ = decodePrefixVarint(vals[j])
+				}
+			}
+			if testing.Verbose() {
+				fmt.Fprint(ioutil.Discard, v)
+			}
+		})
 	}
 }
