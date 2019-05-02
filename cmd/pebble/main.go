799bc62c15c8d75b79d2df42af50ae8e80fc81bd
Andrew Kryczka <andrew.kryczka2@gmail.com>
2019-05-01 21:23:05 -0700

    Add ycsb benchmark
    This PR adapts the logic from Cockroach repo's `workload kv` and
    integrates it into a Pebble subcommand, `ycsb`. It can be run as
    follows:
    
    `go run ./cmd/pebble/ ycsb --wipe ./tmp --read-percent 50 --min-block-bytes 4096 --max-block-bytes 4096 --duration 3600s`
    
    Not all of the options are included. In particular, these ones are
    omitted: `--tolerate-errors`, `--max-rate`, `--ramp`, `--pprof-port`,
    `--histograms`, and `--zipfian`. We can add them later if needed.

--- cmd/pebble/main.go
+++ cmd/pebble/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"math"
 	"os"
 	"time"
 
@@ -30,9 +31,10 @@ func main() {
 	rootCmd.AddCommand(
 		scanCmd,
 		syncCmd,
+		ycsbCmd,
 	)
 
-	for _, cmd := range []*cobra.Command{scanCmd, syncCmd} {
+	for _, cmd := range []*cobra.Command{scanCmd, syncCmd, ycsbCmd} {
 		cmd.Flags().IntVarP(
 			&concurrency, "concurrency", "c", 1, "number of concurrent workers")
 		cmd.Flags().BoolVar(
@@ -51,6 +53,37 @@ func main() {
 		&scanRows, "rows", scanRows, "number of rows to scan in each operation")
 	scanCmd.Flags().IntVar(
 		&scanValueSize, "value", scanValueSize, "size of values to scan")
+
+	ycsbCmd.Flags().IntVar(
+		&ycsbConfig.batch, "batch", 1,
+		"Number of keys to read/insert in each operation")
+	ycsbCmd.Flags().Int64Var(
+		&ycsbConfig.cycleLength, "cycle-length", math.MaxInt64,
+		"Number of keys repeatedly accessed by each writer")
+	ycsbCmd.Flags().IntVar(
+		&ycsbConfig.minBlockBytes, "min-block-bytes", 1,
+		"Minimum amount of raw data written with each insertion")
+	ycsbCmd.Flags().IntVar(
+		&ycsbConfig.maxBlockBytes, "max-block-bytes", 1,
+		"Maximum amount of raw data written with each insertion")
+	ycsbCmd.Flags().Uint64VarP(
+		&ycsbConfig.numOps, "num-ops", "n", 0, "maximum number of operations (0 means unlimited)")
+	ycsbCmd.Flags().IntVar(
+		&ycsbConfig.readPercent, "read-percent", 0,
+		"Percent (0-100) of operations that are reads of existing keys")
+	ycsbCmd.Flags().Int64Var(
+		&ycsbConfig.seed, "seed", 1, "Key hash seed")
+	ycsbCmd.Flags().BoolVar(
+		&ycsbConfig.sequential, "sequential", false,
+		"Pick keys sequentially instead of uniformly at random")
+	ycsbCmd.Flags().StringVar(
+		&ycsbConfig.writeSeq, "write-seq", "",
+		"Initial write sequence value. Can be used to use the data produced by a previous run. "+
+			"It has to be of the form (R|S)<number>, where S implies that it was taken from a "+
+			"previous --sequential run and R implies a previous random run.")
+	ycsbCmd.Flags().Float64Var(
+		&ycsbConfig.targetCompressionRatio, "target-compression-ratio", 1.0,
+		"Target compression ratio for data blocks. Must be >= 1.0")
 
 	if err := rootCmd.Execute(); err != nil {
 		// Cobra has already printed the error message.
