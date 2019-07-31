716f60d6253db0dbfe450da888a781ab7f7d564c
Ryan Kim <ryanfsdf@live.ca>
2019-07-31 15:55:04 -0400

    Refactor compaction and flushing mechanism

--- open.go
+++ open.go
@@ -324,7 +324,7 @@ func (d *DB) replayWAL(
 	if mem != nil && !mem.empty() {
 		c := newFlush(d.opts, d.mu.versions.currentVersion(),
 			1 /* base level */, []flushable{mem})
-		newVE, pendingOutputs, err := d.runCompaction(c)
+		newVE, pendingOutputs, err := d.runCompaction(c, nil /* pacer */)
 		if err != nil {
 			return 0, err
 		}
