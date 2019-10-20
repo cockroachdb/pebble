d968cb15262ad29df2023be10dc6f208574351e0
Peter Mattis <petermattis@gmail.com>
2019-10-20 12:27:56 -0400

    internal/record: wait for flush-loop to terminate in LogWriter.Close
    `LogWriter.Close()` was previously failing to wait for the flush-loop to
    terminate which led to a rare race condition in the `internal/record`
    tests where a buffer shared between a writer and a reader could be
    written to by the writer's flush-loop after the writer had been
    closed.
    
    Added additional synchronization to allow waiting for the flush-loop to
    terminate after `Close` has signalled for it to close. Tweaked the way
    the final sync is performed, so that it is done directly in `Close()`
    rather than asking the sync loop to perform it. The problem with asking
    the sync loop to perform the close was related to the above race: we
    could ask for a sync, but miss flushing of the data in a partially
    filled block.
    
    Fixes #356

--- internal/record/log_writer.go
+++ internal/record/log_writer.go
@@ -210,8 +210,6 @@ type LogWriter struct {
 	w io.Writer
 	// c is w as a closer.
 	c io.Closer
-	// f is w as a flusher.
-	f flusher
 	// s is w as a syncer.
 	s syncer
 	// logNum is the low 32-bits of the log's file number.
@@ -232,8 +230,10 @@ type LogWriter struct {
 		// closed. For signalling of a sync, it is safe to call without holding
 		// flusher.Mutex.
 		ready flusherCond
-		// Has the writer been closed?
-		closed bool
+		// Set to true when the flush loop should be closed.
+		close bool
+		// Closed when the flush loop has terminated.
+		closed chan struct{}
 		// Accumulated flush error.
 		err     error
 		pending []*block
@@ -244,12 +244,10 @@ type LogWriter struct {
 // NewLogWriter returns a new LogWriter.
 func NewLogWriter(w io.Writer, logNum uint64) *LogWriter {
 	c, _ := w.(io.Closer)
-	f, _ := w.(flusher)
 	s, _ := w.(syncer)
 	r := &LogWriter{
 		w: w,
 		c: c,
-		f: f,
 		s: s,
 		// NB: we truncate the 64-bit log number to 32-bits. This is ok because a)
 		// we are very unlikely to reach a file number of 4 billion and b) the log
@@ -263,6 +261,7 @@ func NewLogWriter(w io.Writer, logNum uint64) *LogWriter {
 	}
 	r.block = <-r.free
 	r.flusher.ready.init(&r.flusher.Mutex, &r.flusher.syncQ)
+	r.flusher.closed = make(chan struct{})
 	go r.flushLoop()
 	return r
 }
@@ -270,14 +269,14 @@ func NewLogWriter(w io.Writer, logNum uint64) *LogWriter {
 func (w *LogWriter) flushLoop() {
 	f := &w.flusher
 	f.Lock()
-	defer f.Unlock()
+	defer func() {
+		close(f.closed)
+		f.Unlock()
+	}()
 
 	for {
 		var data []byte
 		for {
-			if f.closed {
-				return
-			}
 			// Grab the portion of the current block that requires flushing. Note that
 			// the current block can be added to the pending blocks list after we release
 			// the flusher lock, but it won't be part of pending.
@@ -286,6 +285,9 @@ func (w *LogWriter) flushLoop() {
 			w.block.flushed = written
 			if len(f.pending) > 0 || len(data) > 0 || !f.syncQ.empty() {
 				break
+			}
+			if f.close {
+				return
 			}
 			f.ready.Wait()
 			continue
@@ -374,18 +376,21 @@ func (w *LogWriter) queueBlock() {
 func (w *LogWriter) Close() error {
 	f := &w.flusher
 
-	// Force a sync of any unwritten data.
-	wg := &sync.WaitGroup{}
-	var err error
-	wg.Add(1)
-	f.syncQ.push(wg, &err)
-	f.ready.Signal()
-	wg.Wait()
-
+	// Signal the flush loop to close.
 	f.Lock()
-	f.closed = true
+	f.close = true
 	f.ready.Signal()
 	f.Unlock()
+
+	// Wait for the flush loop to close. The flush loop will not close until all
+	// pending data has been written or an error occurs.
+	<-f.closed
+
+	// Sync any buffered data to disk.
+	err := w.flusher.err
+	if err == nil && w.s != nil {
+		err = w.s.Sync()
+	}
 
 	if w.c != nil {
 		if err := w.c.Close(); err != nil {
