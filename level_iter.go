bfe7c7c2754945c5ca3f7874d2fb608093ed9eae
Peter Mattis <petermattis@gmail.com>
2019-11-07 20:31:33 -0500

    fix levelIter.Seek{GE,LT} bug
    Fix a bug which caused `levelIter.Seek{GE,LT}` to return `nil,nil` when
    it was positioned at a boundary key and the seek operation was to the
    same sstable that it was already pointing at. The root cause of the bug
    was overloading the meaning of the `levelIter.iter` field.
    
    Took the opportunity to remove some detritus that had been necessary
    before previous simplifications to the `internalIterator`
    API. Specifically, now that there is no longer `Key()` method we don't
    need to setting `levelIter.iter` to `nil` when we're at a boundary
    key. Additionally, there is no need for `Next()` and `Prev()` to do
    anything sane when positioned off the end of the level as all callers
    properly use `First()` and `Last()` now.

--- level_iter.go
+++ level_iter.go
@@ -32,8 +32,7 @@ type levelIter struct {
 	cmp       Compare
 	index     int
 	// The key to return when iterating past an sstable boundary and that
-	// boundary is a range deletion tombstone. Note that if boundary != nil, then
-	// iter == nil, and if iter != nil, then boundary == nil.
+	// boundary is a range deletion tombstone.
 	boundary     *InternalKey
 	iter         internalIterator
 	newIters     tableNewIters
@@ -179,21 +178,21 @@ func (l *levelIter) loadFile(index, dir int) bool {
 	}
 	if l.iter != nil {
 		l.err = l.iter.Close()
+		l.iter = nil
 		if l.err != nil {
 			return false
 		}
-		l.iter = nil
 	}
 	if l.rangeDelIter != nil {
 		// Close the rangeDelIter too since the mergingIter does not call close() on it (it can't
 		// because it does not know when the levelIter will switch it).
 		rdi := *l.rangeDelIter
 		if rdi != nil {
+			*l.rangeDelIter = nil
 			if l.err = rdi.Close(); l.err != nil {
 				return false
 			}
 		}
-		*l.rangeDelIter = nil
 	}
 
 	for ; ; index += dir {
@@ -323,24 +322,9 @@ func (l *levelIter) Next() (*InternalKey, []byte) {
 		return nil, nil
 	}
 
-	if l.iter == nil {
-		// One of the following cases:
-		// - Was positioned at boundary key and now stepping past it.
-		// - Positioned before the beginning.
-		// - None of the above: must have exhausted the iterator -- nothing to return.
-		if l.boundary != nil {
-			// levelIter is being stepped past the boundary key, so now we can load the next file.
-			if l.loadFile(l.index+1, 1) {
-				if key, val := l.iter.First(); key != nil {
-					return key, val
-				}
-				return l.skipEmptyFileForward()
-			}
-			return nil, nil
-		}
-		if l.index == -1 && l.loadFile(0, 1) {
-			// The iterator was positioned off the beginning of the level. Position
-			// at the first entry.
+	if l.boundary != nil {
+		// We're stepping past the boundary key, so now we can load the next file.
+		if l.loadFile(l.index+1, 1) {
 			if key, val := l.iter.First(); key != nil {
 				return key, val
 			}
@@ -349,6 +333,9 @@ func (l *levelIter) Next() (*InternalKey, []byte) {
 		return nil, nil
 	}
 
+	if l.iter == nil {
+		return nil, nil
+	}
 	if key, val := l.iter.Next(); key != nil {
 		return key, val
 	}
@@ -360,19 +347,9 @@ func (l *levelIter) Prev() (*InternalKey, []byte) {
 		return nil, nil
 	}
 
-	if l.iter == nil {
-		if l.boundary != nil {
-			if l.loadFile(l.index-1, -1) {
-				if key, val := l.iter.Last(); key != nil {
-					return key, val
-				}
-				return l.skipEmptyFileBackward()
-			}
-			return nil, nil
-		}
-		if n := len(l.files); l.index == n && l.loadFile(n-1, -1) {
-			// The iterator was positioned off the end of the level. Position at the
-			// last entry.
+	if l.boundary != nil {
+		// We're stepping past the boundary key, so now we can load the prev file.
+		if l.loadFile(l.index-1, -1) {
 			if key, val := l.iter.Last(); key != nil {
 				return key, val
 			}
@@ -381,6 +358,9 @@ func (l *levelIter) Prev() (*InternalKey, []byte) {
 		return nil, nil
 	}
 
+	if l.iter == nil {
+		return nil, nil
+	}
 	if key, val := l.iter.Prev(); key != nil {
 		return key, val
 	}
@@ -400,26 +380,14 @@ func (l *levelIter) skipEmptyFileForward() (*InternalKey, []byte) {
 	// not have an exhausted iterator causes the code to return that key, else if the rangeDelIter
 	// contributes the largest key of the file, we do the behavior described above.
 	for ; key == nil; key, val = l.iter.First() {
-		if l.err = l.iter.Close(); l.err != nil {
-			return nil, nil
-		}
-		l.iter = nil
-
 		if l.rangeDelIter != nil {
-			// We're being used as part of an Iterator and we've reached the end of
+			// We're being used as part of a mergingIter and we've reached the end of
 			// the sstable. If the boundary is a range deletion tombstone, return
 			// that key.
 			if f := &l.files[l.index]; f.Largest.Kind() == InternalKeyKindRangeDelete {
 				l.boundary = &f.Largest
 				return l.boundary, nil
 			}
-			rdi := *l.rangeDelIter
-			if rdi != nil {
-				if l.err = rdi.Close(); l.err != nil {
-					return nil, nil
-				}
-			}
-			*l.rangeDelIter = nil
 		}
 
 		// Current file was exhausted. Move to the next file.
@@ -443,26 +411,14 @@ func (l *levelIter) skipEmptyFileBackward() (*InternalKey, []byte) {
 	// not have an exhausted iterator causes the code to return that key, else if the rangeDelIter
 	// contributes the smallest key of the file, we do the behavior described above.
 	for ; key == nil; key, val = l.iter.Last() {
-		if l.err = l.iter.Close(); l.err != nil {
-			return nil, nil
-		}
-		l.iter = nil
-
 		if l.rangeDelIter != nil {
-			// We're being used as part of an Iterator and we've reached the end of
+			// We're being used as part of a mergingIter and we've reached the end of
 			// the sstable. If the boundary is a range deletion tombstone, return
 			// that key.
 			if f := &l.files[l.index]; f.Smallest.Kind() == InternalKeyKindRangeDelete {
 				l.boundary = &f.Smallest
 				return l.boundary, nil
 			}
-			rdi := *l.rangeDelIter
-			if rdi != nil {
-				if l.err = rdi.Close(); l.err != nil {
-					return nil, nil
-				}
-			}
-			*l.rangeDelIter = nil
 		}
 
 		// Current file was exhausted. Move to the previous file.
