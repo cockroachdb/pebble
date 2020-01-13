fa7a7f647ec947a496c1f1dd593e52f596393356
sumeerbhola <sumeer@cockroachlabs.com>
2020-01-13 10:51:28 -0500

    db: fix an incomplete manifest file error when reopening a db This was caused by the snapshot VersionEdit not containing minUnflushedLogNum and the nextFileNum. Also print the nextFileNum in the "manifest dump".
    Found using the extension to the metamorphic test that closes and reopens the DB.

---
+++ version_set_test.go
@@ -0,0 +1,51 @@
+// Copyright 2020 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package pebble
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/sstable"
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVersionSetCheckpoint(t *testing.T) {
+	mem := vfs.NewMem()
+	require.NoError(t, mem.MkdirAll("ext", 0755))
+
+	opts := &Options{
+		FS:                  mem,
+		MaxManifestFileSize: 1,
+	}
+	d, err := Open("", opts)
+	require.NoError(t, err)
+
+	writeAndIngest := func(k InternalKey, v []byte, filename string) {
+		path := mem.PathJoin("ext", filename)
+		f, err := mem.Create(path)
+		require.NoError(t, err)
+		w := sstable.NewWriter(f, sstable.WriterOptions{})
+		require.NoError(t, w.Add(k, v))
+		require.NoError(t, w.Close())
+		require.NoError(t, d.Ingest([]string{path}))
+	}
+
+	// Multiple manifest files are created such that the latest one must have a correct snapshot
+	// of the preceding state for the DB to be opened correctly and see the written data.
+	writeAndIngest(base.MakeInternalKey([]byte("a"), 0, InternalKeyKindSet), []byte("b"), "a")
+	writeAndIngest(base.MakeInternalKey([]byte("c"), 0, InternalKeyKindSet), []byte("d"), "c")
+	require.NoError(t, d.Close())
+	d, err = Open("", opts)
+	require.NoError(t, err)
+	checkValue := func(k string, expected string) {
+		v, err := d.Get([]byte(k))
+		require.NoError(t, err)
+		require.Equal(t, expected, string(v))
+	}
+	checkValue("a", "b")
+	checkValue("c", "d")
+}
