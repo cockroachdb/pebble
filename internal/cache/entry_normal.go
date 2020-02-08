0df3bcaaf494768a229efc3735169e57e123bbc2
Peter Mattis <petermattis@gmail.com>
2020-02-08 11:07:11 -0500

    internal/cache: optimize allocation of entry objects
    Each entry in the cache has an associated `entry` struct. For large
    block caches, the number of entries can reach 500k-1m or higher, and
    consume a significant fraction of the total allocated objects in the Go
    heap. For manually managed values, the lifetime of the associated
    `entry` is known and the `*entry` never escapes outside of the cache
    package. This allows us to manually manage the memory for these entries,
    which are the majority of entries in the cache.

---
+++ internal/cache/entry_normal.go
@@ -0,0 +1,68 @@
+// Copyright 2020 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+//
+// +build !invariants,!tracing
+
+package cache
+
+import (
+	"runtime"
+	"sync"
+	"unsafe"
+
+	"github.com/cockroachdb/pebble/internal/manual"
+)
+
+var entryAllocPool = sync.Pool{
+	New: func() interface{} {
+		return newEntryAllocCache()
+	},
+}
+
+func entryAllocNew() *entry {
+	a := entryAllocPool.Get().(*entryAllocCache)
+	e := a.alloc()
+	entryAllocPool.Put(a)
+	return e
+}
+
+func entryAllocFree(e *entry) {
+	a := entryAllocPool.Get().(*entryAllocCache)
+	a.free(e)
+	entryAllocPool.Put(a)
+}
+
+type entryAllocCache struct {
+	entries []*entry
+}
+
+func newEntryAllocCache() *entryAllocCache {
+	c := &entryAllocCache{}
+	runtime.SetFinalizer(c, freeEntryAllocCache)
+	return c
+}
+
+func freeEntryAllocCache(obj interface{}) {
+	c := obj.(*entryAllocCache)
+	for i, e := range c.entries {
+		buf := (*[manual.MaxArrayLen]byte)(unsafe.Pointer(e))[:entrySize:entrySize]
+		manual.Free(buf)
+		c.entries[i] = nil
+	}
+}
+
+func (c *entryAllocCache) alloc() *entry {
+	n := len(c.entries)
+	if n == 0 {
+		b := manual.New(entrySize)
+		return (*entry)(unsafe.Pointer(&b[0]))
+	}
+	e := c.entries[n-1]
+	c.entries = c.entries[:n-1]
+	return e
+}
+
+func (c *entryAllocCache) free(e *entry) {
+	c.entries = append(c.entries, e)
+}
