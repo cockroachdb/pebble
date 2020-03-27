b512ad587b06f5425d0c2bc822f63aaf16bcfc87
Jackson Owens <jackson@cockroachlabs.com>
2020-03-27 16:34:58 -0400

    db: document that Close may not be called twice
    The Close documentation states that Close may be called multiple times,
    but Close deliberately panics if the DB has already been marked as
    closed.

--- db.go
+++ db.go
@@ -805,8 +805,7 @@ func (d *DB) NewSnapshot() *Snapshot {
 // Close closes the DB.
 //
 // It is not safe to close a DB until all outstanding iterators are closed.
-// It is valid to call Close multiple times. Other methods should not be
-// called after the DB has been closed.
+// It is not valid to call any of a DB's methods after the DB has been closed.
 func (d *DB) Close() error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
