e897d5b75ac9a676804e6137cf76b86ef8bafad1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-04-15 00:02:53 +0300

    base: removed *usually*

--- internal/base/merger.go
+++ internal/base/merger.go
@@ -14,7 +14,7 @@ type Merge func(key, value []byte) (ValueMerger, error)
 // newer or older than all operands received so far as indicated by the function
 // names, `MergeNewer()` and `MergeOlder()`. Once all operands have been received,
 // the client will invoke `Finish()` to obtain the final result. The order of
-// a merge is usually not changed after the first call to `MergeNewer()` or
+// a merge is not changed after the first call to `MergeNewer()` or
 // `MergeOlder()`, i.e. the same method is used to submit all operands.
 //
 // The implementation may choose to merge values into the result immediately upon
