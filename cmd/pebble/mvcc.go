f8073be299095db3b78aaaf6ccac2fcd9feb9fd0
Jackson Owens <jackson@cockroachlabs.com>
2020-05-05 12:18:31 -0400

    cmd/pebble: add bench compact subcommands
    Add bench compact subcommands to cmd/pebble. These subcommands run
    write-only workloads for evaluating changes to compaction heuristics.
    
    A bench compact workload is created by first running load against a
    Pebble instance configured with the pebble.ArchiveCleaner. Next the
    `pebble bench compact new` subcommand reads the pebble instance's
    archive gathering sstable creation and deletion entries from manifests
    and rewriting sstables to remove duplicate keys and zero sequence
    numbers. It writes the sstable history and modified sstables to a new
    directory representing the workload.
    
    A change to a compaction heuristic can be evaluated by compiling the
    compactbench command with the change and running `pebble bench compact run`
    passing the path to the prepared workload. The `run` subcommand ingests
    sstables from the workload, comparing the total size of L0 to the
    original workload's L0 size to establish pacing.
    
    After the workload finishes `run` prints the LSM metrics, compacts the
    entire database and prints the ending state's space amplificaiton, total
    write amplification, etc. There will probably be additional statistics
    that we'll want to add, but this is low-hanging fruit.

--- cmd/pebble/mvcc.go
+++ cmd/pebble/mvcc.go
@@ -135,3 +135,13 @@ func mvccReverseScan(d DB, start, end, ts []byte) (int, int64) {
 	}
 	return count, nbytes
 }
+
+var fauxMVCCMerger = &pebble.Merger{
+	Name: "cockroach_merge_operator",
+	Merge: func(key, value []byte) (pebble.ValueMerger, error) {
+		// This merger is used by the compact benchmark and use the
+		// pebble default value merger to concatenate values.
+		// It shouldn't materially affect the benchmarks.
+		return pebble.DefaultMerger.Merge(key, value)
+	},
+}
