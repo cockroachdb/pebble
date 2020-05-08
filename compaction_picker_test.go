4a47e2ec54049414c76b393b2f1b6597f4014695
Bilal Akhtar <bilal@cockroachlabs.com>
2020-05-08 12:58:02 -0400

    *: Use L0SubLevels to pick base/intra-L0 compactions
    This change updates the compaction picker to call into L0SubLevels
    for picking base and intra-L0 compactions, instead of the previous
    method of picking compactions.
    
    The definition of L0StopWritesThreshold and L0CompactionThreshold
    has now been updated to refer to the number of overlapping L0 files
    as opposed to the previous definition (number of L0 files in all).

--- compaction_picker_test.go
+++ compaction_picker_test.go
@@ -7,6 +7,8 @@ package pebble
 import (
 	"bytes"
 	"fmt"
+	"math"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
@@ -415,4 +417,152 @@ func TestCompactionPickerIntraL0(t *testing.T) {
 				return fmt.Sprintf("unknown command: %s", d.Cmd)
 			}
 		})
+}
+
+func TestCompactionPickerL0(t *testing.T) {
+	fileNums := func(f []*fileMetadata) string {
+		ss := make([]string, 0, len(f))
+		for _, meta := range f {
+			ss = append(ss, meta.FileNum.String())
+		}
+		sort.Strings(ss)
+		return strings.Join(ss, ",")
+	}
+
+	parseMeta := func(s string) (*fileMetadata, error) {
+		parts := strings.Split(s, ":")
+		fileNum, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, err
+		}
+		fields := strings.Fields(parts[1])
+		parts = strings.Split(fields[0], "-")
+		if len(parts) != 2 {
+			return nil, errors.Errorf("malformed table spec: %s", s)
+		}
+		m := &fileMetadata{
+			FileNum:  base.FileNum(fileNum),
+			Smallest: base.ParseInternalKey(strings.TrimSpace(parts[0])),
+			Largest:  base.ParseInternalKey(strings.TrimSpace(parts[1])),
+		}
+		m.SmallestSeqNum = m.Smallest.SeqNum()
+		m.LargestSeqNum = m.Largest.SeqNum()
+
+		for _, field := range fields[1:] {
+			switch field {
+			case "intra_l0_compacting":
+				m.IsIntraL0Compacting = true
+				m.Compacting = true
+			case "base_compacting":
+				fallthrough
+			case "compacting":
+				m.Compacting = true
+			}
+		}
+		return m, nil
+	}
+
+	opts := (*Options)(nil).EnsureDefaults()
+	opts.Experimental.L0SublevelCompactions = true
+	var picker *compactionPickerByScore
+
+	datadriven.RunTest(t, "testdata/compaction_picker_L0", func(td *datadriven.TestData) string {
+		switch td.Cmd {
+		case "define":
+			fileMetas := [manifest.NumLevels][]*manifest.FileMetadata{}
+			baseLevel := manifest.NumLevels - 1
+			level := 0
+			var err error
+			for _, data := range strings.Split(td.Input, "\n") {
+				data = strings.TrimSpace(data)
+				switch data {
+				case "L0", "L1", "L2", "L3", "L4", "L5", "L6":
+					level, err = strconv.Atoi(data[1:])
+					if err != nil {
+						return err.Error()
+					}
+				default:
+					meta, err := parseMeta(data)
+					if err != nil {
+						return err.Error()
+					}
+					if level != 0 && level < baseLevel {
+						baseLevel = level
+					}
+					fileMetas[level] = append(fileMetas[level], meta)
+				}
+			}
+
+			version := &version{
+				Files:       fileMetas,
+			}
+			if err := version.InitL0Sublevels(DefaultComparer.Compare, base.DefaultFormatter); err != nil {
+				t.Fatal(err)
+			}
+
+			vs := &versionSet{
+				opts:    opts,
+				cmp:     DefaultComparer.Compare,
+				cmpName: DefaultComparer.Name,
+			}
+			vs.versions.Init(nil)
+			vs.append(version)
+			picker = &compactionPickerByScore{
+				opts:      opts,
+				vers:      version,
+				baseLevel: baseLevel,
+			}
+			vs.picker = picker
+			inProgressCompactions := []compactionInfo{}
+			for level, files := range version.Files {
+				for _, f := range files {
+					if f.Compacting {
+						c := compactionInfo{
+							startLevel:  level,
+							outputLevel: level+1,
+							inputs:      [2][]*fileMetadata{{f}},
+						}
+						if f.IsIntraL0Compacting {
+							c.outputLevel = c.startLevel
+						}
+						inProgressCompactions = append(inProgressCompactions, c)
+					}
+				}
+			}
+			picker.initLevelMaxBytes(inProgressCompactions)
+			picker.initScores(inProgressCompactions)
+
+			return version.DebugString(base.DefaultFormatter)
+		case "pick-auto":
+			for _, arg := range td.CmdArgs {
+				var err error
+				switch arg.Key {
+				case "l0_compaction_threshold":
+					opts.L0CompactionThreshold, err = strconv.Atoi(arg.Vals[0])
+					if err != nil {
+						return err.Error()
+					}
+				}
+			}
+			c := picker.pickAuto(compactionEnv{
+				bytesCompacted:          new(uint64),
+				earliestUnflushedSeqNum: math.MaxUint64,
+			})
+			var result strings.Builder
+			if c != nil {
+				fmt.Fprintf(&result, "L%d -> L%d\n", c.startLevel, c.outputLevel)
+				fmt.Fprintf(&result, "L%d: %s\n", c.startLevel, fileNums(c.inputs[0]))
+				if len(c.inputs[1]) > 0 {
+					fmt.Fprintf(&result, "L%d: %s\n", c.outputLevel, fileNums(c.inputs[1]))
+				}
+				if len(c.grandparents) > 0 {
+					fmt.Fprintf(&result, "grandparents: %s\n", fileNums(c.grandparents))
+				}
+			} else {
+				return "nil"
+			}
+			return result.String()
+		}
+		return fmt.Sprintf("unrecognized command: %s", td.Cmd)
+	})
 }
