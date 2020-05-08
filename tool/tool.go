5a133cfa147f571b6fcf30246902e74c2e8af62c
Jackson Owens <jackson@cockroachlabs.com>
2020-05-08 13:05:46 -0400

    tool: use Option pattern, add DefaultComparer option
    Some tools such as the wal inspection tool are unable to infer
    the appropriate comparer. For these tools, allow configuring a default
    comparer.
    
    Additionally, convert the Register{Comparer|Filter|Merger} methods into
    options passed into New. These are always configured at tool
    instantiation and providing them upfront ensures that the find tool can
    print the true default comparer for its 'comparer' flag in its help
    output.
    
    Fix #638.

--- tool/tool.go
+++ tool/tool.go
@@ -30,20 +30,63 @@ type Merger = base.Merger
 
 // T is the container for all of the introspection tools.
 type T struct {
-	Commands  []*cobra.Command
-	db        *dbT
-	find      *findT
-	lsm       *lsmT
-	manifest  *manifestT
-	sstable   *sstableT
-	wal       *walT
-	opts      pebble.Options
-	comparers sstable.Comparers
-	mergers   sstable.Mergers
+	Commands        []*cobra.Command
+	db              *dbT
+	find            *findT
+	lsm             *lsmT
+	manifest        *manifestT
+	sstable         *sstableT
+	wal             *walT
+	opts            pebble.Options
+	comparers       sstable.Comparers
+	mergers         sstable.Mergers
+	defaultComparer string
+}
+
+// A Option configures the Pebble introspection tool.
+type Option func(*T)
+
+// Comparers may be passed to New to register comparers for use by
+// the introspesction tools.
+func Comparers(cmps ...*Comparer) Option {
+	return func(t *T) {
+		for _, c := range cmps {
+			t.comparers[c.Name] = c
+		}
+	}
+}
+
+// DefaultComparer registers a comparer for use by the introspection tools and
+// sets it as the default.
+func DefaultComparer(c *Comparer) Option {
+	return func(t *T) {
+		t.comparers[c.Name] = c
+		t.defaultComparer = c.Name
+	}
+}
+
+// Mergers may be passed to New to register mergers for use by the
+// introspection tools.
+func Mergers(mergers ...*Merger) Option {
+	return func(t *T) {
+		for _, m := range mergers {
+			t.mergers[m.Name] = m
+		}
+	}
+}
+
+// Filters may be passed to New to register filter policies for use by the
+// introspection tools.
+func Filters(filters ...FilterPolicy) Option {
+	return func(t *T) {
+		for _, f := range filters {
+			t.opts.Filters[f.Name()] = f
+		}
+	}
 }
 
 // New creates a new introspection tool.
-func New() *T {
+func New(opts ...Option) *T {
 	cache := pebble.NewCache(128 << 20 /* 128 MB */)
 
 	t := &T{
@@ -53,20 +96,26 @@ func New() *T {
 			FS:       vfs.Default,
 			ReadOnly: true,
 		},
-		comparers: make(sstable.Comparers),
-		mergers:   make(sstable.Mergers),
+		comparers:       make(sstable.Comparers),
+		mergers:         make(sstable.Mergers),
+		defaultComparer: base.DefaultComparer.Name,
 	}
 
-	t.RegisterComparer(base.DefaultComparer)
-	t.RegisterFilter(bloom.FilterPolicy(10))
-	t.RegisterMerger(base.DefaultMerger)
+	opts = append(opts,
+		Comparers(base.DefaultComparer),
+		Filters(bloom.FilterPolicy(10)),
+		Mergers(base.DefaultMerger))
+
+	for _, opt := range opts {
+		opt(t)
+	}
 
 	t.db = newDB(&t.opts, t.comparers, t.mergers)
-	t.find = newFind(&t.opts, t.comparers)
+	t.find = newFind(&t.opts, t.comparers, t.defaultComparer)
 	t.lsm = newLSM(&t.opts, t.comparers)
 	t.manifest = newManifest(&t.opts, t.comparers)
 	t.sstable = newSSTable(&t.opts, t.comparers, t.mergers)
-	t.wal = newWAL(&t.opts, t.comparers)
+	t.wal = newWAL(&t.opts, t.comparers, t.defaultComparer)
 	t.Commands = []*cobra.Command{
 		t.db.Root,
 		t.find.Root,
@@ -80,21 +129,6 @@ func New() *T {
 		cache.Unref()
 	})
 	return t
-}
-
-// RegisterComparer registers a comparer for use by the introspection tools.
-func (t *T) RegisterComparer(c *Comparer) {
-	t.comparers[c.Name] = c
-}
-
-// RegisterFilter registers a filter policy for use by the introspection tools.
-func (t *T) RegisterFilter(f FilterPolicy) {
-	t.opts.Filters[f.Name()] = f
-}
-
-// RegisterMerger registers a merger for use by the introspection tools.
-func (t *T) RegisterMerger(m *Merger) {
-	t.mergers[m.Name] = m
 }
 
 // setFS sets the filesystem implementation to use by the introspection tools.
