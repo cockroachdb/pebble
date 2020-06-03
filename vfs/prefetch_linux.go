53c0727f803b52c736c862e417964ae83ab28fa0
Bilal Akhtar <bilal@cockroachlabs.com>
2020-06-03 17:16:43 -0400

    vfs: Add vfs.Prefetch, use it for reading ahead data blocks
    For sequential-like IO workload where we read
    data blocks one after the other in quick succession,
    signalling the OS to asynchronously bring them to
    cache in advance can deliver significant savings in IOPS
    dispatched. In IOPS-bound workloads such as backup on
    an EBS disk, this delivers a 3x speedup. Presumably
    aggregate queries and compactions will be faster
    as well, though this hasn't been benchmarked in
    practice yet.
    
    This change maintains a counter for the number of data
    block reads performed in a singleLevelIterator, and
    when that count exceeds 2, a readahead system
    call is made on Linux. RocksDB has almost
    the exact same behaviour, including the same
    min/max readahead sizes and read count thresholds.
    
    Will address https://github.com/cockroachdb/cockroach/issues/49710
    when it lands in cockraoch.

---
+++ vfs/prefetch_linux.go
@@ -0,0 +1,23 @@
+// Copyright 2020 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+// +build linux
+
+package vfs
+
+import "syscall"
+
+// Prefetch signals the OS (on supported platforms) to fetch the next size
+// bytes in file after offset into cache. Any subsequent reads in that range
+// will not issue disk IO.
+func Prefetch(file File, offset uint64, size uint64) error {
+	type fd interface {
+		Fd() uintptr
+	}
+	if f, ok := file.(fd); ok {
+		_, _, err := syscall.Syscall(syscall.SYS_READAHEAD, uintptr(f.Fd()), uintptr(offset), uintptr(size))
+		return err
+	}
+	return nil
+}
