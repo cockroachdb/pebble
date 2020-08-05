71875f86285ddd48f1a2f2c7be60ad1dd04a893d
Jackson Owens <jackson@cockroachlabs.com>
2020-08-05 13:40:35 -0400

    use InternalKey

--- internal/manifest/btree.go
+++ internal/manifest/btree.go
@@ -2,7 +2,7 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-package generic
+package manifest
 
 import (
 	"strings"
@@ -13,16 +13,13 @@ import (
 
 type example struct {
 	id  uint64
-	key exampleKey
+	key InternalKey
 }
 
 // Methods required by generic contract.
-func (ex *example) ID() uint64      { return ex.id }
-func (ex *example) Key() []byte     { return []byte(ex.key.Key) }
-func (ex *example) String() string  { return ex.key.String() }
-func (ex *example) New() *example   { return new(example) }
-func (ex *example) SetID(v uint64)  { ex.id = v }
-func (ex *example) SetKey(v []byte) { ex.key.Key = string(v) }
+func (ex *example) String() string       { return ex.key.String() }
+func (ex *example) New() *example        { return new(example) }
+func (ex *example) SetKey(k InternalKey) { ex.key = k }
 
 // nilT is a nil instance of the Template type.
 var nilT *example
