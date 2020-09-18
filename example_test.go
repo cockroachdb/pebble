6a1f364ee831d2492cddb6f1cd6e420a0015d971
Peter Mattis <petermattis@gmail.com>
2020-09-18 08:07:45 -0400

    db: add Example_PrefixIteration
    Add an example showing how to iterate over the keys containing a common
    prefix. Fix the example of this same pattern in the comments above
    `Iterator.SeekPrefixGE`.
    
    Fix #907

--- example_test.go
+++ example_test.go
@@ -9,10 +9,11 @@ import (
 	"log"
 
 	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/vfs"
 )
 
 func Example() {
-	db, err := pebble.Open("demo", &pebble.Options{})
+	db, err := pebble.Open("", &pebble.Options{FS: vfs.NewMem()})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,5 +33,52 @@ func Example() {
 		log.Fatal(err)
 	}
 	// Output:
+	// hello world
+}
+
+func Example_PrefixIteration() {
+	db, err := pebble.Open("", &pebble.Options{FS: vfs.NewMem()})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	keyUpperBound := func(b []byte) []byte {
+		end := make([]byte, len(b))
+		copy(end, b)
+		for i := len(end) - 1; i >= 0; i-- {
+			end[i] = end[i] + 1
+			if end[i] != 0 {
+				return end[:i+1]
+			}
+		}
+		return nil // no upper-bound
+	}
+
+	prefixIterOptions := func(prefix []byte) *pebble.IterOptions {
+		return &pebble.IterOptions{
+			LowerBound: prefix,
+			UpperBound: keyUpperBound(prefix),
+		}
+	}
+
+	keys := []string{"hello", "world", "hello world"}
+	for _, key := range keys {
+		if err := db.Set([]byte(key), nil, pebble.Sync); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	iter := db.NewIter(prefixIterOptions([]byte("hello")))
+	for iter.First(); iter.Valid(); iter.Next() {
+		fmt.Printf("%s\n", iter.Key())
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+	// Output:
+	// hello
 	// hello world
 }
