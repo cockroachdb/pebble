3ff2630a7f26412f0826fcead702b65017f4a36b
Jackson Owens <jackson@cockroachlabs.com>
2020-11-13 13:58:21 -0500

    vfs: wrap defaultFS errors with a stack trace
    Many filesystem errors don't get surfaced immediately and instead are
    cached on an iterator to be exposed by a call to `Error`. This can make
    it difficult to ascertain the source of an I/O error (for example
    cockroachdb/cockroach#55351). This commit updates the vfs.Default
    fileystem to wrap all of its errors with a current stack trace.
    Filesystem errors are expected to be relatively infrequent, so this
    wrapping is not expected to hinder performance.

---
+++ vfs/errors_unix_test.go
@@ -0,0 +1,20 @@
+// Copyright 2020 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+// +build darwin dragonfly freebsd linux openbsd
+
+package vfs
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/errors"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/unix"
+)
+
+func TestIsNoSpaceError(t *testing.T) {
+	err := errors.WithStack(unix.ENOSPC)
+	require.True(t, IsNoSpaceError(err))
+}
