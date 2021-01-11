fa41b7980308a39ad30adc841a67bd736ec5e693
Bilal Akhtar <bilal@cockroachlabs.com>
2021-01-11 12:11:20 -0500

    vfs: Add fdFileWrapper to re-implement Fd() method on file wrappers
    Pebble relies on the existence of an `Fd() uintptr` method on file
    handles for some optimizations (eg. WAL preallocation, Prefetch).
    However, two of Pebble's own file wrappers used in production,
    namely diskHealthCheckingFile and syncingFile, "hide" this
    method even if the underlying os.File had it.
    
    This change adds another type, fdFileWrapper, that embeds an "outer"
    vfs.File while also reimplementing an Fd() method that bypasses
    that outer vfs.File and passes through to the inner file.
    
    Any file wrappers external to Pebble should call WithFd()
    in a similar way to ensure they don't hide the Fd() method
    unintentionally too.
    
    Fixes #1047.

---
+++ vfs/fd_test.go
@@ -0,0 +1,37 @@
+// Copyright 2021 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package vfs
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileWrappersHaveFd(t *testing.T) {
+	// Use the real filesystem so that we can test vfs.Default, which returns
+	// files with Fd().
+	tmpf, err := ioutil.TempFile("", "pebble-db-fd-file")
+	require.NoError(t, err)
+	filename := tmpf.Name()
+	defer os.Remove(filename)
+
+	// File wrapper case 1: Check if diskHealthCheckingFile has Fd().
+	fs2 := WithDiskHealthChecks(Default, 10 * time.Second, func(s string, duration time.Duration) {})
+	f2, err := fs2.Open(filename)
+	require.NoError(t, err)
+	if _, ok := f2.(fdGetter); !ok {
+		t.Fatal("expected diskHealthCheckingFile to export Fd() method")
+	}
+	// File wrapper case 2: Check if syncingFile has Fd().
+	f3 := NewSyncingFile(f2, SyncingFileOptions{BytesPerSync: 8 << 10 /* 8 KB */})
+	if _, ok := f3.(fdGetter); !ok {
+		t.Fatal("expected syncingFile to export Fd() method")
+	}
+	require.NoError(t, f2.Close())
+}
