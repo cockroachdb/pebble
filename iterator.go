cd94c15faeb97a48baaf6e1ecf2501f04ae8956f
Mike Faraponov <mikefaraponov@gmail.com>
2021-03-22 13:34:41 +0200

    db: reduce getInternal allocations of getIter

--- iterator.go
+++ iterator.go
@@ -95,6 +95,7 @@ type Iterator struct {
 	iterKey             *InternalKey
 	iterValue           []byte
 	alloc               *iterAlloc
+	getIterAlloc        *getIterAlloc
 	prefixOrFullSeekKey []byte
 	readSampling        readSampling
 
@@ -1019,10 +1020,11 @@ func (i *Iterator) Close() error {
 		i.valueCloser = nil
 	}
 
+	const maxKeyBufCacheSize = 4 << 10 // 4 KB
+
 	if alloc := i.alloc; alloc != nil {
 		// Avoid caching the key buf if it is overly large. The constant is fairly
 		// arbitrary.
-		const maxKeyBufCacheSize = 4 << 10 // 4 KB
 		if cap(i.keyBuf) >= maxKeyBufCacheSize {
 			alloc.keyBuf = nil
 		} else {
@@ -1035,6 +1037,14 @@ func (i *Iterator) Close() error {
 		}
 		*i = Iterator{}
 		iterAllocPool.Put(alloc)
+	} else if alloc := i.getIterAlloc; alloc != nil {
+		if cap(i.keyBuf) >= maxKeyBufCacheSize {
+			alloc.keyBuf = nil
+		} else {
+			alloc.keyBuf = i.keyBuf
+		}
+		*i = Iterator{}
+		getIterAllocPool.Put(alloc)
 	}
 	return err
 }
@@ -1121,18 +1131,19 @@ func (i *Iterator) Clone() (*Iterator, error) {
 	buf := iterAllocPool.Get().(*iterAlloc)
 	dbi := &buf.dbi
 	*dbi = Iterator{
-		opts:      i.opts,
-		alloc:     buf,
-		cmp:       i.cmp,
-		equal:     i.equal,
-		iter:      &buf.merging,
-		merge:     i.merge,
-		split:     i.split,
-		readState: readState,
-		keyBuf:    buf.keyBuf,
-		batch:     i.batch,
-		newIters:  i.newIters,
-		seqNum:    i.seqNum,
+		opts:                i.opts,
+		alloc:               buf,
+		cmp:                 i.cmp,
+		equal:               i.equal,
+		iter:                &buf.merging,
+		merge:               i.merge,
+		split:               i.split,
+		readState:           readState,
+		keyBuf:              buf.keyBuf,
+		prefixOrFullSeekKey: buf.prefixOrFullSeekKey,
+		batch:               i.batch,
+		newIters:            i.newIters,
+		seqNum:              i.seqNum,
 	}
 	return finishInitializingIter(buf), nil
 }
