a49897762d7bb98c373051f5f992f90872072af7
Peter Mattis <petermattis@gmail.com>
2021-05-10 08:45:35 -0400

    sstable: fix sstable.Writer key order checks for nil keys
    Fix the `sstable.Writer.addPoint` key order check which was incorrectly
    firing when a nil user key was added to the DB. The problem was that
    comparing the zero value of `w.meta.LargestPoint` vs a nil user-key was
    simply invalid. Instead, we need to avoid this check when a point record
    has never been added to the sstable, similar to what is done in
    `sstable.Writer.addTombstone`.
    
    Also fix the setting of `w.meta.SmallestPoint` to ensure that
    `w.meta.SmallestPoint.UserKey` is always non-nil after a point record
    has been added. This is necessary for `WriterMetadata.Smallest` to work
    correctly so that the sstable bounds can be set correctly if the
    smallest record in an sstable as a nil or zero-length user key.
    
    Fixes #1133

--- sstable/writer.go
+++ sstable/writer.go
@@ -220,7 +220,7 @@ func (w *Writer) Add(key InternalKey, value []byte) error {
 }
 
 func (w *Writer) addPoint(key InternalKey, value []byte) error {
-	if !w.disableKeyOrderChecks {
+	if !w.disableKeyOrderChecks && w.meta.LargestPoint.UserKey != nil {
 		// TODO(peter): Manually inlined version of base.InternalCompare(). This is
 		// 3.5% faster on BenchmarkWriter on go1.13. Remove if go1.14 or future
 		// versions show this to not be a performance win.
@@ -246,8 +246,14 @@ func (w *Writer) addPoint(key InternalKey, value []byte) error {
 	w.block.add(key, value)
 
 	w.meta.updateSeqNum(key.SeqNum())
-	if w.props.NumEntries == 0 {
+	if w.meta.SmallestPoint.UserKey == nil {
 		w.meta.SmallestPoint = key.Clone()
+		// Ensure that SmallestPoint.UserKey is not nil. This is required by
+		// WriterMetadata.Smallest in order to distinguish between an unset
+		// SmallestPoint and a zero-length one.
+		if w.meta.SmallestPoint.UserKey == nil {
+			w.meta.SmallestPoint.UserKey = make([]byte, 0)
+		}
 	}
 	// block.curKey contains the most recently added key to the block.
 	w.meta.LargestPoint.UserKey = w.block.curKey[:len(w.block.curKey)-8]
