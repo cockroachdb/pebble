a31338b00bba1d5491ba961c92d8dfdaeb3465b5
Nick Travers <travers@cockroachlabs.com>
2021-09-09 13:38:32 -0700

    internal/base: add SetWithDelete key kind
    Currently, SingleDelete is only guaranteed to behave correctly if the
    key has been Set _at most once_. Undefined behavior results outside of
    this requirement.
    
    cockroachdb/cockroach#69891 identified a sequence of operations that can
    result in non-deterministic, undefined behavior, due to the way in which
    early intents are removed and cause the DB to "forget" whether a key has
    been Set at most once, violating the key assumption for SingleDeletes.
    
    Consider the following sequence (where `=>` implies "happens-before"):
    
    ```
    a.SET.1 => a.(DEL|SINGLEDEL).2 => a.SET.3 => a.SINGLEDEL.4
    ```
    
    If the middle two operations meet in a single compaction, the
    intermediate sequence would become:
    
    ```
    a.SET.1 => a.SET.3 => a.SINGLEDEL.4
    ```
    
    A second compaction of this intermediate sequence would result in just
    `a.SET.1`, as `a.SET.3` and `a.SINGLEDEL.4` will be elided. This
    incorrectly "resurrects" `a.SET.1`.
    
    This undefined behavior was demonstrated in #1252.
    
    A solution, outlined in #1255, works as follows:
    
    - A new key kind, SetWithDelete, represents a Set that has potentially
      consumed a Delete or SingleDelete in a compaction.
    - The existing Set key kind now represents a promise that it has not
      consumed a Delete or SingleDelete. This is what will be written when a
      caller uses `pebble.Writer.Set`.
    - A `SET => SINGLEDEL` continues to cause both to be consumed.
    - A `SETWITHDEL => SINGLEDEL` is "upgraded" into a regular `DEL`,
      reflecting the fact that there may be deleted entries under the
      SetWithDelete that should not be resurrected.
    
    This patch introduces the new key kind and implements the logic
    described above, required for writing out these new keys during a
    compaction to improve the determinism of the `SET => SINGLEDEL`
    semantics.
    
    This new record type is gated behind a format major version, to ensure
    that older DBs continue to use the existing semantics, while newer DBs
    will make use of the new semantics during compactions.
    
    This change is one-way (i.e. once migrated, an older version of the DB
    cannot read sstables written by a newer version).
    
    Addresses #1255, cockroachdb/cockroach#69891.

--- compaction_iter_test.go
+++ compaction_iter_test.go
@@ -79,7 +79,16 @@ func TestCompactionIter(t *testing.T) {
 	var elideTombstones bool
 	var allowZeroSeqnum bool
 
-	newIter := func() *compactionIter {
+	// The input to the data-driven test is dependent on the format major
+	// version we are testing against.
+	fileFunc := func(formatVersion FormatMajorVersion) string {
+		if formatVersion < FormatSetWithDelete {
+			return "testdata/compaction_iter"
+		}
+		return "testdata/compaction_iter_set_with_del"
+	}
+
+	newIter := func(formatVersion FormatMajorVersion) *compactionIter {
 		// To adhere to the existing assumption that range deletion blocks in
 		// SSTables are not released while iterating, and therefore not
 		// susceptible to use-after-free bugs, we skip the zeroing of
@@ -105,96 +114,113 @@ func TestCompactionIter(t *testing.T) {
 			func(_, _ []byte) bool {
 				return elideTombstones
 			},
+			formatVersion,
 		)
 	}
 
-	datadriven.RunTest(t, "testdata/compaction_iter", func(d *datadriven.TestData) string {
-		switch d.Cmd {
-		case "define":
-			keys = keys[:0]
-			vals = vals[:0]
-			for _, key := range strings.Split(d.Input, "\n") {
-				j := strings.Index(key, ":")
-				keys = append(keys, base.ParseInternalKey(key[:j]))
-				vals = append(vals, []byte(key[j+1:]))
-			}
-			return ""
+	runTest := func(t *testing.T, formatVersion FormatMajorVersion) {
+		datadriven.RunTest(t, fileFunc(formatVersion), func(d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "define":
+				keys = keys[:0]
+				vals = vals[:0]
+				for _, key := range strings.Split(d.Input, "\n") {
+					j := strings.Index(key, ":")
+					keys = append(keys, base.ParseInternalKey(key[:j]))
+					vals = append(vals, []byte(key[j+1:]))
+				}
+				return ""
 
-		case "iter":
-			snapshots = snapshots[:0]
-			elideTombstones = false
-			allowZeroSeqnum = false
-			for _, arg := range d.CmdArgs {
-				switch arg.Key {
-				case "snapshots":
-					for _, val := range arg.Vals {
-						seqNum, err := strconv.Atoi(val)
+			case "iter":
+				snapshots = snapshots[:0]
+				elideTombstones = false
+				allowZeroSeqnum = false
+				for _, arg := range d.CmdArgs {
+					switch arg.Key {
+					case "snapshots":
+						for _, val := range arg.Vals {
+							seqNum, err := strconv.Atoi(val)
+							if err != nil {
+								return err.Error()
+							}
+							snapshots = append(snapshots, uint64(seqNum))
+						}
+					case "elide-tombstones":
+						var err error
+						elideTombstones, err = strconv.ParseBool(arg.Vals[0])
 						if err != nil {
 							return err.Error()
 						}
-						snapshots = append(snapshots, uint64(seqNum))
+					case "allow-zero-seqnum":
+						var err error
+						allowZeroSeqnum, err = strconv.ParseBool(arg.Vals[0])
+						if err != nil {
+							return err.Error()
+						}
+					default:
+						return fmt.Sprintf("%s: unknown arg: %s", d.Cmd, arg.Key)
 					}
-				case "elide-tombstones":
-					var err error
-					elideTombstones, err = strconv.ParseBool(arg.Vals[0])
-					if err != nil {
-						return err.Error()
-					}
-				case "allow-zero-seqnum":
-					var err error
-					allowZeroSeqnum, err = strconv.ParseBool(arg.Vals[0])
-					if err != nil {
-						return err.Error()
-					}
-				default:
-					return fmt.Sprintf("%s: unknown arg: %s", d.Cmd, arg.Key)
 				}
-			}
-			sort.Slice(snapshots, func(i, j int) bool {
-				return snapshots[i] < snapshots[j]
-			})
+				sort.Slice(snapshots, func(i, j int) bool {
+					return snapshots[i] < snapshots[j]
+				})
 
-			iter := newIter()
-			var b bytes.Buffer
-			for _, line := range strings.Split(d.Input, "\n") {
-				parts := strings.Fields(line)
-				if len(parts) == 0 {
-					continue
-				}
-				switch parts[0] {
-				case "first":
-					iter.First()
-				case "next":
-					iter.Next()
-				case "tombstones":
-					var key []byte
-					if len(parts) == 2 {
-						key = []byte(parts[1])
+				iter := newIter(formatVersion)
+				var b bytes.Buffer
+				for _, line := range strings.Split(d.Input, "\n") {
+					parts := strings.Fields(line)
+					if len(parts) == 0 {
+						continue
 					}
-					for _, v := range iter.Tombstones(key, false) {
-						fmt.Fprintf(&b, "%s-%s#%d\n",
-							v.Start.UserKey, v.End, v.Start.SeqNum())
+					switch parts[0] {
+					case "first":
+						iter.First()
+					case "next":
+						iter.Next()
+					case "tombstones":
+						var key []byte
+						if len(parts) == 2 {
+							key = []byte(parts[1])
+						}
+						for _, v := range iter.Tombstones(key, false) {
+							fmt.Fprintf(&b, "%s-%s#%d\n",
+								v.Start.UserKey, v.End, v.Start.SeqNum())
+						}
+						fmt.Fprintf(&b, ".\n")
+						continue
+					default:
+						return fmt.Sprintf("unknown op: %s", parts[0])
 					}
-					fmt.Fprintf(&b, ".\n")
-					continue
-				default:
-					return fmt.Sprintf("unknown op: %s", parts[0])
-				}
-				if iter.Valid() {
-					fmt.Fprintf(&b, "%s:%s\n", iter.Key(), iter.Value())
-					if iter.Key().Kind() == InternalKeyKindRangeDelete {
-						iter.rangeDelFrag.Add(iter.cloneKey(iter.Key()), iter.Value())
+					if iter.Valid() {
+						fmt.Fprintf(&b, "%s:%s\n", iter.Key(), iter.Value())
+						if iter.Key().Kind() == InternalKeyKindRangeDelete {
+							iter.rangeDelFrag.Add(iter.cloneKey(iter.Key()), iter.Value())
+						}
+					} else if err := iter.Error(); err != nil {
+						fmt.Fprintf(&b, "err=%v\n", err)
+					} else {
+						fmt.Fprintf(&b, ".\n")
 					}
-				} else if err := iter.Error(); err != nil {
-					fmt.Fprintf(&b, "err=%v\n", err)
-				} else {
-					fmt.Fprintf(&b, ".\n")
 				}
-			}
-			return b.String()
+				return b.String()
 
-		default:
-			return fmt.Sprintf("unknown command: %s", d.Cmd)
-		}
-	})
+			default:
+				return fmt.Sprintf("unknown command: %s", d.Cmd)
+			}
+		})
+	}
+
+	// Rather than testing against all format version, we test against the
+	// significant boundaries.
+	formatVersions := []FormatMajorVersion{
+		FormatMostCompatible,
+		FormatSetWithDelete - 1,
+		FormatSetWithDelete,
+		FormatNewest,
+	}
+	for _, formatVersion := range formatVersions {
+		t.Run(fmt.Sprintf("version-%s", formatVersion), func(t *testing.T) {
+			runTest(t, formatVersion)
+		})
+	}
 }
