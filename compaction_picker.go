ff1ac5c7c5ebfb0c8aa427f40c9e11b414641a0a
Arjun Nair <nair@cockroachlabs.com>
2021-10-05 18:10:35 -0400

    pebble: add additional mechanism to schedule read compactions
    If the compaction picker tries to pick a read compaction, and returns
    nil for any reason, then maybeScheduleCompactionPicker, won't schedule
    additional compactions. In a read heavy workload, flushes might not happen
    frequently enough to schedule additional read compactions. So, we need iterators
    to schedule these compactions, when new compactions are added.
    
    This pr will become more relevant when we merge https://github.com/cockroachdb/pebble/pull/1200,
    since #1200 increases the probability of the read compactions picker returning nil.
    
    The other benefit of the pr is that it gets rid of the ridiculous variance in our ycsb/C/64
    benchmarks.
    ```
    name              old ops/sec  new ops/sec  delta
    ycsb/C/values=64   1.69M ±25%   2.82M ± 1%  +67.19%  (p=0.008 n=5+5)
    ycsb/B/values=64    844k ± 6%   1134k ± 1%  +34.37%  (p=0.008 n=5+5)
    
    name              old read     new read     delta
    ycsb/C/values=64   2.73G ± 6%   2.84G ± 4%     ~     (p=0.151 n=5+5)
    ycsb/B/values=64   30.4G ±10%   38.9G ±27%     ~     (p=0.056 n=5+5)
    
    name              old write    new write    delta
    ycsb/C/values=64   2.65G ± 6%   2.75G ± 4%     ~     (p=0.095 n=5+5)
    ycsb/B/values=64   33.6G ± 9%   43.2G ±24%  +28.77%  (p=0.032 n=5+5)
    
    name              old r-amp    new r-amp    delta
    ycsb/C/values=64    2.19 ± 2%    1.18 ± 2%  -45.97%  (p=0.000 n=4+5)
    ycsb/B/values=64    3.81 ± 3%    3.68 ± 2%   -3.36%  (p=0.032 n=5+5)
    
    name              old w-amp    new w-amp    delta
    ycsb/C/values=64    0.00         0.00          ~     (all equal)
    ycsb/B/values=64    10.8 ±10%    10.3 ±25%     ~     (p=0.841 n=5+5)
    ```

--- compaction_picker.go
+++ compaction_picker.go
@@ -43,6 +43,7 @@ type compactionPicker interface {
 type readCompactionEnv struct {
 	readCompactions *[]readCompaction
 	flushing        bool
+	rescheduleReadCompaction              *bool
 }
 
 // Information about in-progress compactions provided to the compaction picker. These are used to
@@ -1022,8 +1023,15 @@ func (p *compactionPickerByScore) pickAuto(env compactionEnv) (pc *pickedCompact
 		return pc
 	}
 
+	//nolint
 	if pc := p.pickReadTriggeredCompaction(env); pc != nil {
 		return pc
+	} else {
+		// We won't be scheduling a read compaction, and in read
+		// heavy workloads, compactions won't be scheduled during flushes,
+		// so we need to signal to the iterator to schedule a compaction
+		// when it adds compactions to the read compaction queue.
+		*env.readCompactionEnv.rescheduleReadCompaction = true 
 	}
 
 	return nil
