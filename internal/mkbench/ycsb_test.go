6344a8b4d83b2222276614afd5bf0b7cc35b959e
Nick Travers <travers@cockroachlabs.com>
2021-11-03 20:39:52 -0700

    mkbench: minor improvements / refactoring / cleanup
    Ahead of a much larger change that is intended to add a parser for
    write-throughput benchmark data, split out some smaller tweaks into a
    separate patch to reduce noise in the follow up patch.
    
    Use cobra for flags - this allows for using `mkbench` in a
    subcommand-style  when there are multiple benchmark parsers (soon to be
    two). In the absence of a subcommand, run the YCSB benchmark parser. As
    a sanity check, running the following command (with no args / flags)
    does not result in a diff to the file:
    
    ```bash
    pushd ./internal/mkbench/testdata
      # Without flags.
      go run ../
    
      # The same command with flags.
      go run ../ --dir --in data.js --out data.js
    popd
    ```
    
    Split out test helpers and utility methods into separate files.
    
    Improve the diff output in the `mkbench` tests, making use of the
    `difflib` library that is used elsewhere.
    
    Prefix various functions with `ycsb` to avoid name collisions with
    similar functions that will be added in a subsequent patch.
    
    Filter YCSB benchmark data in the filesystem walker - this avoids having
    to read and iterate through non-YCSB data.
    
    Add copyright notices to files in which they were previously absent.
    
    Append newlines to generated files - a trivial cosmetic change.

--- internal/mkbench/ycsb_test.go
+++ internal/mkbench/ycsb_test.go
@@ -1,17 +1,14 @@
+// Copyright 2021 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/cockroachdb/errors"
-	"github.com/cockroachdb/errors/oserror"
-	"github.com/kr/pretty"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,17 +17,17 @@ const (
 	dataJSPath  = "./testdata/data.js"
 )
 
-func Test_FromScratch(t *testing.T) {
+func TestParseYCSB_FromScratch(t *testing.T) {
 	// Write out a new data.js file from the input data.
 	fPath := filepath.Join(t.TempDir(), "data.js")
-	ParseYCSB(dataDirPath, fPath, fPath)
+	parseYCSB(dataDirPath, fPath, fPath)
 
 	// Confirm the two data.js files are now equal.
 	err := filesEqual(dataJSPath, fPath)
 	require.NoError(t, err)
 }
 
-func Test_Existing(t *testing.T) {
+func TestYCSB_Existing(t *testing.T) {
 	// Set up the test directory.
 	testDir := t.TempDir()
 	newDataDir := filepath.Join(testDir, "data")
@@ -43,78 +40,16 @@ func Test_Existing(t *testing.T) {
 	require.NoError(t, err)
 
 	// Construct the data.js file on the test data with a single day removed.
-	ParseYCSB(newDataDir, newDataJS, newDataJS)
+	parseYCSB(newDataDir, newDataJS, newDataJS)
 
 	// Confirm the two data.js files are not equal.
 	err = filesEqual(dataJSPath, newDataJS)
 	require.Error(t, err)
 
 	// Re-construct the data.js file with the full set of data.
-	ParseYCSB(dataDirPath, dataJSPath, newDataJS)
+	parseYCSB(dataDirPath, dataJSPath, newDataJS)
 
 	// Confirm the two data.js files are now equal.
 	err = filesEqual(dataJSPath, newDataJS)
 	require.NoError(t, err)
-}
-
-// diff returns the diff between contents of a and b.
-func filesEqual(a, b string) error {
-	aBytes, err := ioutil.ReadFile(a)
-	if err != nil {
-		return err
-	}
-	bBytes, err := ioutil.ReadFile(b)
-	if err != nil {
-		return err
-	}
-
-	// Normalize newlines.
-	aBytes = bytes.Replace(aBytes, []byte{13, 10} /* \r\n */, []byte{10} /* \n */, -1)
-	bBytes = bytes.Replace(bBytes, []byte{13, 10}, []byte{10}, -1)
-
-	d := pretty.Diff(string(aBytes), string(bBytes))
-	if d != nil {
-		return errors.Newf("a != b; diff = %s", strings.Join(d, "\n"))
-	}
-
-	return nil
-}
-
-// copyDir recursively copies the fromPath to toPath, excluding certain paths.
-func copyDir(fromPath, toPath string) error {
-	return filepath.Walk(fromPath, func(path string, info os.FileInfo, e error) error {
-		if e != nil {
-			return e
-		}
-
-		rel, err := filepath.Rel(fromPath, path)
-		if err != nil {
-			return err
-		}
-
-		// Preserve the directory structure.
-		if info.IsDir() {
-			err := os.Mkdir(filepath.Join(toPath, rel), 0700)
-			if err != nil && !oserror.IsNotExist(err) {
-				return err
-			}
-			return nil
-		}
-
-		// Copy files.
-		fIn, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer func() { _ = fIn.Close() }()
-
-		fOut, err := os.OpenFile(filepath.Join(toPath, rel), os.O_CREATE|os.O_WRONLY, 0700)
-		if err != nil {
-			return err
-		}
-		defer func() { _ = fOut.Close() }()
-
-		_, err = io.Copy(fOut, fIn)
-		return err
-	})
 }
