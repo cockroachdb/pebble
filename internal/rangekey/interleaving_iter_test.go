2f2ab0e1b2e1196592c01255cf3e1eef4b502b0b
Jackson Owens <jackson_owens@alumni.brown.edu>
2021-12-07 19:04:01 -0500

    internal/rangekey: add InterleavingIter
    Add an InterleavingIter implementation of the InternalIterator interface that
    wraps another InternalIterator and a `*rangekey.Iter`, interleaving the two.
    An `InterleavingIter` steps through both a point iterator and a range key
    iterator in tandem. At any point, a caller may query its `HasRangeKey()`,
    `RangeKeyBounds()` and `RangeKeys()` methods to retrieve information about the
    range keys covering the current iterator position, if any.  Additionally, when
    an interleaving iterator passes over the inclusive start boundary of a range
    key, it returns a key with the special InternalKey trailer
    `InternalKeyBoundaryRangeKey`. This key (with the RangeKeySet key-kind and an
    infinite sequence number) indicates the beginning of a range key.
    
    Eventually, the `*pebble.Iterator` will use an `InterleavingIter` when
    configured to surface range keys.

---
+++ internal/rangekey/interleaving_iter_test.go
@@ -0,0 +1,216 @@
+// Copyright 2021 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package rangekey
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/datadriven"
+	"github.com/cockroachdb/pebble/internal/keyspan"
+	"github.com/cockroachdb/pebble/internal/testkeys"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInterleavingIter(t *testing.T) {
+	cmp := testkeys.Comparer.Compare
+	var rangeKeyIter Iter
+	var pointIter pointIterator
+	var iter InterleavingIter
+	var buf bytes.Buffer
+
+	formatKey := func(k *base.InternalKey, v []byte) {
+		if k == nil {
+			fmt.Fprint(&buf, ".")
+			return
+		}
+		fmt.Fprintf(&buf, "PointKey: %s\nRangeKey: ", k.String())
+		if iter.HasRangeKey() {
+			start, end := iter.RangeKeyBounds()
+			fmt.Fprintf(&buf, "[%s, %s)", start, end)
+			formatRangeKeyItems(&buf, iter.RangeKeys())
+		} else {
+			fmt.Fprint(&buf, ".")
+		}
+		fmt.Fprint(&buf, "\n-")
+	}
+
+	datadriven.RunTest(t, "testdata/interleaving_iter", func(td *datadriven.TestData) string {
+		buf.Reset()
+		switch td.Cmd {
+		case "define-rangekeys":
+			var spans []keyspan.Span
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				startKey, value := Parse(line)
+				endKey, v, ok := DecodeEndKey(startKey.Kind(), value)
+				require.True(t, ok)
+				spans = append(spans, keyspan.Span{
+					Start: startKey,
+					End:   endKey,
+					Value: v,
+				})
+			}
+			rangeKeyIter.Init(cmp, testkeys.Comparer.FormatKey, keyspan.NewIter(cmp, spans))
+			iter.Init(&pointIter, &rangeKeyIter)
+			return "OK"
+		case "define-pointkeys":
+			var points []base.InternalKey
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				points = append(points, base.ParseInternalKey(line))
+			}
+			pointIter = pointIterator{cmp: cmp, keys: points}
+			iter.Init(&pointIter, &rangeKeyIter)
+			return "OK"
+		case "iter":
+			buf.Reset()
+			// Clear any previous bounds.
+			iter.SetBounds(nil, nil)
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				bufLen := buf.Len()
+				line = strings.TrimSpace(line)
+				i := strings.IndexByte(line, ' ')
+				iterCmd := line
+				if i > 0 {
+					iterCmd = string(line[:i])
+				}
+				switch iterCmd {
+				case "first":
+					formatKey(iter.First())
+				case "last":
+					formatKey(iter.Last())
+				case "next":
+					formatKey(iter.Next())
+				case "prev":
+					formatKey(iter.Prev())
+				case "seek-ge":
+					formatKey(iter.SeekGE([]byte(strings.TrimSpace(line[i:]))))
+				case "seek-lt":
+					formatKey(iter.SeekLT([]byte(strings.TrimSpace(line[i:]))))
+				case "set-bounds":
+					bounds := strings.Fields(line[i:])
+					if len(bounds) != 2 {
+						return fmt.Sprintf("set-bounds expects 2 bounds, got %d", len(bounds))
+					}
+					l, u := []byte(bounds[0]), []byte(bounds[1])
+					if bounds[0] == "." {
+						l = nil
+					}
+					if bounds[1] == "." {
+						u = nil
+					}
+					iter.SetBounds(l, u)
+				default:
+					return fmt.Sprintf("unrecognized iter command %q", iterCmd)
+				}
+				require.NoError(t, iter.Error())
+				if buf.Len() > bufLen {
+					fmt.Fprintln(&buf)
+				}
+			}
+			return strings.TrimSpace(buf.String())
+		default:
+			return fmt.Sprintf("unrecognized command %q", td.Cmd)
+		}
+	})
+}
+
+type pointIterator struct {
+	cmp   base.Compare
+	keys  []base.InternalKey
+	lower []byte
+	upper []byte
+	index int
+}
+
+var _ base.InternalIterator = &pointIterator{}
+
+func (i *pointIterator) SeekGE(key []byte) (*base.InternalKey, []byte) {
+	i.index = sort.Search(len(i.keys), func(j int) bool {
+		return i.cmp(i.keys[j].UserKey, key) >= 0
+	})
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.upper != nil && i.cmp(i.keys[i.index].UserKey, i.upper) >= 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) SeekPrefixGE(prefix, key []byte, trySeekUsingNext bool) (*base.InternalKey, []byte) {
+	return i.SeekGE(key)
+}
+
+func (i *pointIterator) SeekLT(key []byte) (*base.InternalKey, []byte) {
+	i.index = sort.Search(len(i.keys), func(j int) bool {
+		return i.cmp(i.keys[j].UserKey, key) >= 0
+	})
+	i.index--
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.lower != nil && i.cmp(i.keys[i.index].UserKey, i.lower) < 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) First() (*base.InternalKey, []byte) {
+	i.index = 0
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.upper != nil && i.cmp(i.keys[i.index].UserKey, i.upper) >= 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) Last() (*base.InternalKey, []byte) {
+	i.index = len(i.keys) - 1
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.lower != nil && i.cmp(i.keys[i.index].UserKey, i.lower) < 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) Next() (*base.InternalKey, []byte) {
+	i.index++
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.upper != nil && i.cmp(i.keys[i.index].UserKey, i.upper) >= 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) Prev() (*base.InternalKey, []byte) {
+	i.index--
+	if i.index < 0 || i.index >= len(i.keys) {
+		return nil, nil
+	}
+	if i.lower != nil && i.cmp(i.keys[i.index].UserKey, i.lower) < 0 {
+		return nil, nil
+	}
+	return &i.keys[i.index], nil
+}
+
+func (i *pointIterator) Close() error   { return nil }
+func (i *pointIterator) Error() error   { return nil }
+func (i *pointIterator) String() string { return "test-point-iterator" }
+func (i *pointIterator) SetBounds(lower, upper []byte) {
+	i.lower, i.upper = lower, upper
+}
