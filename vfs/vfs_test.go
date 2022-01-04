a34dca4ea28574d41817618a9fb21c1752dafda2
Andrew Werner <awerner32@gmail.com>
2022-01-04 09:10:30 -0500

    vfs: allow MemFS to be Open()'d at the root directory
    Before this change, a call to Open for "/" would result in `pebble/vfs: empty
    file name` because the root directory has no name. This prevented use of
    vfs.Clone on MemFS at the root of the FS.
    
    This commit also adds a special case to the memNode to return the name "/"
    for its root node. This matches the behavior of the Default FS on the mac
    and linux (maybe elsewhere, I don't know)..

--- vfs/vfs_test.go
+++ vfs/vfs_test.go
@@ -295,3 +295,22 @@ func TestVFSCreateLinkSemantics(t *testing.T) {
 		})
 	}
 }
+
+// TestVFSRootDirName ensures that opening the root directory on both the
+// Default and MemFS works and returns a File which has the name "/".
+func TestVFSRootDirName(t *testing.T) {
+	for _, fs := range []FS{Default, NewMem()} {
+		rootDir, err := fs.Open("/")
+		require.NoError(t, err)
+		fi, err := rootDir.Stat()
+		require.NoError(t, err)
+
+		// Special case the difference between the Default and MemFS name for the
+		// root directory on windows.
+		exp := sep
+		if runtime.GOOS == "windows" && fs == Default {
+			exp = "\\"
+		}
+		require.Equal(t, exp, fi.Name())
+	}
+}
