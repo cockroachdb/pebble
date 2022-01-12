5377f52c8c9c08bc6178444f6ca5731f73e8d74e
Jackson Owens <jackson@cockroachlabs.com>
2022-01-12 18:29:26 -0500

    internal/rangekey: add merging, fragmenting iterator
    Add a rangekey.MergingIter type that implements the keyspan.FragmentIterator
    interface, merging and fragmenting keyspan.FragmentIterators across many
    levels. MergingIter uses a heap to consider the spans between adjacent, unique
    user keys across levels.
    
    In future work, I'd like to explore adapting MergingIter to implement a
    different interface (eg, no longer adhere to FragmentIterator) where each
    positioning method returns the full set of fragments with those bounds. This
    would require the rangekey.Iter type to explicitly depend on MergingIter, but
    would eliminate redundant key comparisons in rangekey.Iter.

---
+++ internal/rangekey/bound_iter_test.go
@@ -0,0 +1,84 @@
+// Copyright 2022 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package rangekey
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/datadriven"
+	"github.com/cockroachdb/pebble/internal/keyspan"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFragmentBoundIterator(t *testing.T) {
+	cmp := base.DefaultComparer.Compare
+	var buf bytes.Buffer
+	var iter fragmentBoundIterator
+
+	formatKey := func(bk boundKey) {
+		if !bk.valid() {
+			fmt.Fprint(&buf, ".")
+			return
+		}
+		fmt.Fprintf(&buf, "%s", bk)
+	}
+
+	datadriven.RunTest(t, "testdata/fragment_bound_iterator", func(td *datadriven.TestData) string {
+		switch td.Cmd {
+		case "define":
+			var spans []keyspan.Span
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				startKey, value := Parse(line)
+				endKey, v, ok := DecodeEndKey(startKey.Kind(), value)
+				require.True(t, ok)
+				spans = append(spans, keyspan.Span{
+					Start: startKey,
+					End:   endKey,
+					Value: v,
+				})
+			}
+			iter.init(keyspan.NewIter(cmp, spans))
+			return "OK"
+		case "iter":
+			buf.Reset()
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				bufLen := buf.Len()
+				line = strings.TrimSpace(line)
+				i := strings.IndexByte(line, ' ')
+				iterCmd := line
+				if i > 0 {
+					iterCmd = string(line[:i])
+				}
+				switch iterCmd {
+				case "first":
+					formatKey(iter.first(cmp))
+				case "last":
+					formatKey(iter.last(cmp))
+				case "next":
+					formatKey(iter.next(cmp))
+				case "prev":
+					formatKey(iter.prev(cmp))
+				case "seek-ge":
+					formatKey(iter.seekGE(cmp, []byte(strings.TrimSpace(line[i:]))))
+				case "seek-lt":
+					formatKey(iter.seekLT(cmp, []byte(strings.TrimSpace(line[i:]))))
+				}
+				if buf.Len() > bufLen {
+					fmt.Fprintln(&buf)
+				}
+			}
+			return strings.TrimSpace(buf.String())
+
+		default:
+			return fmt.Sprintf("unrecognized command %q", td.Cmd)
+		}
+	})
+}
