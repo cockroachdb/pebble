3f8713d3e213b27b24429ef5a2b34b4b62150f9b
Nick Travers <travers@cockroachlabs.com>
2022-01-26 09:21:05 -0800

    tool/logs: various improvements to compaction event parser
    An accumulation of improvements, based on using the tool as part of
    various "real-world" debugging exercises.
    
    **Accept any file as an input.** Previously, filenames were inspected to
    determine how the log lines should be parsed. Rely instead on the
    regular expressions.
    
    **Rework the windowing algorithm.** Previously log lines could be
    grouped into the wrong window based on the start and end timestamps.
    Improve the windowing.
    
    **Group by node, store and job.** Previously events were grouped only by
    job ID. In the event that a store stopped and restarted and logged to
    the same file, duplicate events could be dropped.
    
    **Parse logging context.** The timestamp, node and store fields are
    common to all parsed lines. Add a `logContext` struct that encapsulates
    this common information.
    
    **Update read-amp parsing**. Newer versions of Cockroach print the LSM
    debug as a single log event, with a single timestamp prefix. Update the
    parser to search for the LSM debug log line and rely on the populated
    logging context from previous lines.

--- tool/logs/compaction_test.go
+++ tool/logs/compaction_test.go
@@ -24,11 +24,10 @@ const (
 	compactionEndLine   = `I211215 14:26:56.318543 51831533 3@vendor/github.com/cockroachdb/pebble/compaction.go:1886 ⋮ [n5,pebble,s6] 1216554  [JOB 284925] compacted(default) L2 [442555] (4.2 M) + L3 [445853] (8.4 M) -> L3 [445883 445887] (13 M), in 0.3s, output rate 42 M/s`
 	flushStartLine      = `I211213 16:23:48.903751 21136 3@vendor/github.com/cockroachdb/pebble/event.go:599 ⋮ [n9,pebble,s8] 24 [JOB 10] flushing 2 memtables to L0`
 	flushEndLine        = `I211213 16:23:49.134464 21136 3@vendor/github.com/cockroachdb/pebble/event.go:603 ⋮ [n9,pebble,s8] 26 [JOB 10] flushed 2 memtables to L0 [1535806] (1.3 M), in 0.2s, output rate 5.8 M/s`
-	readAmpLine         = `I211215 14:55:15.802648 155 kv/kvserver/store.go:2668 ⋮ [n5,s6] 109057 +  total     44905   672 G       -   1.2 T   714 G   118 K   215 G    34 K   4.0 T   379 K   2.8 T     514     3.4`
+	readAmpLine         = `  total     31766   188 G       -   257 G   187 G    48 K   3.6 G     744   536 G    49 K   278 G       5     2.1`
 
 	compactionStartNoNodeStoreLine = `I211215 14:26:56.012382 51831533 3@vendor/github.com/cockroachdb/pebble/compaction.go:1845 ⋮ [n?,pebble,s?] 1216510  [JOB 284925] compacting(default) L2 [442555] (4.2 M) + L3 [445853] (8.4 M)`
 	flushStartNoNodeStoreLine      = `I211213 16:23:48.903751 21136 3@vendor/github.com/cockroachdb/pebble/event.go:599 ⋮ [n?,pebble,s?] 24 [JOB 10] flushing 2 memtables to L0`
-	readAmpNoNodeStoreLine         = `I211215 14:55:15.802648 155 kv/kvserver/store.go:2668 ⋮ [n?,s?] 109057 +  total     44905   672 G       -   1.2 T   714 G   118 K   215 G    34 K   4.0 T   379 K   2.8 T     514     3.4`
 )
 
 func TestCompactionLogs_Regex(t *testing.T) {
@@ -79,16 +78,13 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   compactionPattern,
 			line: compactionStartLine,
 			matches: map[int]string{
-				compactionPatternTimestampIdx: "211215 14:26:56.012382",
-				compactionPatternNode:         "5",
-				compactionPatternStore:        "6",
-				compactionPatternJobIdx:       "284925",
-				compactionPatternSuffixIdx:    "ing",
-				compactionPatternTypeIdx:      "default",
-				compactionPatternFromIdx:      "2",
-				compactionPatternToIdx:        "3",
-				compactionPatternDigitIdx:     "8.4",
-				compactionPatternUnitIdx:      "M",
+				compactionPatternJobIdx:    "284925",
+				compactionPatternSuffixIdx: "ing",
+				compactionPatternTypeIdx:   "default",
+				compactionPatternFromIdx:   "2",
+				compactionPatternToIdx:     "3",
+				compactionPatternDigitIdx:  "8.4",
+				compactionPatternUnitIdx:   "M",
 			},
 		},
 		{
@@ -96,16 +92,13 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   compactionPattern,
 			line: compactionStartNoNodeStoreLine,
 			matches: map[int]string{
-				compactionPatternTimestampIdx: "211215 14:26:56.012382",
-				compactionPatternNode:         "?",
-				compactionPatternStore:        "?",
-				compactionPatternJobIdx:       "284925",
-				compactionPatternSuffixIdx:    "ing",
-				compactionPatternTypeIdx:      "default",
-				compactionPatternFromIdx:      "2",
-				compactionPatternToIdx:        "3",
-				compactionPatternDigitIdx:     "8.4",
-				compactionPatternUnitIdx:      "M",
+				compactionPatternJobIdx:    "284925",
+				compactionPatternSuffixIdx: "ing",
+				compactionPatternTypeIdx:   "default",
+				compactionPatternFromIdx:   "2",
+				compactionPatternToIdx:     "3",
+				compactionPatternDigitIdx:  "8.4",
+				compactionPatternUnitIdx:   "M",
 			},
 		},
 		{
@@ -113,16 +106,13 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   compactionPattern,
 			line: compactionEndLine,
 			matches: map[int]string{
-				compactionPatternTimestampIdx: "211215 14:26:56.318543",
-				compactionPatternNode:         "5",
-				compactionPatternStore:        "6",
-				compactionPatternJobIdx:       "284925",
-				compactionPatternSuffixIdx:    "ed",
-				compactionPatternTypeIdx:      "default",
-				compactionPatternFromIdx:      "2",
-				compactionPatternToIdx:        "3",
-				compactionPatternDigitIdx:     "13",
-				compactionPatternUnitIdx:      "M",
+				compactionPatternJobIdx:    "284925",
+				compactionPatternSuffixIdx: "ed",
+				compactionPatternTypeIdx:   "default",
+				compactionPatternFromIdx:   "2",
+				compactionPatternToIdx:     "3",
+				compactionPatternDigitIdx:  "13",
+				compactionPatternUnitIdx:   "M",
 			},
 		},
 		{
@@ -130,13 +120,10 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   flushPattern,
 			line: flushStartLine,
 			matches: map[int]string{
-				flushPatternTimestampIdx: "211213 16:23:48.903751",
-				flushPatternNode:         "9",
-				flushPatternStore:        "8",
-				flushPatternJobIdx:       "10",
-				flushPatternSuffixIdx:    "ing",
-				flushPatternDigitIdx:     "",
-				flushPatternUnitIdx:      "",
+				flushPatternJobIdx:    "10",
+				flushPatternSuffixIdx: "ing",
+				flushPatternDigitIdx:  "",
+				flushPatternUnitIdx:   "",
 			},
 		},
 		{
@@ -144,13 +131,10 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   flushPattern,
 			line: flushStartNoNodeStoreLine,
 			matches: map[int]string{
-				flushPatternTimestampIdx: "211213 16:23:48.903751",
-				flushPatternNode:         "?",
-				flushPatternStore:        "?",
-				flushPatternJobIdx:       "10",
-				flushPatternSuffixIdx:    "ing",
-				flushPatternDigitIdx:     "",
-				flushPatternUnitIdx:      "",
+				flushPatternJobIdx:    "10",
+				flushPatternSuffixIdx: "ing",
+				flushPatternDigitIdx:  "",
+				flushPatternUnitIdx:   "",
 			},
 		},
 		{
@@ -158,35 +142,18 @@ func TestCompactionLogs_Regex(t *testing.T) {
 			re:   flushPattern,
 			line: flushEndLine,
 			matches: map[int]string{
-				flushPatternTimestampIdx: "211213 16:23:49.134464",
-				flushPatternNode:         "9",
-				flushPatternStore:        "8",
-				flushPatternJobIdx:       "10",
-				flushPatternSuffixIdx:    "ed",
-				flushPatternDigitIdx:     "1.3",
-				flushPatternUnitIdx:      "M",
+				flushPatternJobIdx:    "10",
+				flushPatternSuffixIdx: "ed",
+				flushPatternDigitIdx:  "1.3",
+				flushPatternUnitIdx:   "M",
 			},
 		},
 		{
-			name: "read amp",
+			name: "read amp suffix",
 			re:   readAmpPattern,
 			line: readAmpLine,
 			matches: map[int]string{
-				readAmpPatternTimestampIdx: "211215 14:55:15.802648",
-				readAmpPatternNode:         "5",
-				readAmpPatternStore:        "6",
-				readAmpPatternValueIdx:     "514",
-			},
-		},
-		{
-			name: "read amp - no node / store",
-			re:   readAmpPattern,
-			line: readAmpNoNodeStoreLine,
-			matches: map[int]string{
-				readAmpPatternTimestampIdx: "211215 14:55:15.802648",
-				readAmpPatternNode:         "?",
-				readAmpPatternStore:        "?",
-				readAmpPatternValueIdx:     "514",
+				readAmpPatternValueIdx: "5",
 			},
 		},
 	}
@@ -204,19 +171,19 @@ func TestCompactionLogs_Regex(t *testing.T) {
 
 func TestCompactionLogs(t *testing.T) {
 	var c *logEventCollector
-	var pebbleFileNum, cockroachFileNum int
+	var logNum int
 	resetFn := func() {
 		c = newEventCollector()
-		pebbleFileNum, cockroachFileNum = 0, 0
+		logNum = 0
 	}
 	resetFn()
 
 	dir := t.TempDir()
 	datadriven.RunTest(t, "testdata/compactions", func(td *datadriven.TestData) string {
 		switch td.Cmd {
-		case "pebble-log":
-			basename := fmt.Sprintf("pebble.%d.log", pebbleFileNum)
-			pebbleFileNum++
+		case "log":
+			basename := fmt.Sprintf("%d.log", logNum)
+			logNum++
 			f, err := os.Create(filepath.Join(dir, basename))
 			if err != nil {
 				panic(err)
@@ -226,24 +193,7 @@ func TestCompactionLogs(t *testing.T) {
 			}
 			_ = f.Close()
 
-			if err = parseLog(f.Name(), c, parsePebbleFn); err != nil {
-				return err.Error()
-			}
-			return basename
-
-		case "cockroach-log":
-			basename := fmt.Sprintf("cockroach.%d.log", cockroachFileNum)
-			cockroachFileNum++
-			f, err := os.Create(filepath.Join(dir, basename))
-			if err != nil {
-				panic(err)
-			}
-			if _, err := f.WriteString(td.Input); err != nil {
-				panic(err)
-			}
-			_ = f.Close()
-
-			if err = parseLog(f.Name(), c, parseCockroachFn); err != nil {
+			if err = parseLog(f.Name(), c); err != nil {
 				return err.Error()
 			}
 			return basename
