5ad67e2366f23831917eb2e388109b9cef697a92
Bilal Akhtar <bilal@cockroachlabs.com>
2022-03-11 12:05:14 -0500

    internal/keyspan: Add LevelIter for range keys
    When reading range keys directly from sstables, we will
    need to instantiate levelIters for each level that contains
    sstables that could contain range keys. This change adds
    a levelIter that implements keyspan.FragmentIterator and
    takes advantage of level invariants to only have one sstable's
    range key block iter open at once. On top of that,
    as fragmentBoundIterator does not currently support
    SetBounds or truncation of spans based on iterator bounds, those
    features are provided by LevelIter.
    
    Also moves TableNewRangeKeyIter from the base package to
    internal/keyspan.

--- internal/keyspan/iter_test.go
+++ internal/keyspan/iter_test.go
@@ -14,6 +14,64 @@ import (
 	"github.com/cockroachdb/pebble/internal/datadriven"
 )
 
+func runFragmentIteratorCmd(iter FragmentIterator, input string) string {
+	var b bytes.Buffer
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+		var start *base.InternalKey
+		switch parts[0] {
+		case "seek-ge":
+			if len(parts) != 2 {
+				return "seek-ge <key>\n"
+			}
+			start, _ = iter.SeekGE([]byte(strings.TrimSpace(parts[1])), false /* trySeekUsingNext */)
+		case "seek-lt":
+			if len(parts) != 2 {
+				return "seek-lt <key>\n"
+			}
+			start, _ = iter.SeekLT([]byte(strings.TrimSpace(parts[1])))
+		case "first":
+			start, _ = iter.First()
+		case "last":
+			start, _ = iter.Last()
+		case "next":
+			start, _ = iter.Next()
+		case "prev":
+			start, _ = iter.Prev()
+		case "close":
+			iter.Close()
+			iter = nil
+		case "set-bounds":
+			if len(parts) != 3 {
+				return fmt.Sprintf("set-bounds expects 2 bounds, got %d", len(parts)-1)
+			}
+			l, u := []byte(parts[1]), []byte(parts[2])
+			if parts[1] == "." {
+				l = nil
+			}
+			if parts[2] == "." {
+				u = nil
+			}
+			iter.SetBounds(l, u)
+			fmt.Fprintf(&b, ".\n")
+			continue
+		default:
+			return fmt.Sprintf("unknown op: %s", parts[0])
+		}
+		if iter.Valid() {
+			fmt.Fprintf(&b, "%s-%s#%d\n", start.UserKey, iter.End(), start.SeqNum())
+		} else if err := iter.Error(); err != nil {
+			fmt.Fprintf(&b, "err=%v\n", err)
+		} else {
+			fmt.Fprintf(&b, ".\n")
+		}
+	}
+	return b.String()
+}
+
 func TestIter(t *testing.T) {
 	var tombstones []Span
 	datadriven.RunTest(t, "testdata/iter", func(d *datadriven.TestData) string {
@@ -33,58 +91,7 @@ func TestIter(t *testing.T) {
 			iter := NewIter(base.DefaultComparer.Compare, tombstones)
 			defer iter.Close()
 
-			var b bytes.Buffer
-			for _, line := range strings.Split(d.Input, "\n") {
-				parts := strings.Fields(line)
-				if len(parts) == 0 {
-					continue
-				}
-				var start *base.InternalKey
-				switch parts[0] {
-				case "seek-ge":
-					if len(parts) != 2 {
-						return "seek-ge <key>\n"
-					}
-					start, _ = iter.SeekGE([]byte(strings.TrimSpace(parts[1])), false /* trySeekUsingNext */)
-				case "seek-lt":
-					if len(parts) != 2 {
-						return "seek-lt <key>\n"
-					}
-					start, _ = iter.SeekLT([]byte(strings.TrimSpace(parts[1])))
-				case "first":
-					start, _ = iter.First()
-				case "last":
-					start, _ = iter.Last()
-				case "next":
-					start, _ = iter.Next()
-				case "prev":
-					start, _ = iter.Prev()
-				case "set-bounds":
-					if len(parts) != 3 {
-						return fmt.Sprintf("set-bounds expects 2 bounds, got %d", len(parts)-1)
-					}
-					l, u := []byte(parts[1]), []byte(parts[2])
-					if parts[1] == "." {
-						l = nil
-					}
-					if parts[2] == "." {
-						u = nil
-					}
-					iter.SetBounds(l, u)
-					fmt.Fprintf(&b, ".\n")
-					continue
-				default:
-					return fmt.Sprintf("unknown op: %s", parts[0])
-				}
-				if iter.Valid() {
-					fmt.Fprintf(&b, "%s-%s#%d\n", start.UserKey, iter.End(), start.SeqNum())
-				} else if err := iter.Error(); err != nil {
-					fmt.Fprintf(&b, "err=%v\n", err)
-				} else {
-					fmt.Fprintf(&b, ".\n")
-				}
-			}
-			return b.String()
+			return runFragmentIteratorCmd(iter, d.Input)
 
 		default:
 			return fmt.Sprintf("unknown command: %s", d.Cmd)
