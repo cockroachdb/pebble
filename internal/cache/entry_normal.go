45e9c79275e1f544cd5ba2d3bc1cc68f97411f98
Nick Travers <travers@cockroachlabs.com>
2022-03-21 08:27:51 -0700

    cache: use finalizer for freeing manually allocated memory
    Currently, the `entryCacheAlloc` struct maintains a slice of `entry`
    structs. When CGo is enabled, the latter is backed by manually allocated
    memory. As the former is a pooled struct, when it is released from the
    pool, it will be garbage collected by the Go runtime.
    
    The patch in #1087 introduced a regression where a no-op finalizer is
    used unless running with `invariants` or `trace` when `race` is
    disabled.  Typical production binaries will not have these flags set, so
    the no-op finalizer is installed in place of a "real" finalizer. This
    results in a small memory leak given that the memory backing each of the
    `entry` items is not released when the pool releases an
    `entryCacheAlloc` and it is garbage collected by the runtime.
    
    Revert to installing the "real" finalizer in this case, as it is
    required for correctness.
    
    Update the documentation on the use of finalizers to point out that we
    do indeed rely on them for correctness, in a very niche, but valid
    usecase for freeing manually allocated memory.
    
    Fixes #1588.

--- internal/cache/entry_normal.go
+++ internal/cache/entry_normal.go
@@ -8,6 +8,7 @@
 package cache
 
 import (
+	"runtime"
 	"sync"
 	"unsafe"
 
@@ -54,9 +55,12 @@ type entryAllocCache struct {
 func newEntryAllocCache() *entryAllocCache {
 	c := &entryAllocCache{}
 	if !entriesGoAllocated {
-		// Note: this is a no-op if invariants and tracing are disabled or race is
-		// enabled.
-		invariants.SetFinalizer(c, freeEntryAllocCache)
+		// Note the use of a "real" finalizer here (as opposed to a build tag-gated
+		// no-op finalizer). Without the finalizer, objects released from the pool
+		// and subsequently GC'd by the Go runtime would fail to have their manually
+		// allocated memory freed, which results in a memory leak.
+		// lint:ignore SetFinalizer
+		runtime.SetFinalizer(c, freeEntryAllocCache)
 	}
 	return c
 }
