7931ba4836a6cc05f3b2b6e2c61f2f7f44c6d038
Jared Wasinger <j-wasinger@hotmail.com>
2022-04-06 01:18:46 +0000

    track manually-allocated memory size and expose it through metrics

--- db.go
+++ db.go
@@ -1562,6 +1562,7 @@ func (d *DB) Metrics() *Metrics {
 	metrics.BlockCache = d.opts.Cache.Metrics()
 	metrics.TableCache, metrics.Filter = d.tableCache.metrics()
 	metrics.TableIters = int64(d.tableCache.iterCount())
+	metrics.ManualAllocSize = manual.ManualAllocSize()
 	return metrics
 }
 
