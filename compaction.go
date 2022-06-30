4b8a54d191ef0f0eef6ecf10fb71fc975540d688
Nick Travers <travers@cockroachlabs.com>
2022-06-30 16:04:53 -0700

    *: treat manifest-related errors during logAndApply as fatal
    As certain manifest-related errors encountered during `logAndApply`
    could leave file and b-tree reference counts inconsistent (as mentioned
    in #1792), and / or L0 sublevel metadata incorrect (as identified in #1669),
    remove the L0 sublevel regeneration logic added in #1784 in favor of
    treating all manifest related errors encountered during `logAndApply` as
    fatal.
    
    Adapt the existing test case to cover ingestions, which are also
    susceptible to the original issue identified in #1669. Specifically, an
    ingestion requires application of a version edit, which may fail and
    leave the metadata for the L0 sublevels in an inconsistent state.

--- compaction.go
+++ compaction.go
@@ -1895,7 +1895,6 @@ func (d *DB) compact1(c *compaction, errChannel chan error) (err error) {
 			return d.getInProgressCompactionInfoLocked(c)
 		})
 		if err != nil {
-			info.Err = err
 			// TODO(peter): untested.
 			d.mu.versions.obsoleteTables = append(d.mu.versions.obsoleteTables, pendingOutputs...)
 			d.mu.versions.incrementObsoleteTablesLocked(pendingOutputs)
