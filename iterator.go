f4a46002dee561463fa312b38ce4ac59b88a785c
Bilal Akhtar <bilal@cockroachlabs.com>
2022-08-29 10:39:10 -0400

    *: don't close pointIter twice if bypassing merging iter
    Currently, if the pointIter is an sstable iterator under an `Iterator`,
    it could get Close()d twice. This is not an issue if the pointIter
    is a merging iter as merging iters are idempotent for closes, but
    sstable iterators release themselves into a pool on close so
    making their closes idempotent is harder, and double-closes lead
    to double iterator reuse.
    
    This change adds a `closePointIterOnce bool` to track if the
    point iter should only be closed once, _and_ that closing i.iter
    would close it too.

--- iterator.go
+++ iterator.go
@@ -235,6 +235,12 @@ type Iterator struct {
 	// Used for an optimization in external iterators to reduce the number of
 	// merging levels.
 	forwardOnly bool
+	// closePointIterOnce is set to true if this point iter can only be Close()d
+	// once, _and_ closing i.iter and then i.pointIter would close i.pointIter
+	// twice. This is necessary to track if the point iter is an internal iterator
+	// that could release its resources to a pool on Close(), making it harder for
+	// that iterator to make its own closes idempotent.
+	closePointIterOnce bool
 	// Used in some tests to disable the random disabling of seek optimizations.
 	forceEnableSeekOpt bool
 }
@@ -1739,7 +1745,7 @@ func (i *Iterator) Close() error {
 		// NB: If the iterators were still connected to i.iter, they may be
 		// closed, but calling Close on a closed internal iterator or fragment
 		// iterator is allowed.
-		if i.pointIter != nil {
+		if i.pointIter != nil && !i.closePointIterOnce {
 			i.err = firstError(i.err, i.pointIter.Close())
 		}
 		if i.rangeKey != nil && i.rangeKey.rangeKeyIter != nil {
