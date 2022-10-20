cc0dd372b6d10b7cebacbdf3fc19bbb0074af31b
Nick Travers <travers@cockroachlabs.com>
2022-10-20 10:39:54 -0700

    manifest: unref files with range keys when version refcnt is zero
    A manifest version uses reference counting to ensure that obsolete
    SSTables are only removed once it is safe to do so (i.e. no iterators or
    snapshots are open referencing tables in a older manifest version).
    
    In #1771, a new slice of `LevelMetadata` was added to support range
    keys. A slice already existed for point keys (plus range dels).
    Currently, when a version's ref count reaches zero, only the files
    present in the point key `LevelMetadata` slice have their ref counts
    decremented. If a file also contains range keys, the reference count
    never becomes zero and the file becomes "zombied", and can never be
    deleted from disk.
    
    Decrement the ref counts on files present in the range keys
    `LevelMetadata` slice, allowing the ref counts to be correctly zeroed.
    
    Add regression test to exercise the bug observed in
    cockroachdb/cockroach#90149.
    
    Fix cockroachdb/cockroach#90149.

--- compaction_test.go
+++ compaction_test.go
@@ -2200,6 +2200,13 @@ func TestCompactionTombstones(t *testing.T) {
 				d.mu.Unlock()
 				return str
 
+			case "close":
+				if err := d.Close(); err != nil {
+					return err.Error()
+				}
+				d = nil
+				return ""
+
 			case "version":
 				d.mu.Lock()
 				s := d.mu.versions.currentVersion().String()
