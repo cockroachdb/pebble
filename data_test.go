9e337c422db986fbbf262fbc20a9b6f45bc3a12c
Michael Butler <butler@cockroachlabs.com>
2022-11-12 08:31:02 -0500

    compaction: add multilevel compaction test infra
    This patch adds scaffolding to data driven test drivers that will run multi
    level compaction unit tests.
    
    Informs #2053

--- data_test.go
+++ data_test.go
@@ -770,6 +770,8 @@ func runDBDefineCmd(td *datadriven.TestData, opts *Options) (*DB, error) {
 				return nil, errors.Errorf("%s: could not parse %q as float: %s", td.Cmd, arg.Vals[0], err)
 			}
 			opts.Experimental.PointTombstoneWeight = w
+		case "multi-level":
+			opts.Experimental.MultiLevelCompaction = true
 		default:
 			return nil, errors.Errorf("%s: unknown arg: %s", td.Cmd, arg.Key)
 		}
