c411ebedf6773c6e855d2876393f449844956f42
Jackson Owens <jackson@cockroachlabs.com>
2022-11-14 17:27:54 -0500

    internal/rangekey: reuse defragmenting buffers
    ```
    name                                                old time/op    new time/op    delta
    CombinedIteratorSeek/range-key=true/batch=false-16    8.71µs ± 2%    8.36µs ± 1%   -4.07%  (p=0.000 n=10+8)
    
    name                                                old alloc/op   new alloc/op   delta
    CombinedIteratorSeek/range-key=true/batch=false-16      344B ± 0%      278B ± 0%  -19.28%  (p=0.000 n=10+10)
    
    name                                                old allocs/op  new allocs/op  delta
    CombinedIteratorSeek/range-key=true/batch=false-16      8.00 ± 0%      6.00 ± 0%  -25.00%  (p=0.000 n=10+10)
    ```

--- range_keys.go
+++ range_keys.go
@@ -17,7 +17,7 @@ import (
 func (i *Iterator) constructRangeKeyIter() {
 	i.rangeKey.rangeKeyIter = i.rangeKey.iterConfig.Init(
 		&i.comparer, i.seqNum, i.opts.LowerBound, i.opts.UpperBound,
-		&i.hasPrefix, &i.prefixOrFullSeekKey)
+		&i.hasPrefix, &i.prefixOrFullSeekKey, &i.rangeKey.rangeKeyBuffers.internal)
 
 	// If there's an indexed batch with range keys, include it.
 	if i.batch != nil {
