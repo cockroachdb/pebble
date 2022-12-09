88c8fbcc3867b70a0ca6abea416a0b11e51e8894
Jackson Owens <jackson@cockroachlabs.com>
2022-12-09 13:20:18 -0500

    *: use golang.org/x/sys package
    Ever since Go 1.4 [1], the syscall package has been deprecated, replaced
    by golang.org/x/sys's platform-dependent packages. Update existing call
    sites to use these syscall entrypoints.
    
    This commit should not have any functional changes.
    
    [1] https://go.dev/doc/go1.4#major_library_changes

--- vfs/preallocate_linux.go
+++ vfs/preallocate_linux.go
@@ -17,12 +17,8 @@
 
 package vfs
 
-import (
-	"syscall"
-
-	"golang.org/x/sys/unix"
-)
+import "golang.org/x/sys/unix"
 
 func preallocExtend(fd uintptr, offset, length int64) error {
-	return syscall.Fallocate(int(fd), unix.FALLOC_FL_KEEP_SIZE, offset, length)
+	return unix.Fallocate(int(fd), unix.FALLOC_FL_KEEP_SIZE, offset, length)
 }
