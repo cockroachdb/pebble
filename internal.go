3029bb4eda063bfdac249a9af7f8ddd987ebd12e
sumeerbhola <sumeer@cockroachlabs.com>
2022-12-21 10:29:57 -0500

    db: export some types needed for using lazy values

--- internal.go
+++ internal.go
@@ -36,3 +36,14 @@ type internalIterator = base.InternalIterator
 // ErrCorruption is a marker to indicate that data in a file (WAL, MANIFEST,
 // sstable) isn't in the expected format.
 var ErrCorruption = base.ErrCorruption
+
+// AttributeAndLen exports the base.AttributeAndLen type.
+type AttributeAndLen = base.AttributeAndLen
+
+// ShortAttribute exports the base.ShortAttribute type.
+type ShortAttribute = base.ShortAttribute
+
+// LazyFetcher exports the base.LazyFetcher type. This export is needed since
+// LazyValue.Clone requires a pointer to a LazyFetcher struct to avoid
+// allocations. No code outside Pebble needs to peer into a LazyFetcher.
+type LazyFetcher = base.LazyFetcher
