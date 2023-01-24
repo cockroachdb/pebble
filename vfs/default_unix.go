05b9229e0e167471e0fb65105c0610cd2e0e6ff5
Jackson Owens <jackson@cockroachlabs.com>
2023-01-24 17:58:04 -0500

    vfs: add SyncData,SyncTo,Preallocate to vfs.File
    Expand the vfs.File interface to expose SyncData, SyncTo and Preallocate as
    first-class methods. Previously, the file created through vfs.NewSyncingFile
    would perform analagous I/O operations (eg, `Fdatasync`, `sync_file_range`,
    `Fallocate`) outside the context of the interface. This easily allowed the
    accidental loss of the disk-health checking over these operations by minor
    tweaks to the interface of the disk-health checking implementation or by adding
    intermediary VFS wrappers between the disk-health checking FS and the syncing
    file.
    
    See cockroachdb/cockroach#94373 where the introduction of an additional VFS
    wrapper resulted in these I/O operations being uncovered by disk-stall
    detection.

---
+++ vfs/default_unix.go
@@ -0,0 +1,49 @@
+// Copyright 2014 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+//go:build darwin || dragonfly || freebsd || (linux && arm) || netbsd || openbsd || solaris
+// +build darwin dragonfly freebsd linux,arm netbsd openbsd solaris
+
+package vfs
+
+import (
+	"os"
+	"syscall"
+
+	"github.com/cockroachdb/errors"
+)
+
+func wrapOSFile(osFile *os.File) File {
+	return &unixFile{osFile}
+}
+
+func (defaultFS) OpenDir(name string) (File, error) {
+	f, err := os.OpenFile(name, syscall.O_CLOEXEC, 0)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &unixFile{f}, nil
+}
+
+// Assert that unixFile implements vfs.File.
+var _ File = (*unixFile)(nil)
+
+type unixFile struct {
+	*os.File
+}
+
+func (f *unixFile) Attributes() FileAttributes {
+	// No support for sync_file_range.
+	return FileAttributes{CanSyncTo: false}
+}
+
+func (f *unixFile) Preallocate(offset, length int64) error {
+	// It is ok for correctness to no-op file preallocation. WAL recycling is the
+	// more important mechanism for WAL sync performance and it doesn't rely on
+	// fallocate or posix_fallocate in order to be effective.
+	return nil
+}
+
+func (f *unixFile) SyncData() error    { return f.Sync() }
+func (f *unixFile) SyncTo(int64) error { return f.Sync() }
--- vfs/syncing_file_test.go
+++ vfs/syncing_file_test.go
@@ -33,12 +33,8 @@ func TestSyncingFile(t *testing.T) {
 	}
 	s = NewSyncingFile(f, SyncingFileOptions{BytesPerSync: 8 << 10 /* 8 KB */})
 	s.(*syncingFile).fd = 1
-	s.(*syncingFile).syncTo = func(offset int64) error {
-		s.(*syncingFile).ratchetSyncOffset(offset)
-		return nil
-	}
 
-	t.Logf("sync_file_range=%t", s.(*syncingFile).useSyncRange)
+	t.Logf("sync_file_range=%t", s.(*syncingFile).fileAttrs.CanSyncTo)
 
 	testCases := []struct {
 		n              int64
@@ -64,26 +60,26 @@ func TestSyncingFile(t *testing.T) {
 
 func TestSyncingFileClose(t *testing.T) {
 	testCases := []struct {
-		syncToEnabled bool
-		expected      string
+		canSyncTo bool
+		expected  string
 	}{
 		{true, `sync-to(1048576): test [<nil>]
 sync-to(2097152): test [<nil>]
 sync-to(3145728): test [<nil>]
 pre-close: test [offset=4194304 sync-offset=3145728]
-sync: test [<nil>]
+sync-to(4194304): test [<nil>]
 close: test [<nil>]
 `},
-		// When SyncFileRange is not being used, the last sync call ends up syncing
-		// all of the data causing syncingFile.Close to elide the sync.
-		{false, `sync: test [<nil>]
-sync: test [<nil>]
+		// When SyncTo is not being used, the last sync call ends up syncing all
+		// of the data causing syncingFile.Close to elide the sync.
+		{false, `sync-data: test [<nil>]
+sync-data: test [<nil>]
 pre-close: test [offset=4194304 sync-offset=4194304]
 close: test [<nil>]
 `},
 	}
 	for _, c := range testCases {
-		t.Run("", func(t *testing.T) {
+		t.Run(fmt.Sprintf("canSyncTo=%t", c.canSyncTo), func(t *testing.T) {
 			tmpf, err := os.CreateTemp("", "pebble-db-syncing-file-")
 			require.NoError(t, err)
 
@@ -96,16 +92,10 @@ close: test [<nil>]
 
 			var buf bytes.Buffer
 			lf := loggingFile{f, "test", &buf}
-
 			s := NewSyncingFile(lf, SyncingFileOptions{BytesPerSync: 8 << 10 /* 8 KB */}).(*syncingFile)
-			if c.syncToEnabled {
+			s.fileAttrs.CanSyncTo = c.canSyncTo
+			if c.canSyncTo {
 				s.fd = 1
-				s.syncData = lf.Sync
-				s.syncTo = func(offset int64) error {
-					s.ratchetSyncOffset(offset)
-					fmt.Fprintf(lf.w, "sync-to(%d): %s [%v]\n", offset, lf.name, err)
-					return nil
-				}
 			} else {
 				s.fd = InvalidFd
 			}
@@ -134,7 +124,7 @@ close: test [<nil>]
 
 func TestSyncingFileNoSyncOnClose(t *testing.T) {
 	testCases := []struct {
-		useSyncRange bool
+		useSyncTo    bool
 		expectBefore int64
 		expectAfter  int64
 	}{
@@ -143,7 +133,7 @@ func TestSyncingFileNoSyncOnClose(t *testing.T) {
 	}
 
 	for _, c := range testCases {
-		t.Run(fmt.Sprintf("useSyncRange=%v", c.useSyncRange), func(t *testing.T) {
+		t.Run(fmt.Sprintf("useSyncTo=%v", c.useSyncTo), func(t *testing.T) {
 			tmpf, err := os.CreateTemp("", "pebble-db-syncing-file-")
 			require.NoError(t, err)
 
@@ -156,9 +146,8 @@ func TestSyncingFileNoSyncOnClose(t *testing.T) {
 
 			var buf bytes.Buffer
 			lf := loggingFile{f, "test", &buf}
-
 			s := NewSyncingFile(lf, SyncingFileOptions{NoSyncOnClose: true, BytesPerSync: 8 << 10}).(*syncingFile)
-			s.useSyncRange = c.useSyncRange
+			s.fileAttrs.CanSyncTo = c.useSyncTo
 
 			write := func(n int64) {
 				t.Helper()
@@ -175,9 +164,13 @@ func TestSyncingFileNoSyncOnClose(t *testing.T) {
 			require.NoError(t, s.Close())
 			syncToAfter := atomic.LoadInt64(&s.atomic.syncOffset)
 
-			if syncToBefore != c.expectBefore || syncToAfter != c.expectAfter {
-				t.Fatalf("Expected syncTo before and after closing are %d %d but found %d %d",
-					c.expectBefore, c.expectAfter, syncToBefore, syncToAfter)
+			// If we're not able to non-blockingly sync using sync-to,
+			// NoSyncOnClose should elide the sync.
+			if !c.useSyncTo {
+				if syncToBefore != c.expectBefore || syncToAfter != c.expectAfter {
+					t.Fatalf("Expected syncTo before and after closing are %d %d but found %d %d",
+						c.expectBefore, c.expectAfter, syncToBefore, syncToAfter)
+				}
 			}
 		})
 	}
@@ -239,7 +232,7 @@ func BenchmarkSyncWrite(b *testing.B) {
 					if err != nil {
 						b.Fatal(err)
 					}
-					return NewSyncingFile(t, SyncingFileOptions{PreallocateSize: 0})
+					return NewSyncingFile(wrapOSFile(t), SyncingFileOptions{PreallocateSize: 0})
 				})
 			})
 		}
@@ -254,7 +247,7 @@ func BenchmarkSyncWrite(b *testing.B) {
 					if err != nil {
 						b.Fatal(err)
 					}
-					return NewSyncingFile(t, SyncingFileOptions{PreallocateSize: 4 << 20})
+					return NewSyncingFile(wrapOSFile(t), SyncingFileOptions{PreallocateSize: 4 << 20})
 				})
 			})
 		}
@@ -285,7 +278,7 @@ func BenchmarkSyncWrite(b *testing.B) {
 					if err != nil {
 						b.Fatal(err)
 					}
-					return NewSyncingFile(t, SyncingFileOptions{PreallocateSize: 0})
+					return NewSyncingFile(wrapOSFile(t), SyncingFileOptions{PreallocateSize: 0})
 				})
 			})
 		}
