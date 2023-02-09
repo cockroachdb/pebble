0568b5fd3d1476122930958b608a6524e89b38ca
Arjun Nair <nair@cockroachlabs.com>
2023-02-08 19:59:48 -0500

    metamorphic: metamorphically ingest as flushable

--- internal/metamorphic/options.go
+++ internal/metamorphic/options.go
@@ -348,6 +348,7 @@ func randomOptions(rng *rand.Rand) *testOptions {
 		opts.Experimental.MaxWriterConcurrency = 2
 		opts.Experimental.ForceWriterParallelism = true
 	}
+	opts.Experimental.IngestSSTablesAsFlushable = rng.Intn(2) == 0
 	var lopts pebble.LevelOptions
 	lopts.BlockRestartInterval = 1 + rng.Intn(64)  // 1 - 64
 	lopts.BlockSize = 1 << uint(rng.Intn(24))      // 1 - 16MB
