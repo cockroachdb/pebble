9e8708317325837a0c37fbeef2ae8f003cacef4d
Arjun Nair <nair@cockroachlabs.com>
2023-02-21 15:09:38 -0500

    .*: virtual sstable FileMetadata changes
    This pr introduces the FileMetadata changes which will
    be required to make further virtual sstable changes.
    
    This pr doesn't deal with persisting of the FileMetadata
    to disk through the version edits.
    
    RFC: https://github.com/cockroachdb/pebble/blob/master/docs/RFCS/20221122_virtual_sstable.md

--- version_set_test.go
+++ version_set_test.go
@@ -7,8 +7,10 @@ package pebble
 import (
 	"io"
 	"testing"
+	"time"
 
 	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/manifest"
 	"github.com/cockroachdb/pebble/record"
 	"github.com/cockroachdb/pebble/sstable"
 	"github.com/cockroachdb/pebble/vfs"
@@ -23,6 +25,175 @@ func writeAndIngest(t *testing.T, mem vfs.FS, d *DB, k InternalKey, v []byte, fi
 	require.NoError(t, w.Add(k, v))
 	require.NoError(t, w.Close())
 	require.NoError(t, d.Ingest([]string{path}))
+}
+
+// TestLatestRefCounting sanity checks the ref counting implementation for
+// FileMetadata.latestRefs, and makes sure that the zombie table implementation
+// works when the version edit contains virtual sstables. It also checks that
+// we're adding the physical sstable to the obsolete tables list iff the file is
+// truly obsolete.
+func TestLatestRefCounting(t *testing.T) {
+	mem := vfs.NewMem()
+	require.NoError(t, mem.MkdirAll("ext", 0755))
+
+	opts := &Options{
+		FS:                          mem,
+		MaxManifestFileSize:         1,
+		DisableAutomaticCompactions: true,
+	}
+	d, err := Open("", opts)
+	require.NoError(t, err)
+
+	err = d.Set([]byte{'a'}, []byte{'a'}, nil)
+	require.NoError(t, err)
+	err = d.Set([]byte{'b'}, []byte{'b'}, nil)
+	require.NoError(t, err)
+
+	err = d.Flush()
+	require.NoError(t, err)
+
+	iter := d.mu.versions.currentVersion().Levels[0].Iter()
+	var f *fileMetadata = iter.First()
+	require.NotNil(t, f)
+	require.Equal(t, 1, int(f.LatestRefs()))
+	require.Equal(t, 0, len(d.mu.versions.obsoleteTables))
+
+	// Grab some new file nums.
+	d.mu.Lock()
+	f1 := d.mu.versions.nextFileNum
+	f2 := f1 + 1
+	d.mu.versions.nextFileNum += 2
+	d.mu.Unlock()
+
+	m1 := &manifest.FileMetadata{
+		VirtualState: &manifest.VirtualState{
+			Parent:       f.PhysicalMeta(),
+			ParentSSTNum: f.FileNum,
+		},
+		FileNum:        f1,
+		CreationTime:   time.Now().Unix(),
+		Size:           f.Size / 2,
+		SmallestSeqNum: f.SmallestSeqNum,
+		LargestSeqNum:  f.LargestSeqNum,
+		Smallest:       base.MakeInternalKey([]byte{'a'}, f.Smallest.SeqNum(), InternalKeyKindSet),
+		Largest:        base.MakeInternalKey([]byte{'a'}, f.Smallest.SeqNum(), InternalKeyKindSet),
+		HasPointKeys:   true,
+	}
+
+	m2 := &manifest.FileMetadata{
+		VirtualState: &manifest.VirtualState{
+			Parent:       f.PhysicalMeta(),
+			ParentSSTNum: f.FileNum,
+		},
+		FileNum:        f2,
+		CreationTime:   time.Now().Unix(),
+		Size:           f.Size / 2,
+		SmallestSeqNum: f.SmallestSeqNum,
+		LargestSeqNum:  f.LargestSeqNum,
+		Smallest:       base.MakeInternalKey([]byte{'b'}, f.Largest.SeqNum(), InternalKeyKindSet),
+		Largest:        base.MakeInternalKey([]byte{'b'}, f.Largest.SeqNum(), InternalKeyKindSet),
+		HasPointKeys:   true,
+	}
+
+	m1.LargestPointKey = m1.Largest
+	m1.SmallestPointKey = m1.Smallest
+
+	m2.LargestPointKey = m2.Largest
+	m2.SmallestPointKey = m2.Smallest
+
+	m1.ValidateVirtual()
+	m2.ValidateVirtual()
+
+	fileMetrics := func(ve *versionEdit) map[int]*LevelMetrics {
+		metrics := newFileMetrics(ve.NewFiles)
+		for de, f := range ve.DeletedFiles {
+			lm := metrics[de.Level]
+			if lm == nil {
+				lm = &LevelMetrics{}
+				metrics[de.Level] = lm
+			}
+			metrics[de.Level].NumFiles--
+			metrics[de.Level].Size -= int64(f.Size)
+		}
+		return metrics
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	applyVE := func(ve *versionEdit) error {
+		d.mu.versions.logLock()
+		jobID := d.mu.nextJobID
+		d.mu.nextJobID++
+
+		err := d.mu.versions.logAndApply(jobID, ve, fileMetrics(ve), false, func() []compactionInfo {
+			return d.getInProgressCompactionInfoLocked(nil)
+		})
+		d.updateReadStateLocked(nil)
+		return err
+	}
+
+	// Virtualize f.
+	ve := manifest.VersionEdit{}
+	d1 := manifest.DeletedFileEntry{Level: 0, FileNum: f.FileNum}
+	n1 := manifest.NewFileEntry{Level: 0, Meta: m1}
+	n2 := manifest.NewFileEntry{Level: 0, Meta: m2}
+
+	ve.DeletedFiles = make(map[manifest.DeletedFileEntry]*manifest.FileMetadata)
+	ve.DeletedFiles[d1] = f
+	ve.NewFiles = append(ve.NewFiles, n1)
+	ve.NewFiles = append(ve.NewFiles, n2)
+
+	require.NoError(t, applyVE(&ve))
+	// 2 latestRefs from 2 virtual sstables in the latest version which refer
+	// to the physical sstable.
+	require.Equal(t, 2, int(m1.LatestRefs()))
+	require.Equal(t, 0, len(d.mu.versions.obsoleteTables))
+
+	// Make sure that f is not present in zombie list, because it is not yet a
+	// zombie.
+	require.Equal(t, 0, len(d.mu.versions.zombieTables))
+
+	// Delete the virtual sstable m1.
+	ve = manifest.VersionEdit{}
+	d1 = manifest.DeletedFileEntry{Level: 0, FileNum: m1.FileNum}
+	ve.DeletedFiles = make(map[manifest.DeletedFileEntry]*manifest.FileMetadata)
+	ve.DeletedFiles[d1] = m1
+	require.NoError(t, applyVE(&ve))
+
+	// Only one virtual sstable in the latest version, confirm that the latest
+	// version ref counting is correct.
+	require.Equal(t, 1, int(m2.LatestRefs()))
+	require.Equal(t, 0, len(d.mu.versions.zombieTables))
+	require.Equal(t, 0, len(d.mu.versions.obsoleteTables))
+
+	// Move m2 from L0 to L6 to test the move compaction case.
+	ve = manifest.VersionEdit{}
+	d1 = manifest.DeletedFileEntry{Level: 0, FileNum: m2.FileNum}
+	n1 = manifest.NewFileEntry{Level: 6, Meta: m2}
+	ve.DeletedFiles = make(map[manifest.DeletedFileEntry]*manifest.FileMetadata)
+	ve.DeletedFiles[d1] = m2
+	ve.NewFiles = append(ve.NewFiles, n1)
+	require.NoError(t, applyVE(&ve))
+
+	require.Equal(t, 1, int(m2.LatestRefs()))
+	require.Equal(t, 0, len(d.mu.versions.zombieTables))
+	require.Equal(t, 0, len(d.mu.versions.obsoleteTables))
+
+	// Delete m2 from L6.
+	ve = manifest.VersionEdit{}
+	d1 = manifest.DeletedFileEntry{Level: 6, FileNum: m2.FileNum}
+	ve.DeletedFiles = make(map[manifest.DeletedFileEntry]*manifest.FileMetadata)
+	ve.DeletedFiles[d1] = m2
+	require.NoError(t, applyVE(&ve))
+
+	// All virtual sstables are gone.
+	require.Equal(t, 0, int(m2.LatestRefs()))
+	require.Equal(t, 1, len(d.mu.versions.zombieTables))
+	require.Equal(t, f.Size, d.mu.versions.zombieTables[f.FileNum])
+
+	// Make sure that the physical file is added to the obsolete tables list.
+	require.Equal(t, 1, len(d.mu.versions.obsoleteTables))
+
 }
 
 func TestVersionSetCheckpoint(t *testing.T) {
