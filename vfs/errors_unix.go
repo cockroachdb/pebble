7e07e44cf4bf506902b8526cd333d8a0268451d4
Leonardo Taccari <leot@NetBSD.org>
2023-02-22 13:15:13 +0100

    vfs: Add support for NetBSD
    Merchanically adjust Unix assumptions also valid for NetBSD and add
    GetDiskUsage support for NetBSD
    
    Complete copypasto of implementation used by all other Unix-es except
    of using unix.Statvfs() instead of unix.Statfs().

--- vfs/errors_unix.go
+++ vfs/errors_unix.go
@@ -2,8 +2,8 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build darwin || dragonfly || freebsd || linux || openbsd
-// +build darwin dragonfly freebsd linux openbsd
+//go:build darwin || dragonfly || freebsd || linux || openbsd || netbsd
+// +build darwin dragonfly freebsd linux openbsd netbsd
 
 package vfs
 
