f4105b18e0be83e56f936b7ecc4b190090c9c730
Laurent Cheylus <foxy@free.fr>
2023-03-15 18:24:58 +0100

    vfs: Add support for OpenBSD
      - Add vfs/disk_usage_openbsd.go to use unix.Statfs with specific
        fields for OpenBSD
      - Fix bug #2341
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- vfs/disk_usage_openbsd.go
+++ vfs/disk_usage_openbsd.go
@@ -1,9 +1,9 @@
-// Copyright 2020 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// Copyright 2023 The LevelDB-Go and Pebble Authors. All rights reserved. Use
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build darwin || dragonfly || freebsd
-// +build darwin dragonfly freebsd
+//go:build openbsd
+// +build openbsd
 
 package vfs
 
@@ -15,9 +15,9 @@ func (defaultFS) GetDiskUsage(path string) (DiskUsage, error) {
 		return DiskUsage{}, err
 	}
 
-	freeBytes := uint64(stat.Bsize) * uint64(stat.Bfree)
-	availBytes := uint64(stat.Bsize) * uint64(stat.Bavail)
-	totalBytes := uint64(stat.Bsize) * uint64(stat.Blocks)
+	freeBytes := uint64(stat.F_bsize) * uint64(stat.F_bfree)
+	availBytes := uint64(stat.F_bsize) * uint64(stat.F_bavail)
+	totalBytes := uint64(stat.F_bsize) * uint64(stat.F_blocks)
 	return DiskUsage{
 		AvailBytes: availBytes,
 		TotalBytes: totalBytes,
