772b4fabbacbc9d7bae3bd751d6ec1f84c031e85
Jackson Owens <jackson@cockroachlabs.com>
2023-05-08 17:56:27 -0400

    vfs: unify linux implementation across arm and !arm
    Remove the arm-specific VFS logic, extending the previously non-arm linux file
    to apply to all linux builds. This separation originated in #171, due to the
    SyncFileRange syscall being undefined in arm builds, but the unix package now
    provides it.
    
    Fix #2514.

--- vfs/default_linux.go
+++ vfs/default_linux.go
@@ -2,8 +2,8 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build linux && !arm
-// +build linux,!arm
+//go:build linux
+// +build linux
 
 package vfs
 
