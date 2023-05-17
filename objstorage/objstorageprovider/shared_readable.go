8c2cb1915c9c4124e7dd39693b74e56d98388455
Radu Berinde <radu@cockroachlabs.com>
2023-05-17 12:04:11 -0700

    objstorage: implement read-ahead for shared objects
    We Reuse the existing ramp-up logic. For now, the max readahead is
    256KB but we will probably want to make it configurable and increase
    it for shared objects.

--- objstorage/objstorageprovider/shared_readable.go
+++ objstorage/objstorageprovider/shared_readable.go
@@ -42,24 +42,78 @@ func (r *sharedReadable) Size() int64 {
 
 func (r *sharedReadable) NewReadHandle(_ context.Context) objstorage.ReadHandle {
 	// TODO(radu): use a pool.
-	return &sharedReadHandle{readable: r}
+	rh := &sharedReadHandle{readable: r}
+	rh.readahead.state = makeReadaheadState()
+	return rh
 }
 
 type sharedReadHandle struct {
-	readable *sharedReadable
+	readable  *sharedReadable
+	readahead struct {
+		state  readaheadState
+		data   []byte
+		offset int64
+	}
+	forCompaction bool
 }
 
 var _ objstorage.ReadHandle = (*sharedReadHandle)(nil)
 
 func (r *sharedReadHandle) ReadAt(ctx context.Context, p []byte, offset int64) error {
+	// Check if we already have the data from a previous read-ahead.
+	if rhSize := int64(len(r.readahead.data)); rhSize > 0 {
+		if r.readahead.offset <= offset && r.readahead.offset+rhSize > offset {
+			n := copy(p, r.readahead.data[offset-r.readahead.offset:])
+			if n == len(p) {
+				// All data was available.
+				return nil
+			}
+			// Use the data that we had and do a shorter read.
+			offset += int64(n)
+			p = p[n:]
+		}
+	}
+
+	if readaheadSize := r.maybeReadahead(offset, len(p)); readaheadSize > len(p) {
+		r.readahead.offset = offset
+		// TODO(radu): we need to somehow account for this memory.
+		if cap(r.readahead.data) >= readaheadSize {
+			r.readahead.data = r.readahead.data[:readaheadSize]
+		} else {
+			r.readahead.data = make([]byte, readaheadSize)
+		}
+		if err := r.readable.ReadAt(ctx, r.readahead.data, offset); err != nil {
+			// Make sure we don't treat the data as valid next time.
+			r.readahead.data = r.readahead.data[:0]
+			return err
+		}
+		copy(p, r.readahead.data)
+		return nil
+	}
+
 	return r.readable.ReadAt(ctx, p, offset)
+}
+
+func (r *sharedReadHandle) maybeReadahead(offset int64, len int) int {
+	// TODO(radu): maxReadaheadSize is only 256KB, we probably want 1MB+.
+	if r.forCompaction {
+		return maxReadaheadSize
+	}
+	return int(r.readahead.state.maybeReadahead(offset, int64(len)))
 }
 
 func (r *sharedReadHandle) Close() error {
 	r.readable = nil
+	r.readahead.data = nil
 	return nil
 }
 
-func (r *sharedReadHandle) SetupForCompaction() {}
+func (r *sharedReadHandle) SetupForCompaction() {
+	r.forCompaction = true
+}
 
-func (r *sharedReadHandle) RecordCacheHit(_ context.Context, offset, size int64) {}
+func (r *sharedReadHandle) RecordCacheHit(_ context.Context, offset, size int64) {
+	if !r.forCompaction {
+		r.readahead.state.recordCacheHit(offset, size)
+	}
+}
