4d130219ae022309ff094db337f52e51de3ac676
Rahul Aggarwal <rahul.aggarwal@cockroachlabs.com>
2023-06-13 10:40:17 -0400

    db: Add Option to Filter SSTables
    This change allows users to pass in a `start` and `end` bound
    option to filter sstables. This will be useful when trying to obtain
    metrics only for those sstables that intersect this key range.
    
    Informs: https://github.com/cockroachdb/cockroach/issues/102604
    Release note: None

--- db_test.go
+++ db_test.go
@@ -1254,6 +1254,46 @@ func TestCloseCleanerRace(t *testing.T) {
 	}
 }
 
+func TestFilterSSTablesWithOption(t *testing.T) {
+	d, err := Open("", &Options{
+		FS: vfs.NewMem(),
+	})
+	require.NoError(t, err)
+	defer func() {
+		if d != nil {
+			require.NoError(t, d.Close())
+		}
+	}()
+
+	// Create two sstables.
+	require.NoError(t, d.Set([]byte("/Table/5"), nil, nil))
+	require.NoError(t, d.Flush())
+	require.NoError(t, d.Set([]byte("/Table/10"), nil, nil))
+	require.NoError(t, d.Flush())
+
+	tableInfos, err := d.SSTables(WithKeyRangeFilter([]byte("/Table/5"), []byte("/Table/6")))
+	require.NoError(t, err)
+
+	totalTables := 0
+	for _, levelTables := range tableInfos {
+		totalTables += len(levelTables)
+	}
+
+	// with filter second sstable should not be returned
+	require.EqualValues(t, 1, totalTables)
+
+	tableInfos, err = d.SSTables()
+	require.NoError(t, err)
+
+	totalTables = 0
+	for _, levelTables := range tableInfos {
+		totalTables += len(levelTables)
+	}
+
+	// without filter
+	require.EqualValues(t, 2, totalTables)
+}
+
 func TestSSTables(t *testing.T) {
 	d, err := Open("", &Options{
 		FS: vfs.NewMem(),
