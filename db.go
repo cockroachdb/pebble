c4511f5f251c63e5aab612b2568b7fe0903b7c0d
Rahul Aggarwal <rahul.aggarwal@cockroachlabs.com>
2023-07-06 15:33:46 -0400

    pebble: Collect Pebble Key Statistics
    Created a new function `ScanStatistics` that returns counts of the
    different key kinds in Pebble (by level) as well as the number of
    snapshot keys. Also added two new parameters to `DB.ScanInternal`, to
    indicate which level to exclusively scan as well as whether or not to
    include obsolete keys.
    
    Informs: #1996

--- db.go
+++ db.go
@@ -1176,6 +1176,9 @@ func (d *DB) ScanInternal(
 	visitRangeDel func(start, end []byte, seqNum uint64) error,
 	visitRangeKey func(start, end []byte, keys []rangekey.Key) error,
 	visitSharedFile func(sst *SharedSSTMeta) error,
+	visitKey func(key *InternalKey, value LazyValue) error,
+	level *int,
+	includeObsoleteKeys bool,
 ) error {
 	iter := d.newInternalIter(nil /* snapshot */, &scanInternalOptions{
 		IterOptions: IterOptions{
@@ -1183,10 +1186,12 @@ func (d *DB) ScanInternal(
 			LowerBound: lower,
 			UpperBound: upper,
 		},
-		skipSharedLevels: visitSharedFile != nil,
+		skipSharedLevels:    visitSharedFile != nil,
+		level:               level,
+		includeObsoleteKeys: includeObsoleteKeys,
 	})
 	defer iter.close()
-	return scanInternalImpl(ctx, lower, upper, iter, visitPointKey, visitRangeDel, visitRangeKey, visitSharedFile)
+	return scanInternalImpl(ctx, lower, upper, iter, visitPointKey, visitRangeDel, visitRangeKey, visitSharedFile, visitKey)
 }
 
 // newInternalIter constructs and returns a new scanInternalIterator on this db.
@@ -1237,15 +1242,18 @@ func (d *DB) newInternalIter(s *Snapshot, o *scanInternalOptions) *scanInternalI
 }
 
 func finishInitializingInternalIter(buf *iterAlloc, i *scanInternalIterator) *scanInternalIterator {
-	// Short-hand.
-	memtables := i.readState.memtables
-	// We only need to read from memtables which contain sequence numbers older
-	// than seqNum. Trim off newer memtables.
-	for j := len(memtables) - 1; j >= 0; j-- {
-		if logSeqNum := memtables[j].logSeqNum; logSeqNum < i.seqNum {
-			break
+	var memtables flushableList
+	if i.opts.level == nil {
+		// Short-hand.
+		memtables = i.readState.memtables
+		// We only need to read from memtables which contain sequence numbers older
+		// than seqNum. Trim off newer memtables.
+		for j := len(memtables) - 1; j >= 0; j-- {
+			if logSeqNum := memtables[j].logSeqNum; logSeqNum < i.seqNum {
+				break
+			}
+			memtables = memtables[:j]
 		}
-		memtables = memtables[:j]
 	}
 	i.initializeBoundBufs(i.opts.LowerBound, i.opts.UpperBound)
 
@@ -1953,7 +1961,6 @@ func (d *DB) SSTables(opts ...SSTablesOption) ([][]SSTableInfo, error) {
 			if opt.start != nil && opt.end != nil && !m.Overlaps(d.opts.Comparer.Compare, opt.start, opt.end, true /* exclusive end */) {
 				continue
 			}
-
 			destTables[j] = SSTableInfo{TableInfo: m.TableInfo()}
 			if opt.withProperties {
 				p, err := d.tableCache.getTableProperties(
@@ -2482,6 +2489,60 @@ func (d *DB) SetCreatorID(creatorID uint64) error {
 		return nil
 	}
 	return d.objProvider.SetCreatorID(objstorage.CreatorID(creatorID))
+}
+
+// KeyStatistics keeps track of the number of keys that have been pinned by a
+// compaction as well as counts of the different key kinds in the lsm.
+type KeyStatistics struct {
+	compactionPinnedCount int
+	kindsCount            map[string]int
+}
+
+// LsmKeyStatistics is used by DB.ScanStatistics.
+type LsmKeyStatistics struct {
+	accumulated *KeyStatistics
+	levels      map[int]*KeyStatistics
+}
+
+// ScanStatistics returns the count of different key kinds within the lsm for a
+// key span [lower, upper) as well as the number of snapshot keys.
+func (d *DB) ScanStatistics(lower, upper []byte) (*LsmKeyStatistics, error) {
+	stats := &LsmKeyStatistics{}
+	stats.levels = make(map[int]*KeyStatistics)
+
+	// statistics per level
+	for lvl := 0; lvl < numLevels; lvl++ {
+		stats.levels[lvl] = &KeyStatistics{}
+		stats.levels[lvl].kindsCount = make(map[string]int)
+
+		var prevKey *InternalKey
+		err := d.ScanInternal(context.TODO(), lower, upper, nil, nil, nil, nil,
+			func(key *InternalKey, _ LazyValue) error {
+				if prevKey != nil && d.cmp(prevKey.UserKey, key.UserKey) == 0 {
+					stats.levels[lvl].compactionPinnedCount++
+				}
+				stats.levels[lvl].kindsCount[key.Kind().String()]++
+				prevKey = key
+				return nil
+			},
+			&lvl, true)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// statistics in aggregate across all levels
+	stats.accumulated = &KeyStatistics{}
+	stats.accumulated.kindsCount = make(map[string]int)
+	for lvl := 0; lvl < numLevels; lvl++ {
+		stats.accumulated.compactionPinnedCount += stats.levels[lvl].compactionPinnedCount
+		for kind, count := range stats.levels[lvl].kindsCount {
+			stats.accumulated.kindsCount[kind] += count
+		}
+	}
+
+	return stats, nil
 }
 
 // ObjProvider returns the objstorage.Provider for this database. Meant to be
