628c4107d05ae695c8334f37be53f0170e24376f
Radu Berinde <radu@cockroachlabs.com>
2023-07-18 09:45:23 -0700

    db: improve metrics formatting
    This change improves the formatting of the Pebble metrics:
     - we no longer try to artificially fit non-tabular information in a
       single table
     - all byte figures end in B/KB/MB/etc
     - counts and corresponding sizes are grouped together
     - flushable ingests information moved under ingestions
    
    These metrics are periodically logged and can be obtained through the
    db console as well.
    
    Release note: Pebble metrics formatting improvements; tools that parse
    these logs might need updating.
    
    Fixes #1473.

--- tool/logs/compaction_test.go
+++ tool/logs/compaction_test.go
@@ -24,7 +24,8 @@ const (
 	compactionEndLine   = `I211215 14:26:56.318543 51831533 3@vendor/github.com/cockroachdb/pebble/compaction.go:1886 ⋮ [n5,pebble,s6] 1216554  [JOB 284925] compacted(default) L2 [442555] (4.2 M) + L3 [445853] (8.4 M) -> L3 [445883 445887] (13 M), in 0.3s, output rate 42 M/s`
 	flushStartLine      = `I211213 16:23:48.903751 21136 3@vendor/github.com/cockroachdb/pebble/event.go:599 ⋮ [n9,pebble,s8] 24 [JOB 10] flushing 2 memtables to L0`
 	flushEndLine        = `I211213 16:23:49.134464 21136 3@vendor/github.com/cockroachdb/pebble/event.go:603 ⋮ [n9,pebble,s8] 26 [JOB 10] flushed 2 memtables to L0 [1535806] (1.3 M), in 0.2s, output rate 5.8 M/s`
-	readAmpLine         = `  total     31766   188 G       -   257 G   187 G    48 K   3.6 G     744   536 G    49 K   278 G       5     2.1`
+	readAmpLine23_1     = `  total     31766   188 G       -   257 G   187 G    48 K   3.6 G     744   536 G    49 K   278 G       5     2.1`
+	readAmpLine         = `total |   32K 188GB     0B |     - | 257GB |   48K  187GB |   744  3.6GB |   49K  536GB | 278GB |   5  2.1`
 
 	compactionStartNoNodeStoreLine = `I211215 14:26:56.012382 51831533 3@vendor/github.com/cockroachdb/pebble/compaction.go:1845 ⋮ [n?,pebble,s?] 1216510  [JOB 284925] compacting(default) L2 [442555] (4.2 M) + L3 [445853] (8.4 M)`
 	flushStartNoNodeStoreLine      = `I211213 16:23:48.903751 21136 3@vendor/github.com/cockroachdb/pebble/event.go:599 ⋮ [n?,pebble,s?] 24 [JOB 10] flushing 2 memtables to L0`
@@ -157,6 +158,14 @@ func TestCompactionLogs_Regex(t *testing.T) {
 				readAmpPatternValueIdx: "5",
 			},
 		},
+		{
+			name: "read amp suffix 23.1",
+			re:   readAmpPattern,
+			line: readAmpLine23_1,
+			matches: map[int]string{
+				readAmpPatternValueIdx: "5",
+			},
+		},
 	}
 
 	for _, tc := range tcs {
@@ -171,75 +180,76 @@ func TestCompactionLogs_Regex(t *testing.T) {
 }
 
 func TestCompactionLogs(t *testing.T) {
-	var c *logEventCollector
-	var logNum int
-	resetFn := func() {
-		c = newEventCollector()
-		logNum = 0
-	}
-	resetFn()
-
 	dir := t.TempDir()
-	datadriven.RunTest(t, "testdata/compactions", func(t *testing.T, td *datadriven.TestData) string {
-		switch td.Cmd {
-		case "log":
-			basename := fmt.Sprintf("%d.log", logNum)
-			logNum++
-			f, err := os.Create(filepath.Join(dir, basename))
-			if err != nil {
-				panic(err)
-			}
-			if _, err = f.WriteString(td.Input); err != nil {
-				panic(err)
-			}
-			_ = f.Close()
-
-			if err = parseLog(f.Name(), c); err != nil {
-				return err.Error()
-			}
-			return basename
-
-		case "summarize":
-			window := 1 * time.Minute
-			longRunning := time.Duration(math.MaxInt64)
-
-			var err error
-			for _, cmdArg := range td.CmdArgs {
-				switch cmdArg.Key {
-				case "window":
-					window, err = time.ParseDuration(cmdArg.Vals[0])
-					if err != nil {
-						panic(errors.Newf("could not parse window: %s", err))
-					}
-
-				case "long-running":
-					longRunning, err = time.ParseDuration(cmdArg.Vals[0])
-					if err != nil {
-						panic(errors.Newf("could not parse long-running: %s", err))
-					}
-
-				default:
-					panic(errors.Newf("unknown arg %q", cmdArg.Key))
-				}
-			}
-
-			a := newAggregator(window, longRunning, c.events, c.readAmps)
-			windows := a.aggregate()
-
-			var b bytes.Buffer
-			for _, w := range windows {
-				b.WriteString(w.String())
-			}
-
-			return b.String()
-
-		case "reset":
-			resetFn()
-			return ""
-
-		default:
-			return fmt.Sprintf("unknown command %q", td.Cmd)
+	datadriven.Walk(t, "testdata", func(t *testing.T, path string) {
+		var c *logEventCollector
+		var logNum int
+		resetFn := func() {
+			c = newEventCollector()
+			logNum = 0
 		}
+		resetFn()
+		datadriven.RunTest(t, path, func(t *testing.T, td *datadriven.TestData) string {
+			switch td.Cmd {
+			case "log":
+				basename := fmt.Sprintf("%d.log", logNum)
+				logNum++
+				f, err := os.Create(filepath.Join(dir, basename))
+				if err != nil {
+					panic(err)
+				}
+				if _, err = f.WriteString(td.Input); err != nil {
+					panic(err)
+				}
+				_ = f.Close()
+
+				if err = parseLog(f.Name(), c); err != nil {
+					return err.Error()
+				}
+				return basename
+
+			case "summarize":
+				window := 1 * time.Minute
+				longRunning := time.Duration(math.MaxInt64)
+
+				var err error
+				for _, cmdArg := range td.CmdArgs {
+					switch cmdArg.Key {
+					case "window":
+						window, err = time.ParseDuration(cmdArg.Vals[0])
+						if err != nil {
+							panic(errors.Newf("could not parse window: %s", err))
+						}
+
+					case "long-running":
+						longRunning, err = time.ParseDuration(cmdArg.Vals[0])
+						if err != nil {
+							panic(errors.Newf("could not parse long-running: %s", err))
+						}
+
+					default:
+						panic(errors.Newf("unknown arg %q", cmdArg.Key))
+					}
+				}
+
+				a := newAggregator(window, longRunning, c.events, c.readAmps)
+				windows := a.aggregate()
+
+				var b bytes.Buffer
+				for _, w := range windows {
+					b.WriteString(w.String())
+				}
+
+				return b.String()
+
+			case "reset":
+				resetFn()
+				return ""
+
+			default:
+				return fmt.Sprintf("unknown command %q", td.Cmd)
+			}
+		})
 	})
 }
 
