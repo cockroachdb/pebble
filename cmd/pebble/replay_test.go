43a29468e25782b0dcdfa5d7fd657799288f2bea
Jackson Owens <jackson@cockroachlabs.com>
2023-07-18 13:07:24 -0400

    cmd/pebble: use non-default block cache size in replay tool
    If a block cache size is configured within a checkpoint's OPTIONS file, or if a
    block cache size provided through the options string command-line flag, create
    a cache of the provided size instead. Also, add a `--max-cache-size` CLI flag
    so that a replay run that used a much larger block cache can be used on a
    machine with less memory available.

--- cmd/pebble/replay_test.go
+++ cmd/pebble/replay_test.go
@@ -5,35 +5,48 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/internal/cache"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseOptionsStr(t *testing.T) {
 	type testCase struct {
-		optionsStr string
-		options    *pebble.Options
+		c       replayConfig
+		options *pebble.Options
 	}
 
 	testCases := []testCase{
 		{
-			optionsStr: `[Options] max_concurrent_compactions=9`,
-			options:    &pebble.Options{MaxConcurrentCompactions: func() int { return 9 }},
+			c:       replayConfig{optionsString: `[Options] max_concurrent_compactions=9`},
+			options: &pebble.Options{MaxConcurrentCompactions: func() int { return 9 }},
 		},
 		{
-			optionsStr: `[Options] bytes_per_sync=90000`,
-			options:    &pebble.Options{BytesPerSync: 90000},
+			c:       replayConfig{optionsString: `[Options] bytes_per_sync=90000`},
+			options: &pebble.Options{BytesPerSync: 90000},
 		},
 		{
-			optionsStr: `[Options] [Level "0"] target_file_size=222`,
+			c:       replayConfig{optionsString: fmt.Sprintf(`[Options] cache_size=%d`, 16<<20 /* 16MB */)},
+			options: &pebble.Options{Cache: cache.New(16 << 20 /* 16 MB */)},
+		},
+		{
+			c: replayConfig{
+				maxCacheSize:  16 << 20, /* 16 MB */
+				optionsString: fmt.Sprintf(`[Options] cache_size=%d`, 10<<30 /* 10 GB */),
+			},
+			options: &pebble.Options{Cache: cache.New(16 << 20 /* 16 MB */)},
+		},
+		{
+			c: replayConfig{optionsString: `[Options] [Level "0"] target_file_size=222`},
 			options: &pebble.Options{Levels: []pebble.LevelOptions{
 				{TargetFileSize: 222},
 			}},
 		},
 		{
-			optionsStr: `[Options] lbase_max_bytes=10  max_open_files=20  [Level "0"] target_file_size=30 [Level "1"] index_block_size=40`,
+			c: replayConfig{optionsString: `[Options] lbase_max_bytes=10  max_open_files=20  [Level "0"] target_file_size=30 [Level "1"] index_block_size=40`},
 			options: &pebble.Options{
 				LBaseMaxBytes: 10,
 				MaxOpenFiles:  20,
@@ -47,12 +60,18 @@ func TestParseOptionsStr(t *testing.T) {
 
 	for _, tc := range testCases {
 		o := new(pebble.Options)
-		require.NoError(t, parseCustomOptions(tc.optionsStr, o))
+		require.NoError(t, tc.c.parseCustomOptions(tc.c.optionsString, o))
 		o.EnsureDefaults()
 		got := o.String()
 
 		tc.options.EnsureDefaults()
 		want := tc.options.String()
 		require.Equal(t, want, got)
+		if o.Cache != nil {
+			o.Cache.Unref()
+		}
+		if tc.options.Cache != nil {
+			tc.options.Cache.Unref()
+		}
 	}
 }
