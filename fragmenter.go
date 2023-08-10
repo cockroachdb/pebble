cab827e841540c39dcb9c790d16f5ea4f786cc73
Rahul Aggarwal <rahul.aggarwal@cockroachlabs.com>
2023-08-10 15:56:41 -0400

    pebble: Export keyspan.Fragmenter
    This pull request exports the keyspan.Fragmenter to be used in
    Cockroach's MultiSSTWriter changes.
    
    Related pr: https://github.com/cockroachdb/cockroach/pull/106771

---
+++ fragmenter.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package pebble
+
+import "github.com/cockroachdb/pebble/internal/keyspan"
+
+// Fragmenter exports the keyspan.Fragmenter type
+type Fragmenter = keyspan.Fragmenter
