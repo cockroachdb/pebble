2c413243f30f25ba6be6faba140a31eb13e00b94
Jackson Owens <jackson@cockroachlabs.com>
2023-08-14 11:09:55 -0400

    sstable: fix two-level monotonic bounds bug
    This commit fixes a sstable iterator bug surfaced by the metamorphic test in
    the issue #2816.
    
    In the circumstances of this bug, a two-level sstable iterator with
    monotonically advanced bounds could reuse the wrong index block if a previous
    block was excluded due to a block-property filter. An accompanying regression
    unit test is added too.
    
    Additionally, it refactors some additional sstable tests to run against more
    table formats and automatically against newly introduced table formats.
    
    While this commit is sufficient to fix the bug, in subsequent work I anticipate
    refactoring and tightening invariants. This refactoring will not be suitable
    for a backport, so this commit first resolves the bug in a backportable way.

--- sstable/block.go
+++ sstable/block.go
@@ -643,10 +643,12 @@ func (i *blockIter) cacheEntry() {
 // SeekGE implements internalIterator.SeekGE, as documented in the pebble
 // package.
 func (i *blockIter) SeekGE(key []byte, flags base.SeekGEFlags) (*InternalKey, base.LazyValue) {
-	i.clearCache()
+	if invariants.Enabled && i.isDataInvalidated() {
+		panic(errors.AssertionFailedf("invalidated blockIter used"))
+	}
 
 	ikey := base.MakeSearchKey(key)
-
+	i.clearCache()
 	// Find the index of the smallest restart point whose key is > the key
 	// sought; index will be numRestarts if there is no such restart point.
 	i.offset = 0
@@ -771,10 +773,13 @@ func (i *blockIter) SeekPrefixGE(
 // SeekLT implements internalIterator.SeekLT, as documented in the pebble
 // package.
 func (i *blockIter) SeekLT(key []byte, flags base.SeekLTFlags) (*InternalKey, base.LazyValue) {
-	i.clearCache()
+	if invariants.Enabled && i.isDataInvalidated() {
+		panic(errors.AssertionFailedf("invalidated blockIter used"))
+	}
 
 	ikey := base.MakeSearchKey(key)
 
+	i.clearCache()
 	// Find the index of the smallest restart point whose key is >= the key
 	// sought; index will be numRestarts if there is no such restart point.
 	i.offset = 0
@@ -917,6 +922,10 @@ func (i *blockIter) SeekLT(key []byte, flags base.SeekLTFlags) (*InternalKey, ba
 // First implements internalIterator.First, as documented in the pebble
 // package.
 func (i *blockIter) First() (*InternalKey, base.LazyValue) {
+	if invariants.Enabled && i.isDataInvalidated() {
+		panic(errors.AssertionFailedf("invalidated blockIter used"))
+	}
+
 	i.offset = 0
 	if !i.valid() {
 		return nil, base.LazyValue{}
@@ -943,6 +952,10 @@ func decodeRestart(b []byte) int32 {
 
 // Last implements internalIterator.Last, as documented in the pebble package.
 func (i *blockIter) Last() (*InternalKey, base.LazyValue) {
+	if invariants.Enabled && i.isDataInvalidated() {
+		panic(errors.AssertionFailedf("invalidated blockIter used"))
+	}
+
 	// Seek forward from the last restart point.
 	i.offset = decodeRestart(i.data[i.restarts+4*(i.numRestarts-1):])
 	if !i.valid() {
