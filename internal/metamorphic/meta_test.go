a207649c83016461d545d1f803e399e9bcd922be
Radu Berinde <radu@cockroachlabs.com>
2023-09-03 13:14:10 -0700

    metamorphic: allow coverage instrumentation
    `TestMeta` runs multiple instances of itself in other processes by
    executing the same binary (`os.Args[0]`). This doesn't work with the
    go test code coverage infrastructure - the "inner" call is not
    covered.
    
    To address this, we modify `TestMeta` to allow specifying the "inner
    binary" and we create a regular binary `metarunner` that can run a
    single instance of `TestMeta` (i.e. either `--run-dir` or `--compare`
    is passed). This will allow building `metarunner` with binary coverage
    instrumentation (see https://go.dev/testing/coverage/#building).

--- internal/metamorphic/meta_test.go
+++ internal/metamorphic/meta_test.go
@@ -5,14 +5,11 @@
 package metamorphic
 
 import (
-	"flag"
-	"math"
 	"path/filepath"
-	"regexp"
 	"strings"
 	"testing"
 
-	"github.com/cockroachdb/pebble/internal/randvar"
+	"github.com/cockroachdb/pebble/internal/metamorphic/metaflags"
 	"github.com/cockroachdb/pebble/metamorphic"
 )
 
@@ -24,68 +21,15 @@ import (
 //   a variable length suffix).
 // - Add support for Writer.LogData
 
-var (
-	dir = flag.String("dir", "_meta",
-		"the directory storing test state")
-	fs = flag.String("fs", "rand",
-		`force the tests to use either memory or disk-backed filesystems (valid: "mem", "disk", "rand")`)
-	// TODO: default error rate to a non-zero value. Currently, retrying is
-	// non-deterministic because of the Ierator.*WithLimit() methods since
-	// they may say that the Iterator is not valid, but be positioned at a
-	// certain key that can be returned in the future if the limit is changed.
-	// Since that key is hidden from clients of Iterator, the retryableIter
-	// using SeekGE will not necessarily position the Iterator that saw an
-	// injected error at the same place as an Iterator that did not see that
-	// error.
-	errorRate = flag.Float64("error-rate", 0.0,
-		"rate of errors injected into filesystem operations (0 ≤ r < 1)")
-	failRE = flag.String("fail", "",
-		"fail the test if the supplied regular expression matches the output")
-	traceFile = flag.String("trace-file", "",
-		"write an execution trace to `<run-dir>/file`")
-	keep = flag.Bool("keep", false,
-		"keep the DB directory even on successful runs")
-	seed = flag.Uint64("seed", 0,
-		"a pseudorandom number generator seed")
-	ops        = randvar.NewFlag("uniform:5000-10000")
-	maxThreads = flag.Int("max-threads", math.MaxInt,
-		"limit execution of a single run to the provided number of threads; must be ≥ 1")
-	runDir = flag.String("run-dir", "",
-		"the specific configuration to (re-)run (used for post-mortem debugging)")
-	compare = flag.String("compare", "",
-		`comma separated list of options files to compare. The result of each run is compared with
-the result of the run from the first options file in the list. Example, -compare
-random-003,standard-000. The dir flag should have the directory containing these directories.
-Example, -dir _meta/200610-203012.077`)
+var runOnceFlags, runFlags = metaflags.InitAllFlags()
 
-	// The following options may be used for split-version metamorphic testing.
-	// To perform split-version testing, the client runs the metamorphic tests
-	// on an earlier Pebble SHA passing the `--keep` flag. The client then
-	// switches to the later Pebble SHA, setting the below options to point to
-	// the `ops` file and one of the previous run's data directories.
-	previousOps = flag.String("previous-ops", "",
-		"path to an ops file, used to prepopulate the set of keys operations draw from")
-	initialStatePath = flag.String("initial-state", "",
-		"path to a database's data directory, used to prepopulate the test run's databases")
-	initialStateDesc = flag.String("initial-state-desc", "",
-		`a human-readable description of the initial database state.
-		If set this parameter is written to the OPTIONS to aid in
-		debugging. It's intended to describe the lineage of a
-		database's state, including sufficient information for
-		reproduction (eg, SHA, prng seed, etc).`)
-)
-
-func init() {
-	flag.Var(ops, "ops", "")
-}
-
-// TestMeta generates a random set of operations to run, then runs the test
-// with different options. See standardOptions() for the set of options that
-// are always run, and randomOptions() for the randomly generated options. The
-// number of operations to generate is determined by the `--ops` flag. If a
-// failure occurs, the output is kept in `_meta/<test>`, though note that a
-// subsequent invocation will overwrite that output. A test can be re-run by
-// using the `--run-dir` flag. For example:
+// TestMeta generates a random set of operations to run, then runs multiple
+// instances of the test with varying options. See standardOptions() for the set
+// of options that are always run, and randomOptions() for the randomly
+// generated options. The number of operations to generate is determined by the
+// `--ops` flag. If a failure occurs, the output is kept in `_meta/<test>`,
+// though note that a subsequent invocation will overwrite that output. A test
+// can be re-run by using the `--run-dir` flag. For example:
 //
 //	go test -v -run TestMeta --run-dir _meta/standard-017
 //
@@ -102,58 +46,27 @@ func init() {
 // and options. This must be run on the same commit SHA as the original
 // failure, otherwise changes to the metamorphic tests may cause the generated
 // operations and options to differ.
+//
+// Each instance of the test is run in a different process, by executing the
+// same binary (i.e. os.Args[0]) and passing `--run_dir`; the "inner" binary can
+// be customized via the --inner-binary flag (used for code coverage
+// instrumentation).
 func TestMeta(t *testing.T) {
-	opts := []metamorphic.RunOption{
-		metamorphic.Seed(*seed),
-		metamorphic.OpCount(ops.Static),
-		metamorphic.MaxThreads(*maxThreads),
-	}
-	onceOpts := []metamorphic.RunOnceOption{
-		metamorphic.MaxThreads(*maxThreads),
-	}
+	switch {
+	case runOnceFlags.Compare != "":
+		runDirs := strings.Split(runOnceFlags.Compare, ",")
+		onceOpts := runOnceFlags.MakeRunOnceOptions()
+		metamorphic.Compare(t, runOnceFlags.Dir, runOnceFlags.Seed, runDirs, onceOpts...)
 
-	if *keep {
-		opts = append(opts, metamorphic.KeepData{})
-		onceOpts = append(onceOpts, metamorphic.KeepData{})
-	}
-	if *failRE != "" {
-		opts = append(opts, metamorphic.FailOnMatch{Regexp: regexp.MustCompile(*failRE)})
-		onceOpts = append(onceOpts, metamorphic.FailOnMatch{Regexp: regexp.MustCompile(*failRE)})
-	}
-	if *errorRate > 0 {
-		opts = append(opts, metamorphic.InjectErrorsRate(*errorRate))
-		onceOpts = append(onceOpts, metamorphic.InjectErrorsRate(*errorRate))
-	}
-	if *traceFile != "" {
-		opts = append(opts, metamorphic.RuntimeTrace(*traceFile))
-	}
-	if *previousOps != "" {
-		opts = append(opts, metamorphic.ExtendPreviousRun(*previousOps, *initialStatePath, *initialStateDesc))
-	}
-	// If the filesystem type was forced, all tests will use that value.
-	switch *fs {
-	case "", "rand", "default":
-		// No-op. Use the generated value for the filesystem.
-	case "disk":
-		opts = append(opts, metamorphic.UseDisk)
-	case "mem":
-		opts = append(opts, metamorphic.UseInMemory)
-	default:
-		t.Fatalf("unknown forced filesystem type: %q", *fs)
-	}
-
-	if *compare != "" {
-		runDirs := strings.Split(*compare, ",")
-		metamorphic.Compare(t, *dir, *seed, runDirs, onceOpts...)
-		return
-	}
-
-	if *runDir != "" {
+	case runOnceFlags.RunDir != "":
 		// The --run-dir flag is specified either in the child process (see
 		// runOptions() below) or the user specified it manually in order to re-run
 		// a test.
-		metamorphic.RunOnce(t, *runDir, *seed, filepath.Join(*runDir, "history"), onceOpts...)
-		return
+		onceOpts := runOnceFlags.MakeRunOnceOptions()
+		metamorphic.RunOnce(t, runOnceFlags.RunDir, runOnceFlags.Seed, filepath.Join(runOnceFlags.RunDir, "history"), onceOpts...)
+
+	default:
+		opts := runFlags.MakeRunOptions()
+		metamorphic.RunAndCompare(t, runFlags.Dir, opts...)
 	}
-	metamorphic.RunAndCompare(t, *dir, opts...)
 }
