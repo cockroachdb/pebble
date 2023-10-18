2acc384ae0ab158ce8af538834ef2aefe4a883bf
Adrian Sutton <adrian@oplabs.co>
2023-10-18 13:40:56 +1000

    internal/manual,internal/rawalloc: Reduce max array size to support MIPS.

--- internal/manual/manual_mips.go
+++ internal/manual/manual_mips.go
@@ -2,12 +2,12 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build 386 || amd64p32 || arm || armbe || ppc || sparc
-// +build 386 amd64p32 arm armbe ppc sparc
+//go:build mips || mipsle || mips64p32 || mips64p32le
+// +build mips mipsle mips64p32 mips64p32le
 
 package manual
 
 const (
 	// MaxArrayLen is a safe maximum length for slices on this architecture.
-	MaxArrayLen = 1<<31 - 1
+	MaxArrayLen = 1 << 30
 )
