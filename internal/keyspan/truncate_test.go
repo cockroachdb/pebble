48cce3658d1c74eccd8cc6728fbb078351a339c0
Jackson Owens <jackson@cockroachlabs.com>
2023-11-02 15:57:55 -0400

    internal/keyspan: document InterleavingIter error invariants
    In #3021 (33a77e17) we fixed the InterleavingIter's error handling to propagate
    all errors encountered by child iterators. This commit documents the invariants
    more thoroughly.
    
    Additionally the behavior of nextPos|prevPos is adjusted to set posExhausted if
    on i.err != nil on entry. This fixes a remaining latent bug when changing
    directions, resulting in two steps. Previously, if both steps were on the same
    iterator and the first step errored but the second did not, the error could go
    unnoticed.
    
    Future work will unit test these cases explicitly using new error-injection
    testing facilities.

--- internal/keyspan/truncate_test.go
+++ internal/keyspan/truncate_test.go
@@ -5,9 +5,7 @@
 package keyspan
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"strings"
 	"testing"
 
@@ -90,60 +88,4 @@ func TestTruncate(t *testing.T) {
 			return fmt.Sprintf("unknown command: %s", d.Cmd)
 		}
 	})
-}
-
-// runIterCmd evaluates a datadriven command controlling an internal
-// keyspan.FragmentIterator, returning a string with the results of the iterator
-// operations.
-func runIterCmd(t *testing.T, td *datadriven.TestData, iter FragmentIterator) string {
-	var buf bytes.Buffer
-	lines := strings.Split(strings.TrimSpace(td.Input), "\n")
-	for i, line := range lines {
-		if i > 0 {
-			fmt.Fprintln(&buf)
-		}
-		line = strings.TrimSpace(line)
-		i := strings.IndexByte(line, '#')
-		iterCmd := line
-		if i > 0 {
-			iterCmd = string(line[:i])
-		}
-		dataDrivenRunIterOp(&buf, iter, iterCmd)
-	}
-	return buf.String()
-}
-
-func dataDrivenRunIterOp(w io.Writer, it FragmentIterator, op string) {
-	fields := strings.FieldsFunc(op, func(r rune) bool { return iterDelim[r] })
-	var s *Span
-	switch strings.ToLower(fields[0]) {
-	case "first":
-		s = it.First()
-	case "last":
-		s = it.Last()
-	case "seekge", "seek-ge":
-		if len(fields) == 1 {
-			panic(fmt.Sprintf("unable to parse iter op %q", op))
-		}
-		s = it.SeekGE([]byte(fields[1]))
-	case "seeklt", "seek-lt":
-		if len(fields) == 1 {
-			panic(fmt.Sprintf("unable to parse iter op %q", op))
-		}
-		s = it.SeekLT([]byte(fields[1]))
-	case "next":
-		s = it.Next()
-	case "prev":
-		s = it.Prev()
-	default:
-		panic(fmt.Sprintf("unrecognized iter op %q", fields[0]))
-	}
-	if s == nil {
-		fmt.Fprint(w, "<nil>")
-		if err := it.Error(); err != nil {
-			fmt.Fprintf(w, " err=<%s>", it.Error())
-		}
-		return
-	}
-	fmt.Fprint(w, s)
 }
