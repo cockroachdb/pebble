8782c55737ecba8d61304478bd9ac8d535e7a6c0
Radu Berinde <radu@cockroachlabs.com>
2024-01-05 08:10:06 -0800

    github: add nightly stress action
    Add a nightly unit tests stress run. This won't autofile issues
    (requires more work); for now we can keep an eye on the build.

---
+++ scripts/stress.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Stress all packages, one at a time. This allows for a more useful output.
+for p in $(go list ./... | sed 's#github.com/cockroachdb/pebble#.#'); do
+echo
+  echo ""
+  echo ""
+  echo "Stressing $p"
+  echo ""
+  make stress STRESSFLAGS='-maxtime 5m -maxruns 100' "PKG=$p"
+done
