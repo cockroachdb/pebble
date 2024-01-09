4b398734c797c0653f77103f202ec0cd96ac206c
Jackson Owens <jackson@cockroachlabs.com>
2024-01-09 14:13:56 -0500

    internal/keyspan: simplify Get and fix error handling
    The keyspan.Get helper is used by `DB.Get` to retrieve range deletions
    overlapping a particular key. It was written before #2146 when a `SeekGE(k)`
    seeked the iterator to the first span with a _start key_ ≥ k. In #2146, the
    semantics of `SeekGE(k)` was modified to seek to the first span with an end key
    greater than k (or, more intuitively, the first span to "cover" a key ≥ k).
    With the new semantics, a simple `SeekGE` seeks to the span covering `k` if
    any, and `keyspan.Get` only needs to verify that the returned span doesn't lie
    wholly after `k`.
    
    Despite being written against the old iterator interface, the previous
    keyspan.Get implementation remained correct in the absence of iterator errors
    after #2146. However, if an iterator error occurred during the SeekLT,
    keyspan.Get would improperly step the iterator forward without consulting
    `FragmentIterator.Error()`.

---
+++ internal/keyspan/get_test.go
@@ -0,0 +1,51 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package keyspan
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/internal/testkeys"
+)
+
+func TestGet(t *testing.T) {
+	cmp := testkeys.Comparer.Compare
+	var buf bytes.Buffer
+	var spans []Span
+	datadriven.RunTest(t, "testdata/get", func(t *testing.T, td *datadriven.TestData) string {
+		buf.Reset()
+		switch td.Cmd {
+		case "define":
+			spans = spans[:0]
+			lines := strings.Split(strings.TrimSpace(td.Input), "\n")
+			for _, line := range lines {
+				spans = append(spans, ParseSpan(line))
+			}
+			return ""
+		case "get":
+			var iter FragmentIterator = NewIter(cmp, spans)
+			if cmdArg, ok := td.Arg("probes"); ok {
+				iter = attachProbes(iter, probeContext{log: &buf},
+					parseProbes(cmdArg.Vals...)...)
+			}
+
+			for _, line := range strings.Split(strings.TrimSpace(td.Input), "\n") {
+				s, err := Get(cmp, iter, []byte(line))
+				if err != nil {
+					fmt.Fprintf(&buf, "Get(%q) = nil <err=%q>\n", line, err)
+					continue
+				}
+				fmt.Fprintf(&buf, "Get(%q) = %v\n", line, s)
+			}
+			return buf.String()
+		default:
+			return fmt.Sprintf("unrecognized command %q", td.Cmd)
+		}
+	})
+}
