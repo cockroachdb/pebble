c47a16e904690f4fc7f4a1c7f13784b6cf57c99f
Jackson Owens <jackson@cockroachlabs.com>
2024-01-09 14:13:56 -0500

    internal/keyspan: simplify Get and fix error handling
    The keyspan.Get helper is used by `DB.Get` to retrieve range deletions
    overlapping a particular key. It was written before #2146 when a `SeekGE(k)`
    seeked the iterator to the first span with a _start key_ ≥ k. In #2146, the
    semantics of `SeekGE(k)` was modified to seek to the first span with an end key
    greater than k (or, more intuitively, the first span to "cover" a key ≥ k).
    With the new semantics, a simple `SeekGE` seeks to the span covering `k` if
    any, and `keyspan.Get` only needs to verify that the returned span doesn't lie
    wholly after `k`.
    
    Despite being written against the old iterator interface, the previous
    keyspan.Get implementation remained correct in the absence of iterator errors
    after #2146. However, if an iterator error occurred during the SeekLT,
    keyspan.Get would improperly step the iterator forward without consulting
    `FragmentIterator.Error()`.

--- internal/keyspan/get.go
+++ internal/keyspan/get.go
@@ -6,48 +6,21 @@ package keyspan
 
 import "github.com/cockroachdb/pebble/internal/base"
 
-// Get returns the newest span that contains the target key. If no span
-// contains the target key, an empty span is returned. The snapshot
-// parameter controls the visibility of spans (only spans older than the
-// snapshot sequence number are visible). The iterator must contain
+// Get returns the newest span that contains the target key. If no span contains
+// the target key, an empty span is returned. The iterator must contain
 // fragmented spans: no span may overlap another.
-func Get(cmp base.Compare, iter FragmentIterator, key []byte) *Span {
-	// NB: We use SeekLT in order to land on the proper span for a search
-	// key that resides in the middle of a span. Consider the scenario:
-	//
-	//     a---e
-	//         e---i
-	//
-	// The spans are indexed by their start keys `a` and `e`. If the
-	// search key is `c` we want to land on the span [a,e). If we were
-	// to use SeekGE then the search key `c` would land on the span
-	// [e,i) and we'd have to backtrack. The one complexity here is what
-	// happens for the search key `e`. In that case SeekLT will land us
-	// on the span [a,e) and we'll have to move forward.
-	iterSpan := iter.SeekLT(key)
+//
+// If an error occurs while seeking iter, a nil span and non-nil error is
+// returned.
+func Get(cmp base.Compare, iter FragmentIterator, key []byte) (*Span, error) {
+	// NB: FragmentIterator.SeekGE moves the iterator to the first span covering
+	// a key greater than or equal to the given key. This is equivalent to
+	// seeking to the first span with an end key greater than the given key.
+	iterSpan := iter.SeekGE(key)
 	if iterSpan == nil {
-		iterSpan = iter.Next()
-		if iterSpan == nil {
-			// The iterator is empty.
-			return nil
-		}
-		if cmp(key, iterSpan.Start) < 0 {
-			// The search key lies before the first span.
-			return nil
-		}
+		return nil, iter.Error()
+	} else if cmp(iterSpan.Start, key) > 0 {
+		return nil, nil
 	}
-
-	// Invariant: key > iterSpan.Start
-	if cmp(key, iterSpan.End) >= 0 {
-		// The current span lies before the search key. Advance the iterator
-		// once to potentially land on a key with a start key exactly equal to
-		// key. (See the comment at the beginning of this function.)
-		iterSpan = iter.Next()
-		if iterSpan == nil || cmp(key, iterSpan.Start) < 0 {
-			// We've run out of spans or we've moved on to a span which
-			// starts after our search key.
-			return nil
-		}
-	}
-	return iterSpan
+	return iterSpan, nil
 }
