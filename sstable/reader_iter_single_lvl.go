add437ada366defa0e7f9308b97cedabe416024a
Michael Butler <butler@cockroachlabs.com>
2024-01-22 13:32:52 -0600

    sstable: add block level suffix replacement support
    This patch teaches the block iterator to replace the suffix of all surfaced
    keys with a replacement suffix passed during block initiation. In other
    words, the iterator will read the block as if all underlying keys actually had
    the replacement suffix.
    
    For correctness, this patch relies on two invariants: for all keys in the
    in the sst:
      1. no two keys in the sst share the same prefix
      2. pebble.Compare(replacementSuffix,originalSuffix) > 0
    
    During the normal readpath, i.e. for blocks without suffix rewriting, this
    patch does slow Seeks down:
    ```
    ❯ benchstat master suffix
    name                           old time/op  new time/op  delta
    BlockIterSeekGE/restart=16-24   345ns ± 0%   367ns ± 0%  +6.53%  (p=0.002 n=6+6)
    BlockIterSeekLT/restart=16-24   398ns ± 0%   416ns ± 0%  +4.55%  (p=0.008 n=5+5)
    BlockIterNext/restart=16-24    15.4ns ± 0%  15.6ns ± 0%  +1.09%  (p=0.002 n=6+6)
    BlockIterPrev/restart=16-24    30.6ns ± 0%  30.6ns ± 0%  -0.23%  (p=0.006 n=6+6)
    ```
    
    This patch also allows the user to initiate a reader with a single suffix
    replacement rule.
    
    add

--- sstable/reader_iter_single_lvl.go
+++ sstable/reader_iter_single_lvl.go
@@ -213,7 +213,9 @@ func (i *singleLevelIterator) init(
 	i.stats = stats
 	i.hideObsoletePoints = hideObsoletePoints
 	i.bufferPool = bufferPool
-	err = i.index.initHandle(i.cmp, indexH, r.Properties.GlobalSeqNum, false, r.syntheticPrefix)
+	// Index blocks don't need a synthetic suffix replacement. See detailed note
+	// for the SyntheticSuffix data type.
+	err = i.index.initHandle(i.cmp, r.Split, indexH, r.Properties.GlobalSeqNum, false, r.syntheticPrefix, nil)
 	if err != nil {
 		// blockIter.Close releases indexH and always returns a nil error
 		_ = i.index.Close()
@@ -435,7 +437,7 @@ func (i *singleLevelIterator) loadBlock(dir int8) loadBlockResult {
 		i.err = err
 		return loadBlockFailed
 	}
-	i.err = i.data.initHandle(i.cmp, block, i.reader.Properties.GlobalSeqNum, i.hideObsoletePoints, i.reader.syntheticPrefix)
+	i.err = i.data.initHandle(i.cmp, i.reader.Split, block, i.reader.Properties.GlobalSeqNum, i.hideObsoletePoints, i.reader.syntheticPrefix, i.vState.syntheticSuffix)
 	if i.err != nil {
 		// The block is partially loaded, and we don't want it to appear valid.
 		i.data.invalidate()
