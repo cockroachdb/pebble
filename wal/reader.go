5cbdec3c062a5010b2bc91bfde93589279e5faa9
Jackson Owens <jackson@cockroachlabs.com>
2024-02-01 16:42:16 -0500

    wal: add facilities to list WAL files across Dirs
    Add some internal (still unexported) facilities for listing the WAL files
    across several directories, ordering the consitutent files by log number and
    index.

---
+++ wal/reader.go
@@ -0,0 +1,87 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package wal
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/pebble/internal/base"
+)
+
+// A segment represents an individual physical file that makes up a contiguous
+// segment of a logical WAL. If a failover occurred during a WAL's lifetime, a
+// WAL may be composed of multiple segments.
+type segment struct {
+	logNameIndex logNameIndex
+	dir          Dir
+}
+
+func (s segment) String() string {
+	return fmt.Sprintf("(%s,%s)", s.dir.Dirname, s.logNameIndex)
+}
+
+// A logicalWAL identifies a logical WAL and its consituent segment files.
+type logicalWAL struct {
+	NumWAL
+	// segments contains the list of the consistuent physical segment files that
+	// make up the single logical WAL file. segments is ordered by increasing
+	// logIndex.
+	segments []segment
+}
+
+func (w logicalWAL) String() string {
+	var sb strings.Builder
+	sb.WriteString(base.DiskFileNum(w.NumWAL).String())
+	sb.WriteString(": {")
+	for i := range w.segments {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		sb.WriteString(w.segments[i].String())
+	}
+	sb.WriteString("}")
+	return sb.String()
+}
+
+// listLogs finds all log files in the provided directories. It returns an
+// ordered list of WALs in increasing NumWAL order.
+func listLogs(dirs ...Dir) ([]logicalWAL, error) {
+	var wals []logicalWAL
+	for _, d := range dirs {
+		ls, err := d.FS.List(d.Dirname)
+		if err != nil {
+			return nil, errors.Wrapf(err, "reading %q", d.Dirname)
+		}
+		for _, name := range ls {
+			dfn, li, ok := parseLogFilename(name)
+			if !ok {
+				continue
+			}
+			// Have we seen this logical log number yet?
+			i, found := slices.BinarySearchFunc(wals, dfn, func(lw logicalWAL, n NumWAL) int {
+				return cmp.Compare(lw.NumWAL, n)
+			})
+			if !found {
+				wals = slices.Insert(wals, i, logicalWAL{NumWAL: dfn, segments: make([]segment, 0, 1)})
+			}
+
+			// Ensure we haven't seen this log index yet, and find where it
+			// slots within this log's segments.
+			j, found := slices.BinarySearchFunc(wals[i].segments, li, func(s segment, li logNameIndex) int {
+				return cmp.Compare(s.logNameIndex, li)
+			})
+			if found {
+				return nil, errors.Errorf("wal: duplicate logIndex=%s for WAL %s in %s and %s",
+					li, dfn, d.Dirname, wals[i].segments[j].dir.Dirname)
+			}
+			wals[i].segments = slices.Insert(wals[i].segments, j, segment{logNameIndex: li, dir: d})
+		}
+	}
+	return wals, nil
+}
---
+++ wal/reader_test.go
@@ -0,0 +1,87 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package wal
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestList(t *testing.T) {
+	var buf bytes.Buffer
+	filesystems := map[string]*vfs.MemFS{}
+	getFS := func(name string) *vfs.MemFS {
+		if _, ok := filesystems[name]; !ok {
+			filesystems[name] = vfs.NewMem()
+		}
+		return filesystems[name]
+	}
+
+	datadriven.RunTest(t, "testdata/list", func(t *testing.T, td *datadriven.TestData) string {
+		buf.Reset()
+		switch td.Cmd {
+		case "list":
+			var dirs []Dir
+			for _, arg := range td.CmdArgs {
+				var dirname string
+				if len(arg.Vals) > 1 {
+					dirname = arg.Vals[1]
+				}
+				dirs = append(dirs, Dir{
+					FS:      getFS(arg.Vals[0]),
+					Dirname: dirname,
+				})
+			}
+			logs, err := listLogs(dirs...)
+			if err != nil {
+				return err.Error()
+			}
+			for i := range logs {
+				fmt.Fprintln(&buf, logs[i].String())
+			}
+			return buf.String()
+		case "reset":
+			clear(filesystems)
+			return ""
+		case "touch":
+			for _, l := range strings.Split(strings.TrimSpace(td.Input), "\n") {
+				if l == "" {
+					continue
+				}
+				fields := strings.Fields(l)
+				fsName := fields[0]
+				filename := fields[1]
+
+				fs := getFS(fsName)
+				require.NoError(t, fs.MkdirAll(fs.PathDir(filename), os.ModePerm))
+				f, err := fs.Create(filename)
+				require.NoError(t, err)
+				require.NoError(t, f.Close())
+			}
+
+			// Sort the filesystem names for determinism.
+			var names []string
+			for name := range filesystems {
+				names = append(names, name)
+			}
+			slices.Sort(names)
+			for _, name := range names {
+				fmt.Fprintf(&buf, "%s:\n", name)
+				fmt.Fprint(&buf, filesystems[name].String())
+			}
+			return buf.String()
+		default:
+			return fmt.Sprintf("unrecognized command %q", td.Cmd)
+		}
+	})
+}
