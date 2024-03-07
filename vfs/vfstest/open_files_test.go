b06a300dbb94c966e0439714f396da8797ee6b4d
Jackson Owens <jackson@cockroachlabs.com>
2024-03-07 17:40:04 -0500

    vfs/vfstest: add WithOpenFileTracking
    Add a wrapping vfs.FS implementation that tracks open files and prints the
    stack trace of the call to open them. I wrote this to debug leaked files in
    Cockroach. This is a common occurrence (eg, cockroachdb/cockroach#117853).

---
+++ vfs/vfstest/open_files_test.go
@@ -0,0 +1,79 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package vfstest
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpenFiles(t *testing.T) {
+	var buf bytes.Buffer
+	fs, dumpOpenFiles := WithOpenFileTracking(vfs.NewMem())
+	require.NoError(t, fs.MkdirAll("dir", os.ModePerm))
+
+	type fileOpenOp struct {
+		name string
+		fn   func() vfs.File
+	}
+	fileOpenOps := []fileOpenOp{
+		{name: "OpenDir", fn: func() vfs.File {
+			f, err := fs.OpenDir("dir")
+			require.NoError(t, err)
+			return f
+		}},
+		{name: "Create", fn: func() vfs.File {
+			f, err := fs.Create("foo")
+			require.NoError(t, err)
+			return f
+		}},
+		{name: "Open", fn: func() vfs.File {
+			f, err := fs.Open("foo")
+			require.NoError(t, err)
+			return f
+		}},
+		{name: "OpenReadWrite", fn: func() vfs.File {
+			f, err := fs.Open("foo")
+			require.NoError(t, err)
+			return f
+		}},
+		{name: "ReuseForWrite", fn: func() vfs.File {
+			f, err := fs.ReuseForWrite("foo", "bar")
+			require.NoError(t, err)
+			return f
+		}},
+	}
+
+	t.Run("leaks", func(t *testing.T) {
+		for _, op := range fileOpenOps {
+			t.Run(op.name, func(t *testing.T) {
+				f := op.fn()
+				buf.Reset()
+				dumpOpenFiles(&buf)
+				t.Log(buf.String())
+				require.Greater(t, buf.Len(), 0)
+				require.NoError(t, f.Close())
+				buf.Reset()
+				dumpOpenFiles(&buf)
+				require.Equal(t, 0, buf.Len())
+			})
+		}
+	})
+	t.Run("noleaks", func(t *testing.T) {
+		for _, op := range fileOpenOps {
+			t.Run(op.name, func(t *testing.T) {
+				f := op.fn()
+				require.NoError(t, f.Close())
+				buf.Reset()
+				dumpOpenFiles(&buf)
+				require.Equal(t, 0, buf.Len())
+			})
+		}
+	})
+}
