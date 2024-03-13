18676a2b875121256b6b9606347266cd69541c52
David Taylor <tinystatemachine@gmail.com>
2024-03-13 19:11:48 +0000

    sstable: add CopySpan, use in runCopyCompaction

---
+++ sstable/copier.go
@@ -0,0 +1,206 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package sstable
+
+import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/bytealloc"
+	"github.com/cockroachdb/pebble/objstorage"
+)
+
+// CopySpan produces a copy of a approximate subset of an input sstable.
+//
+// The produced sstable contains all keys from the input sstable in the span
+// [start, end), as well as potentially some additional keys from the original
+// file that were adjacent to but outside that span.
+//
+// CopySpan differs from simply seeking a reader to start and iterating a until
+// end passing the results to a writer in that it does not write the new sstable
+// from scratch, key-by-key, recompressing each key nto new blocks and computing
+// new filters and properties. Instead it finds _blocks_ that intersect the
+// requested span and copies those, whole, to the new file, avoiding all
+// decompression and recompression work. It then copies the original filter as
+// these can serve as a filter for the subset as well, just with potentially a
+// higher false positive rate than one computed just from the they keys in it.
+//
+// Closes input and finishes or aborts output, including on non-nil errors.
+func CopySpan(
+	ctx context.Context,
+	input objstorage.Readable,
+	rOpts ReaderOptions,
+	output objstorage.Writable,
+	o WriterOptions,
+	start, end InternalKey,
+) (size uint64, _ error) {
+	r, err := NewReader(input, rOpts)
+	if err != nil {
+		input.Close()
+		output.Abort()
+		return 0, err
+	}
+	defer r.Close() // r.Close now owns calling input.Close().
+
+	// If our input has not filters, our output cannot have filters either.
+	if r.filterBH.Offset == 0 {
+		o.FilterPolicy = nil
+	}
+	o.TableFormat = r.tableFormat
+	w := NewWriter(output, o)
+	defer func() {
+		if w != nil {
+			// set w.err to any non-nil error just so it aborts instead of finishing.
+			w.err = base.ErrNotFound
+			// w.Close now owns calling output.Abort().
+			w.Close()
+		}
+	}()
+
+	if r.Properties.NumValueBlocks > 0 || r.Properties.NumRangeKeys() > 0 {
+		return 0, errors.New("sstable value blocks or range keys")
+	}
+
+	// Set the filter block to be copied over if it exists. It will return false
+	// positives for keys in blocks of the original file that we don't copy, but
+	// filters can always have false positives, so this is fine.
+	if r.tableFilter != nil {
+		filterBlock, err := r.readFilter(ctx, nil, nil)
+		if err != nil {
+			return 0, errors.Wrap(err, "reading filter")
+		}
+		filterBytes := append([]byte{}, filterBlock.Get()...)
+		filterBlock.Release()
+		w.filter = copyFilterWriter{
+			origPolicyName: w.filter.policyName(), origMetaName: w.filter.metaName(), data: filterBytes,
+		}
+	}
+
+	// Copy all the props from the source file; we can't compute our own for many
+	// that depend on seeing every key, such as total count or size so we copy the
+	// original props instead. This will result in over-counts but that is safer
+	// than under-counts.
+	w.props = r.Properties
+	// Reset props that we'll re-derive as we build our own index.
+	w.props.IndexPartitions = 0
+	w.props.TopLevelIndexSize = 0
+	w.props.IndexSize = 0
+	w.props.IndexType = 0
+	if w.filter != nil {
+		if err := checkWriterFilterMatchesReader(r, w); err != nil {
+			return 0, err
+		}
+	}
+
+	// Find the blocks that intersect our span.
+	blocks, err := intersectingIndexEntries(ctx, r, start, end)
+	if err != nil {
+		return 0, err
+	}
+	if len(blocks) > 0 {
+		offset := blocks[0].bh.Offset
+		// The block lengths don't include their trailers, which just sit after the
+		// block length, before the next offset; We get he ones between the blocks
+		// we copy implicitly but need to explicitly add the last trailer to length.
+		length := blocks[len(blocks)-1].bh.Offset + blocks[len(blocks)-1].bh.Length + blockTrailerLen - offset
+		if err := objstorage.Copy(ctx, r.readable, w.writable, offset, length); err != nil {
+			return 0, err
+		}
+		// Update w.meta.Size so subsequently flushed metadata has correct offsets.
+		w.meta.Size += length
+
+		for i := range blocks {
+			blocks[i].bh.Offset -= offset
+			if err := w.addIndexEntrySep(blocks[i].sep, blocks[i].bh, w.dataBlockBuf.tmp[:]); err != nil {
+				return 0, err
+			}
+		}
+	}
+
+	// TODO(dt): Copy range keys (the fact there are none is checked above).
+	// TODO(dt): Copy valblocks keys (the fact there are none is checked above).
+
+	if err := w.Close(); err != nil {
+		w = nil
+		return 0, err
+	}
+	wrote := w.meta.Size
+	w = nil
+	return wrote, nil
+}
+
+// indexEntry captures the two components of an sst index entry: the key and the
+// decoded block handle value.
+type indexEntry struct {
+	sep InternalKey
+	bh  BlockHandleWithProperties
+}
+
+// intersectingIndexEntries returns the entries from the index with separator
+// keys contained by [start, end), i.e. the subset of the sst's index that
+// intersects the provided span.
+func intersectingIndexEntries(
+	ctx context.Context, r *Reader, start, end InternalKey,
+) ([]indexEntry, error) {
+	indexH, err := r.readIndex(ctx, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer indexH.Release()
+	top, err := newBlockIter(r.Compare, r.Split, indexH.Get(), NoTransforms)
+	if err != nil {
+		return nil, err
+	}
+	defer top.Close()
+
+	var alloc bytealloc.A
+	res := make([]indexEntry, 0, r.Properties.NumDataBlocks)
+	for key, value := top.SeekGE(start.UserKey, base.SeekGEFlagsNone); key != nil; key, value = top.Next() {
+		bh, err := decodeBlockHandleWithProperties(value.InPlaceValue())
+		if err != nil {
+			return nil, err
+		}
+		if r.Properties.IndexType != twoLevelIndex {
+			entry := indexEntry{bh: bh, sep: *key}
+			alloc, entry.bh.Props = alloc.Copy(entry.bh.Props)
+			alloc, entry.sep.UserKey = alloc.Copy(entry.sep.UserKey)
+			res = append(res, entry)
+		} else {
+			subBlk, err := r.readBlock(ctx, bh.BlockHandle, nil, nil, nil, nil, nil)
+			if err != nil {
+				return nil, err
+			}
+			defer subBlk.Release() // in-loop, but it is a short loop.
+
+			sub, err := newBlockIter(r.Compare, r.Split, subBlk.Get(), NoTransforms)
+			if err != nil {
+				return nil, err
+			}
+			defer sub.Close() // in-loop, but it is a short loop.
+
+			for key, value := sub.SeekGE(start.UserKey, base.SeekGEFlagsNone); key != nil; key, value = sub.Next() {
+				bh, err := decodeBlockHandleWithProperties(value.InPlaceValue())
+				if err != nil {
+					return nil, err
+				}
+				entry := indexEntry{bh: bh, sep: *key}
+				alloc, entry.bh.Props = alloc.Copy(entry.bh.Props)
+				alloc, entry.sep.UserKey = alloc.Copy(entry.sep.UserKey)
+				res = append(res, entry)
+				if base.InternalCompare(r.Compare, end, *key) <= 0 {
+					break
+				}
+			}
+			if err := sub.Error(); err != nil {
+				return nil, err
+			}
+		}
+		if base.InternalCompare(r.Compare, end, *key) <= 0 {
+			break
+		}
+	}
+	return res, top.Error()
+}
