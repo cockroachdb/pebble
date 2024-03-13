a1f40ed8d5ff37dcbf8fe24cc2f0c9a07de92e01
David Taylor <tinystatemachine@gmail.com>
2024-03-13 14:22:36 +0000

    sstable: pull reader/writer filter check into helper

--- sstable/suffix_rewriter.go
+++ sstable/suffix_rewriter.go
@@ -272,6 +272,16 @@ func rewriteBlocks(
 	return nil
 }
 
+func checkWriterFilterMatchesReader(r *Reader, w *Writer) error {
+	if r.Properties.FilterPolicyName != w.filter.policyName() {
+		return errors.New("mismatched filters")
+	}
+	if was, is := r.Properties.ComparerName, w.props.ComparerName; was != is {
+		return errors.Errorf("mismatched Comparer %s vs %s, replacement requires same splitter to copy filters", was, is)
+	}
+	return nil
+}
+
 func rewriteDataBlocksToWriter(
 	r *Reader,
 	w *Writer,
@@ -287,11 +297,8 @@ func rewriteDataBlocksToWriter(
 	blocks := make([]blockWithSpan, len(data))
 
 	if w.filter != nil {
-		if r.Properties.FilterPolicyName != w.filter.policyName() {
-			return errors.New("mismatched filters")
-		}
-		if was, is := r.Properties.ComparerName, w.props.ComparerName; was != is {
-			return errors.Errorf("mismatched Comparer %s vs %s, replacement requires same splitter to copy filters", was, is)
+		if err := checkWriterFilterMatchesReader(r, w); err != nil {
+			return err
 		}
 	}
 
