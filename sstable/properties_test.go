4b8dec135cc528665311598760206b9b963d10b9
Nick Travers <travers@cockroachlabs.com>
2024-05-17 14:55:28 -0700

    *: allow compression type to be inferred from initial stats load
    Currently, the compression type for a table is known when the SST is new
    \- i.e. flushed, compacted, ingested. However, when opening an existing
    DB, the compression types will show as "unknown".
    
    Allow the async, background stats loading job that runs on DB open to
    infer the compression type of existing tables by lifting the compression
    field from `sstable.Properties` up into `sstable.CommonProperties`. This
    allows `loadTableStats` to use the field and populate the
    `manifest.TableStats` struct.

--- sstable/properties_test.go
+++ sstable/properties_test.go
@@ -21,15 +21,15 @@ import (
 func TestPropertiesLoad(t *testing.T) {
 	expected := Properties{
 		CommonProperties: CommonProperties{
-			NumEntries:        1727,
-			NumDeletions:      17,
-			NumRangeDeletions: 17,
-			RawKeySize:        23938,
-			RawValueSize:      1912,
+			NumEntries:         1727,
+			NumDeletions:       17,
+			NumRangeDeletions:  17,
+			RawKeySize:         23938,
+			RawValueSize:       1912,
+			CompressionName:    "Snappy",
+			CompressionOptions: "window_bits=-14; level=32767; strategy=0; max_dict_bytes=0; zstd_max_train_bytes=0; enabled=0; ",
 		},
 		ComparerName:           "leveldb.BytewiseComparator",
-		CompressionName:        "Snappy",
-		CompressionOptions:     "window_bits=-14; level=32767; strategy=0; max_dict_bytes=0; zstd_max_train_bytes=0; enabled=0; ",
 		DataSize:               13913,
 		IndexSize:              325,
 		MergerName:             "nullptr",
@@ -57,17 +57,17 @@ func TestPropertiesLoad(t *testing.T) {
 
 var testProps = Properties{
 	CommonProperties: CommonProperties{
-		NumDeletions:      15,
-		NumEntries:        16,
-		NumRangeDeletions: 18,
-		NumRangeKeyDels:   19,
-		NumRangeKeySets:   20,
-		RawKeySize:        25,
-		RawValueSize:      26,
+		NumDeletions:       15,
+		NumEntries:         16,
+		NumRangeDeletions:  18,
+		NumRangeKeyDels:    19,
+		NumRangeKeySets:    20,
+		RawKeySize:         25,
+		RawValueSize:       26,
+		CompressionName:    "compression name",
+		CompressionOptions: "compression option",
 	},
 	ComparerName:           "comparator name",
-	CompressionName:        "compression name",
-	CompressionOptions:     "compression option",
 	DataSize:               3,
 	FilterPolicyName:       "filter policy name",
 	FilterSize:             5,
@@ -93,16 +93,16 @@ func TestPropertiesSave(t *testing.T) {
 	expected := &Properties{}
 	*expected = testProps
 
-	check1 := func(expected *Properties) {
+	check1 := func(e *Properties) {
 		// Check that we can save properties and read them back.
 		var w rawBlockWriter
 		w.restartInterval = propertiesBlockRestartInterval
-		expected.save(TableFormatPebblev2, &w)
+		e.save(TableFormatPebblev2, &w)
 		var props Properties
 
 		require.NoError(t, props.load(w.finish(), 0, make(map[string]struct{})))
 		props.Loaded = nil
-		if diff := pretty.Diff(*expected, props); diff != nil {
+		if diff := pretty.Diff(*e, props); diff != nil {
 			t.Fatalf("%s", strings.Join(diff, "\n"))
 		}
 	}
@@ -116,6 +116,7 @@ func TestPropertiesSave(t *testing.T) {
 		if props.IndexPartitions == 0 {
 			props.TopLevelIndexSize = 0
 		}
+		props.Loaded = nil
 		check1(&props)
 	}
 }
