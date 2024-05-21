6995c62f2dd7a26c0d0494fedf96b991be72f1b5
Radu Berinde <radu@cockroachlabs.com>
2024-05-20 17:50:54 -0700

    sstable: handle synthetic prefix when checking bloom filter
    Fix the sstable iterators to strip the synthetic prefix before
    checking the bloom filter.
    
    Fix the `TestRandomizedPrefixSuffixRewriter` to check `SeekPrefixGE`
    and randomly enable filters.

--- sstable/reader_iter_single_lvl.go
+++ sstable/reader_iter_single_lvl.go
@@ -786,19 +786,13 @@ func (i *singleLevelIterator) seekPrefixGE(
 		}
 		i.lastBloomFilterMatched = false
 		// Check prefix bloom filter.
-		var dataH bufferHandle
-		dataH, i.err = i.reader.readFilter(i.ctx, i.indexFilterRH, i.stats, &i.iterStats)
-		if i.err != nil {
-			i.data.invalidate()
-			return nil
-		}
-		mayContain := i.reader.tableFilter.mayContain(dataH.Get(), prefix)
-		dataH.Release()
-		if !mayContain {
-			// This invalidation may not be necessary for correctness, and may
-			// be a place to optimize later by reusing the already loaded
-			// block. It was necessary in earlier versions of the code since
-			// the caller was allowed to call Next when SeekPrefixGE returned
+		var mayContain bool
+		mayContain, i.err = i.bloomFilterMayContain(prefix)
+		if i.err != nil || !mayContain {
+			// In the i.err == nil case, this invalidation may not be necessary for
+			// correctness, and may be a place to optimize later by reusing the
+			// already loaded block. It was necessary in earlier versions of the code
+			// since the caller was allowed to call Next when SeekPrefixGE returned
 			// nil. This is no longer allowed.
 			i.data.invalidate()
 			return nil
@@ -831,6 +825,27 @@ func (i *singleLevelIterator) seekPrefixGE(
 	i.boundsCmp = 0
 	i.positionedUsingLatestBounds = true
 	return i.maybeVerifyKey(i.seekGEHelper(key, boundsCmp, flags))
+}
+
+func (i *singleLevelIterator) bloomFilterMayContain(prefix []byte) (bool, error) {
+	// Check prefix bloom filter.
+	prefixToCheck := prefix
+	if i.transforms.SyntheticPrefix.IsSet() {
+		// We have to remove the synthetic prefix.
+		var ok bool
+		prefixToCheck, ok = bytes.CutPrefix(prefix, i.transforms.SyntheticPrefix)
+		if !ok {
+			// This prefix will not be found inside this table.
+			return false, nil
+		}
+	}
+
+	dataH, err := i.reader.readFilter(i.ctx, i.indexFilterRH, i.stats, &i.iterStats)
+	if err != nil {
+		return false, err
+	}
+	defer dataH.Release()
+	return i.reader.tableFilter.mayContain(dataH.Get(), prefixToCheck), nil
 }
 
 // virtualLast should only be called if i.vReader != nil.
