6320cbb149804d466847aadd925073a4ac7f0358
Jackson Owens <jackson@cockroachlabs.com>
2024-06-27 15:22:51 -0400

    sstable/colblk: add Bitmap, BitmapBuilder
    Introduce the sstable/colblk package for columnar block primitives, and add a
    first primitive for storing bitmaps.
    
    The Bitmap uses 1+1/64 physical bits per logical bit. Bits are organized into
    64-bit words, allowing constant-time access of an individual bit. Additionally,
    after the bitmap a summary bitmap consisting of 1 bit per 64-bit word of the
    bitmap provides faster lookup of preceding or successive set bits. This will be
    used by a bitmap indicating when a key prefix changes to quickly find the next
    key within a block with a new prefix.

---
+++ sstable/colblk/base.go
@@ -0,0 +1,45 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+// Package colblk implements a columnar block format.
+package colblk
+
+import "golang.org/x/exp/constraints"
+
+// align returns the next value greater than or equal to offset that's divisible
+// by val.
+func align[T constraints.Integer](offset, val T) T {
+	return (offset + val - 1) & ^(val - 1)
+}
+
+// alignWithZeroes aligns the provided offset to val, and writing zeroes to any
+// bytes in buf between the old offset and new aligned offset. This provides
+// determinism when reusing memory that has not been zeroed.
+func alignWithZeroes[T constraints.Integer](buf []byte, offset, val T) T {
+	aligned := align[T](offset, val)
+	for i := offset; i < aligned; i++ {
+		buf[i] = 0
+	}
+	return aligned
+}
+
+const (
+	align16 = 2
+	align32 = 4
+	align64 = 8
+)
+
+// When multiplying or dividing by align{16,32,64} using signed integers, it's
+// faster to shift to the left to multiply or shift to the right to divide. (The
+// compiler optimization is limited to unsigned integers.) The below constants
+// define the shift amounts corresponding to the above align constants.  (eg,
+// alignNShift = log(alignN)).
+//
+// TODO(jackson): Consider updating usages to use uints? They can be less
+// ergonomic.
+const (
+	align16Shift = 1
+	align32Shift = 2
+	align64Shift = 3
+)
