a9b0d45f8c2cf2e8078dc32e137dc1de783cd443
Radu Berinde <radu@cockroachlabs.com>
2024-07-12 08:30:27 -0700

    crl-release-23.2: objstorage: fix race in vfsSync
    We have a race in `vfsSync`: if two goroutines Sync at the same time,
    one might see the flag=false and return immediately, before the other
    goroutine actually runs/completes the Sync.
    
    The fix is to store a change counter and wait for any in-progress
    syncs as necessary.
    
    Informs https://github.com/cockroachdb/cockroach/issues/124845

--- objstorage/objstorageprovider/provider.go
+++ objstorage/objstorageprovider/provider.go
@@ -45,9 +45,14 @@ type provider struct {
 			storageObjects map[remote.Locator]remote.Storage
 		}
 
-		// localObjectsChanged is set if non-remote objects were created or deleted
-		// but Sync was not yet called.
-		localObjectsChanged bool
+		// TODO(radu): move these fields to a localLockedState struct.
+		// localObjectsChanged is incremented whenever non-remote objects are created.
+		// The purpose of this counter is to avoid syncing the local filesystem when
+		// only remote objects are changed.
+		localObjectsChangeCounter uint64
+		// localObjectsChangeCounterSynced is the value of localObjectsChangeCounter
+		// value at the time the last completed sync was launched.
+		localObjectsChangeCounterSynced uint64
 
 		// knownObjects maintains information about objects that are known to the provider.
 		// It is initialized with the list of files in the manifest when we open a DB.
@@ -550,7 +555,7 @@ func (p *provider) addMetadata(meta objstorage.ObjectMetadata) {
 			CleanupMethod:  meta.Remote.CleanupMethod,
 		})
 	} else {
-		p.mu.localObjectsChanged = true
+		p.mu.localObjectsChangeCounter++
 	}
 }
 
@@ -566,7 +571,7 @@ func (p *provider) removeMetadata(fileNum base.DiskFileNum) {
 	if meta.IsRemote() {
 		p.mu.remote.catalogBatch.DeleteObject(fileNum)
 	} else {
-		p.mu.localObjectsChanged = true
+		p.mu.localObjectsChangeCounter++
 	}
 }
 
