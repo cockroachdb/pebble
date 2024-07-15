b0d03ebddca9232fb5100f97dfc24c772dc9c947
Anish Shanbhag <anish.shanbhag@cockroachlabs.com>
2024-07-15 16:23:51 -0400

    manifest: improve `Annotator` interface with generics
    Refactors `manifest.Annotator` to use generics and a simplified API.
    This eliminates the need to perform pointer manipulation and unsafe
    typecasting when defining a new Annotator. The goal of this change is to improve the `Annotator` interface while not changing any existing behavior.
    
    `BenchmarkNumFilesAnnotator` shows a very slight speedup over master when compared
    to the equivalent implementation of `orderStatistic`:
    ```
                         │     old     │                new                 │
                         │   sec/op    │   sec/op     vs base               │
    NumFilesAnnotator-10   1.953µ ± 1%   1.618µ ± 3%  -17.15% (p=0.002 n=6)
    
                         │    old     │               new                │
                         │    B/op    │    B/op     vs base              │
    NumFilesAnnotator-10   536.0 ± 0%   544.0 ± 0%  +1.49% (p=0.002 n=6)
    
                         │    old     │                new                │
                         │ allocs/op  │ allocs/op   vs base               │
    NumFilesAnnotator-10   7.000 ± 0%   8.000 ± 0%  +14.29% (p=0.002 n=6)
    ```

---
+++ internal/manifest/annotator_test.go
@@ -0,0 +1,58 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package manifest
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/stretchr/testify/require"
+)
+
+func makeTestLevelMetadata(count int) (LevelMetadata, []*FileMetadata) {
+	files := make([]*FileMetadata, count)
+	for i := 0; i < count; i++ {
+		files[i] = newItem(key(i))
+	}
+
+	lm := MakeLevelMetadata(base.DefaultComparer.Compare, 6, files)
+	return lm, files
+}
+
+// NumFilesAnnotator is an Annotator which computes an annotation value
+// equal to the number of files included in the annotation.
+var NumFilesAnnotator = SumAnnotator(func(f *FileMetadata) (uint64, bool) {
+	return 1, true
+})
+
+func TestNumFilesAnnotator(t *testing.T) {
+	const count = 1000
+	lm, _ := makeTestLevelMetadata(0)
+
+	for i := 1; i <= count; i++ {
+		lm.tree.Insert(newItem(key(i)))
+		numFiles := NumFilesAnnotator.LevelAnnotation(lm)
+		require.EqualValues(t, i, numFiles)
+	}
+
+	numFiles := NumFilesAnnotator.LevelAnnotation(lm)
+	require.EqualValues(t, count, numFiles)
+
+	numFiles = NumFilesAnnotator.LevelAnnotation(lm)
+	require.EqualValues(t, count, numFiles)
+
+	lm.tree.Delete(newItem(key(count / 2)))
+	numFiles = NumFilesAnnotator.LevelAnnotation(lm)
+	require.EqualValues(t, count-1, numFiles)
+}
+
+func BenchmarkNumFilesAnnotator(b *testing.B) {
+	lm, _ := makeTestLevelMetadata(0)
+	for i := 1; i <= b.N; i++ {
+		lm.tree.Insert(newItem(key(i)))
+		numFiles := NumFilesAnnotator.LevelAnnotation(lm)
+		require.EqualValues(b, i, numFiles)
+	}
+}
