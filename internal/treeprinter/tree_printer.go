663738357cab75d491c18eb6f79d9c1e704f3414
Radu Berinde <radu@cockroachlabs.com>
2024-08-26 18:37:50 -0700

    treeprinter: fix license

--- internal/treeprinter/tree_printer.go
+++ internal/treeprinter/tree_printer.go
@@ -1,12 +1,6 @@
-// Copyright 2017 The Cockroach Authors.
-//
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
 
 package treeprinter
 
