4029acb41a9a5e800a7101c30e4ccbba5dee5705
Radu Berinde <radu@cockroachlabs.com>
2024-09-11 13:45:08 -0700

    testutils: add indenttree
    This commit adds some helper infrastructure to parse a multi-line
    string into a hierarchy based on indentation.

---
+++ internal/testutils/indenttree/indent_tree_test.go
@@ -0,0 +1,42 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package indenttree
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/internal/treeprinter"
+)
+
+func TestIndentTree(t *testing.T) {
+	datadriven.RunTest(t, "testdata", func(t *testing.T, d *datadriven.TestData) string {
+		switch d.Cmd {
+		case "parse":
+			nodes, err := Parse(d.Input)
+			if err != nil {
+				return fmt.Sprintf("error: %s", err)
+			}
+			tp := treeprinter.New()
+			root := tp.Child("<root>")
+			var dfs func(n Node, tp treeprinter.Node)
+			dfs = func(n Node, parent treeprinter.Node) {
+				child := parent.Child(n.Value())
+				for _, c := range n.Children() {
+					dfs(c, child)
+				}
+			}
+			for _, c := range nodes {
+				dfs(c, root)
+			}
+			return tp.String()
+
+		default:
+			t.Fatalf("unknown command: %s", d.Cmd)
+			return ""
+		}
+	})
+}
