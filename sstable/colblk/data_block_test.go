78b7785a3c6b7b04f07e5774424e0f6ccd4f04e1
Radu Berinde <radu@cockroachlabs.com>
2024-09-30 11:54:39 -0700

    colblk: implement HideObsoletePoints in DataBlockIter
    Benchmark for the no-transform path before/after:
    ```
    name                                                                                old time/op    new time/op    delta
    CockroachDataBlockIterShort/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8/Next-10           12.2ns ± 0%    12.5ns ± 3%  +2.38%  (p=0.008 n=5+5)
    CockroachDataBlockIterShort/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8/SeekGE-10          113ns ± 0%     114ns ± 0%  +0.80%  (p=0.008 n=5+5)
    CockroachDataBlockIterShort/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128/Next-10      10.1ns ± 0%    10.3ns ± 0%  +2.30%  (p=0.016 n=5+4)
    CockroachDataBlockIterShort/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128/SeekGE-10    89.4ns ± 0%    90.3ns ± 0%  +1.08%  (p=0.008 n=5+5)
    ```
    
    Transforms benchmarks:
    ```
    name                                                                                                  time/op
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8/Next-10                      12.3ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8/SeekGE-10                     113ns ± 0%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8,SynthSeqNum/Next-10          12.5ns ± 0%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8,SynthSeqNum/SeekGE-10         114ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8,HideObsolete/Next-10         13.0ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=8,Shared=4,ValueLen=8,HideObsolete/SeekGE-10        117ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128/Next-10                 10.3ns ± 0%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128/SeekGE-10               90.5ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128,SynthSeqNum/Next-10     10.5ns ± 0%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128,SynthSeqNum/SeekGE-10   90.9ns ± 1%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128,HideObsolete/Next-10    10.8ns ± 0%
    CockroachDataBlockIterTransforms/AlphaLen=8,Prefix=128,Shared=64,ValueLen=128,HideObsolete/SeekGE-10  93.9ns ± 0%
    ```

--- sstable/colblk/data_block_test.go
+++ sstable/colblk/data_block_test.go
@@ -43,12 +43,14 @@ func TestDataBlock(t *testing.T) {
 				fmt.Fprint(&buf, &w)
 				sizes = sizes[:0]
 				return buf.String()
-			case "write":
+			case "write", "write-block":
+				// write-block does init/write/finish in a single command, and doesn't
+				// print anything.
+				if td.Cmd == "write-block" {
+					w.Init(testKeysSchema)
+				}
 				for _, line := range strings.Split(td.Input, "\n") {
-					isObsolete := strings.HasSuffix(line, "obsolete")
-					if isObsolete {
-						line = strings.TrimSuffix(line, "obsolete")
-					}
+					line, isObsolete := strings.CutSuffix(line, "obsolete")
 
 					j := strings.IndexRune(line, ':')
 					ik := base.ParseInternalKey(line[:j])
@@ -62,6 +64,11 @@ func TestDataBlock(t *testing.T) {
 					v := []byte(line[j+1:])
 					w.Add(ik, v, vp, kcmp, isObsolete)
 					sizes = append(sizes, w.Size())
+				}
+				if td.Cmd == "write-block" {
+					block, _ := w.Finish(w.Rows(), w.Size())
+					r.Init(testKeysSchema, block)
+					return ""
 				}
 				fmt.Fprint(&buf, &w)
 				return buf.String()
@@ -79,6 +86,7 @@ func TestDataBlock(t *testing.T) {
 				var seqNum uint64
 				td.MaybeScanArgs(t, "synthetic-seq-num", &seqNum)
 				transforms.SyntheticSeqNum = block.SyntheticSeqNum(seqNum)
+				transforms.HideObsoletePoints = td.HasArg("hide-obsolete-points")
 				it.Init(&r, testKeysSchema.NewKeySeeker(), getLazyValuer(func([]byte) base.LazyValue {
 					return base.LazyValue{ValueOrHandle: []byte("mock external value")}
 				}), transforms)
