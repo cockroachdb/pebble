9f5a10829d2d67b9398e6516e77d307a18e8c28a
Radu Berinde <radu@cockroachlabs.com>
2024-10-11 13:55:41 -0700

    sstable: remove describe-binary
    This directive is very similar to `layout` and we don't need both.

--- sstable/colblk_writer_test.go
+++ sstable/colblk_writer_test.go
@@ -11,8 +11,6 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/datadriven"
-	"github.com/cockroachdb/pebble/internal/aligned"
-	"github.com/cockroachdb/pebble/internal/binfmt"
 	"github.com/cockroachdb/pebble/internal/testkeys"
 	"github.com/cockroachdb/pebble/objstorage"
 	"github.com/cockroachdb/pebble/sstable/block"
@@ -73,76 +71,9 @@ func TestColumnarWriter(t *testing.T) {
 				return buf.String()
 			case "props":
 				return r.Properties.String()
-			case "describe-binary":
-				f := binfmt.New(obj.Data())
-				if err := describeSSTableBinary(f, keySchema); err != nil {
-					return err.Error()
-				}
-				return f.String()
 			default:
 				panic(fmt.Sprintf("unrecognized command %q", td.Cmd))
 			}
 		})
 	})
-}
-
-func describeSSTableBinary(f *binfmt.Formatter, schema colblk.KeySchema) error {
-	layout, err := decodeLayout(testkeys.Comparer, f.Data())
-	if err != nil {
-		return err
-	}
-	blockHandles := layout.orderedBlocks()
-	for i, bh := range blockHandles {
-		if f.Offset() < int(bh.Offset) {
-			f.HexBytesln(int(bh.Offset)-f.Offset(), "???")
-		}
-		if bh.Name == "footer" {
-			// Skip the footer; we'll format it down below.
-			continue
-		}
-		f.CommentLine("block %d %s (%04d-%04d)", i, bh.Name, bh.Offset, bh.Offset+bh.Length)
-
-		if layout.Format >= TableFormatPebblev5 {
-			// We can only describe uncompressed data.
-			if block.CompressionIndicator(f.Data()[bh.Offset+bh.Length]) == block.NoCompressionIndicator {
-				switch bh.Name {
-				case "top-index", "index":
-					var d colblk.IndexBlockDecoder
-					// NB: The byte slice used to Init must be aligned (like an
-					// allocated block would be in practice).
-					d.Init(aligned.Copy(f.Data()[bh.Offset : bh.Offset+bh.Length]))
-					d.Describe(f)
-					f.HexBytesln(block.TrailerLen, "%s block trailer", bh.Name)
-					continue
-				case "data":
-					var d colblk.DataBlockDecoder
-					// NB: The byte slice used to Init must be aligned (like an
-					// allocated block would be in practice).
-					d.Init(schema, aligned.Copy(f.Data()[bh.Offset:bh.Offset+bh.Length]))
-					d.Describe(f)
-					f.HexBytesln(block.TrailerLen, "%s block trailer", bh.Name)
-					continue
-				case "range-del", "range-key":
-					var d colblk.KeyspanDecoder
-					d.Init(aligned.Copy(f.Data()[bh.Offset : bh.Offset+bh.Length]))
-					d.Describe(f)
-					f.HexBytesln(block.TrailerLen, "%s block trailer", bh.Name)
-					continue
-				case "properties":
-					f.HexTextln(int(bh.Length))
-					f.HexBytesln(block.TrailerLen, "%s block trailer", bh.Name)
-					continue
-				}
-				// Otherwise fall through.
-			}
-		}
-		// Fall back to just formatting the entire block as an opaque hex bytes.
-		f.HexBytesln(int(bh.Length), bh.Name)
-		f.HexBytesln(block.TrailerLen, "%s block trailer", bh.Name)
-	}
-	if f.Remaining() > rocksDBFooterLen {
-		f.HexBytesln(f.Remaining()-rocksDBFooterLen, "???")
-	}
-	describeFooter(f)
-	return nil
 }
