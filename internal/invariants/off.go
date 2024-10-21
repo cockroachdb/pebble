02ffc5191e793aa5889e5470eb46e3851c810622
Radu Berinde <radu@cockroachlabs.com>
2024-10-21 12:32:00 -0700

    turn on invariants for crdb test builds

--- internal/invariants/off.go
+++ internal/invariants/off.go
@@ -2,8 +2,7 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build !invariants && !race
-// +build !invariants,!race
+//go:build !invariants && !race && (!crdb_test || crdb_test_off)
 
 package invariants
 
