767aadd907e8ecaf955d1be39a8a444d25c203d1
Radu Berinde <radu@cockroachlabs.com>
2024-11-14 05:54:33 -0800

    cache: simplify value code
    We have three tag-dependent file variants for allocating and freeing
    `Value`.  This commit greatly simplifies this code using constants
    instead of build tags and consolidating the non-cgo and the invariant
    implementations.

--- internal/cache/value.go
+++ internal/cache/value.go
@@ -4,7 +4,15 @@
 
 package cache
 
-import "unsafe"
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"github.com/cockroachdb/pebble/internal/buildtags"
+	"github.com/cockroachdb/pebble/internal/invariants"
+	"github.com/cockroachdb/pebble/internal/manual"
+)
 
 // ValueMetadataSize denotes the number of bytes of metadata allocated for a
 // cache entry. Note that builds with cgo disabled allocate no metadata, and
@@ -18,10 +26,70 @@ var _ uint = ValueMetadataSize - uint(unsafe.Sizeof(Value{}))
 
 // Value holds a reference counted immutable value.
 type Value struct {
+	// buf is allocated using the manual package.
 	buf []byte
 	// Reference count for the value. The value is freed when the reference count
 	// drops to zero.
 	ref refcnt
+}
+
+// The Value struct is normally allocated together with the buffer using manual
+// memory.
+//
+// If cgo is not available, the Value must be a normal Go object to keep
+// the buffer reference visible to the GC. We also use the Go allocator if we
+// want to add finalizer assertions.
+const valueEntryGoAllocated = !buildtags.Cgo || invariants.UseFinalizers
+
+func newValue(n int) *Value {
+	if n == 0 {
+		return nil
+	}
+
+	if valueEntryGoAllocated {
+		// Note: if cgo is not enabled, manual.New will do a regular Go allocation.
+		b := manual.New(manual.BlockCacheData, n)
+		v := &Value{buf: b}
+		v.ref.init(1)
+		// Note: this is a no-op if invariants and tracing are disabled or race is
+		// enabled.
+		invariants.SetFinalizer(v, func(obj interface{}) {
+			v := obj.(*Value)
+			if v.buf != nil {
+				fmt.Fprintf(os.Stderr, "%p: cache value was not freed: refs=%d\n%s",
+					v, v.refs(), v.ref.traces())
+				os.Exit(1)
+			}
+		})
+		return v
+	}
+
+	// When we're not performing leak detection, the lifetime of the returned
+	// Value is exactly the lifetime of the backing buffer and we can manually
+	// allocate both.
+	b := manual.New(manual.BlockCacheData, ValueMetadataSize+n)
+	v := (*Value)(unsafe.Pointer(&b[0]))
+	v.buf = b[ValueMetadataSize:]
+	v.ref.init(1)
+	return v
+}
+
+func (v *Value) free() {
+	if invariants.Enabled {
+		// Poison the contents to help catch use-after-free bugs.
+		for i := range v.buf {
+			v.buf[i] = 0xff
+		}
+	}
+	if valueEntryGoAllocated {
+		manual.Free(manual.BlockCacheData, v.buf)
+		v.buf = nil
+		return
+	}
+	n := ValueMetadataSize + cap(v.buf)
+	buf := unsafe.Slice((*byte)(unsafe.Pointer(v)), n)
+	v.buf = nil
+	manual.Free(manual.BlockCacheData, buf)
 }
 
 // Buf returns the buffer associated with the value. The contents of the buffer
