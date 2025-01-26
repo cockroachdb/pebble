cockroachdb/pebble

commit 8a1bd363ba6a3fe2c96898c04cd29d45bd49ad12

metamorphic: add KeyFormat type
Refactor the metamorphic test to pull the particulars of the key format and
into a KeyFormat type. This will be used to allow the metamorphic test to use
other key formats, such as cockroachkvs.

metamorphic/generator.go
@@ -27,7 +27,7 @@ type iterOpts struct {
 	// configure IterOptions.RangeKeyMasking.Suffix.
 	maskSuffix []byte
 
-	// If filterMax is >0, this iterator will filter out any keys that have
+	// If filterMax is != nil, this iterator will filter out any keys that have
 	// suffixes that don't fall within the range [filterMin,filterMax).
 	// Additionally, the iterator will be constructed with a block-property
 	// filter that filters out blocks accordingly. Not all OPTIONS hook up the
@@ -35,8 +35,8 @@ type iterOpts struct {
 	// effectively disabled in some runs. The iterator operations themselves
 	// however will always skip past any points that should be filtered to
 	// ensure determinism.
-	filterMin uint64
-	filterMax uint64
+	filterMin []byte
+	filterMax []byte
 
 	// see IterOptions.UseL6Filters.
 	useL6Filters bool
@@ -46,7 +46,7 @@ type iterOpts struct {
 
 func (o iterOpts) IsZero() bool {
 	return o.lower == nil && o.upper == nil && o.keyTypes == 0 &&
-		o.maskSuffix == nil && o.filterMin == 0 && o.filterMax == 0 && !o.useL6Filters
+		o.maskSuffix == nil && o.filterMin == nil && o.filterMax == nil && !o.useL6Filters
 }
 
 // GenerateOps generates n random operations, drawing randomness from the
@@ -67,7 +67,7 @@ type generator struct {
 
 	// keyManager tracks the state of keys a operation generation time.
 	keyManager   *keyManager
-	keyGenerator *keyGenerator
+	keyGenerator KeyGenerator
 	dbs          objIDSlice
 	// Unordered sets of object IDs for live objects. Used to randomly select on
 	// object when generating an operation. There are 4 concrete objects: the DB
@@ -119,7 +119,7 @@ type generator struct {
 }
 
 func newGenerator(rng *rand.Rand, cfg OpConfig, km *keyManager) *generator {
-	keyGenerator := newKeyGenerator(km, rng, cfg)
+	keyGenerator := km.kf.NewGenerator(km, rng, cfg)
 	g := &generator{
 		cfg:             cfg,
 		rng:             rng,
@@ -531,13 +531,7 @@ func (g *generator) newIter() {
 	// block-property filtering and explicitly within the iterator operations to
 	// ensure determinism.
 	if g.rng.Float64() <= 0.1 {
-		opts.filterMin = uint64(g.keyGenerator.UniformSuffixInt() + 1)
-		opts.filterMax = uint64(g.keyGenerator.UniformSuffixInt() + 1)
-		if opts.filterMin > opts.filterMax {
-			opts.filterMin, opts.filterMax = opts.filterMax, opts.filterMin
-		} else if opts.filterMin == opts.filterMax {
-			opts.filterMax++
-		}
+		opts.filterMin, opts.filterMax = g.keyGenerator.SuffixRange()
 	}
 
 	// Enable L6 filters with a 10% probability.
@@ -839,7 +833,7 @@ func (g *generator) iterSeekPrefixGE(iterID objID) {
 	}
 	// Sometimes limit the key to just the prefix.
 	if g.rng.IntN(3) == 1 {
-		key = g.keyManager.comparer.Split.Prefix(key)
+		key = g.keyManager.kf.Comparer.Split.Prefix(key)
 	}
 	g.add(&iterSeekPrefixGEOp{
 		iterID:          iterID,
@@ -1527,18 +1521,12 @@ func (g *generator) maybeMutateOptions(readerID objID, opts *iterOpts) {
 		}
 
 		// With 1/3 probability, clear existing filter.
-		if opts.filterMax > 0 && g.rng.IntN(3) == 0 {
-			opts.filterMax, opts.filterMin = 0, 0
+		if opts.filterMax != nil && g.rng.IntN(3) == 0 {
+			opts.filterMax, opts.filterMin = nil, nil
 		}
 		// With 10% probability, set a filter range.
 		if g.rng.IntN(10) == 1 {
-			opts.filterMin = uint64(g.keyGenerator.UniformSuffixInt() + 1)
-			opts.filterMax = uint64(g.keyGenerator.UniformSuffixInt() + 1)
-			if opts.filterMin > opts.filterMax {
-				opts.filterMin, opts.filterMax = opts.filterMax, opts.filterMin
-			} else if opts.filterMin == opts.filterMax {
-				opts.filterMax = opts.filterMin + 1
-			}
+			opts.filterMin, opts.filterMax = g.keyGenerator.SuffixRange()
 		}
 		// With 10% probability, flip enablement of L6 filters.
 		if g.rng.Float64() <= 0.1 {
@@ -1548,11 +1536,11 @@ func (g *generator) maybeMutateOptions(readerID objID, opts *iterOpts) {
 }
 
 func (g *generator) cmp(a, b []byte) int {
-	return g.keyManager.comparer.Compare(a, b)
+	return g.keyManager.kf.Comparer.Compare(a, b)
 }
 
 func (g *generator) prefix(a []byte) []byte {
-	n := g.keyManager.comparer.Split(a)
+	n := g.keyManager.kf.Comparer.Split(a)
 	return a[:n:n]
 }
 

metamorphic/parser.go
@@ -331,6 +331,14 @@ func (p *parser) parseArgs(op op, methodName string, args []interface{}) {
 			}
 			*t = val
 
+		case *UserKey:
+			elem.expectToken(p, token.STRING)
+			*t = unquoteBytes(elem.lit)
+
+		case *UserKeySuffix:
+			elem.expectToken(p, token.STRING)
+			*t = unquoteBytes(elem.lit)
+
 		case *[]byte:
 			elem.expectToken(p, token.STRING)
 			*t = unquoteBytes(elem.lit)

metamorphic/testkeys.go
@@ -12,35 +12,55 @@ import (
 
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/internal/testkeys"
+	"github.com/cockroachdb/pebble/sstable"
 )
 
-type keyGenerator struct {
+var TestkeysKeyFormat = KeyFormat{
+	Comparer: testkeys.Comparer,
+	NewGenerator: func(km *keyManager, rng *rand.Rand, cfg OpConfig) KeyGenerator {
+		return &testkeyKeyGenerator{
+			keyManager: km,
+			rng:        rng,
+			cfg:        cfg,
+		}
+	},
+	NewSuffixFilterMask: func() pebble.BlockPropertyFilterMask {
+		return sstable.NewTestKeysMaskingFilter()
+	},
+	NewSuffixBlockPropertyFilter: func(filterMin, filterMax []byte) sstable.BlockPropertyFilter {
+		low, err := testkeys.ParseSuffix(filterMin)
+		if err != nil {
+			panic(err)
+		}
+		high, err := testkeys.ParseSuffix(filterMax)
+		if err != nil {
+			panic(err)
+		}
+		return sstable.NewTestKeysBlockPropertyFilter(uint64(low), uint64(high))
+	},
+}
+
+type testkeyKeyGenerator struct {
 	keyManager *keyManager
 	rng        *rand.Rand
 	cfg        OpConfig
 }
 
-func newKeyGenerator(km *keyManager, rng *rand.Rand, cfg OpConfig) *keyGenerator {
-	return &keyGenerator{
-		keyManager: km,
-		rng:        rng,
-		cfg:        cfg,
-	}
-}
-
 // RandKey returns a random key (either a previously known key, or a new key).
-func (kg *keyGenerator) RandKey(newKeyProbability float64) []byte {
+func (kg *testkeyKeyGenerator) RandKey(newKeyProbability float64) []byte {
 	return kg.randKey(newKeyProbability, nil /* bounds */)
 }
 
 // RandKeyInRange returns a random key (either a previously known key, or a new
 // key) in the given key range.
-func (kg *keyGenerator) RandKeyInRange(newKeyProbability float64, kr pebble.KeyRange) []byte {
+func (kg *testkeyKeyGenerator) RandKeyInRange(
+	newKeyProbability float64, kr pebble.KeyRange,
+) []byte {
 	return kg.randKey(newKeyProbability, &kr)
 }
 
 // RandPrefix returns a random prefix key (a key with no suffix).
-func (kg *keyGenerator) RandPrefix(newPrefix float64) []byte {
+func (kg *testkeyKeyGenerator) RandPrefix(newPrefix float64) []byte {
 	prefixes := kg.keyManager.prefixes()
 	if len(prefixes) > 0 && kg.rng.Float64() > newPrefix {
 		return pickOneUniform(kg.rng, prefixes)
@@ -60,7 +80,7 @@ func (kg *keyGenerator) RandPrefix(newPrefix float64) []byte {
 
 // UniqueKeys takes a key-generating function and uses it to generate n unique
 // keys, returning them in sorted order.
-func (kg *keyGenerator) UniqueKeys(n int, genFn func() []byte) [][]byte {
+func (kg *testkeyKeyGenerator) UniqueKeys(n int, genFn func() []byte) [][]byte {
 	keys := make([][]byte, n)
 	used := make(map[string]struct{}, n)
 	for i := range keys {
@@ -86,16 +106,16 @@ func (kg *keyGenerator) UniqueKeys(n int, genFn func() []byte) [][]byte {
 //
 // May return a nil suffix, with the probability the configuration's suffix
 // distribution assigns to the zero suffix.
-func (kg *keyGenerator) SkewedSuffix(incMaxProb float64) []byte {
-	if suffix := kg.SkewedSuffixInt(incMaxProb); suffix != 0 {
+func (kg *testkeyKeyGenerator) SkewedSuffix(incMaxProb float64) []byte {
+	if suffix := kg.skewedSuffixInt(incMaxProb); suffix != 0 {
 		return testkeys.Suffix(suffix)
 	}
 	return nil
 }
 
-// SkewedSuffixInt is a variant of SkewedSuffix which returns the unencoded
+// skewedSuffixInt is a helper of SkewedSuffix which returns the unencoded
 // suffix as an integer.
-func (kg *keyGenerator) SkewedSuffixInt(incMaxProb float64) int64 {
+func (kg *testkeyKeyGenerator) skewedSuffixInt(incMaxProb float64) int64 {
 	if kg.rng.Float64() < incMaxProb {
 		kg.cfg.writeSuffixDist.IncMax(1)
 	}
@@ -105,9 +125,20 @@ func (kg *keyGenerator) SkewedSuffixInt(incMaxProb float64) int64 {
 // IncMaxSuffix increases the max suffix range and returns the new maximum
 // suffix (which is guaranteed to be larger than any previously generated
 // suffix).
-func (kg *keyGenerator) IncMaxSuffix() []byte {
+func (kg *testkeyKeyGenerator) IncMaxSuffix() []byte {
 	kg.cfg.writeSuffixDist.IncMax(1)
 	return testkeys.Suffix(int64(kg.cfg.writeSuffixDist.Max()))
+}
+
+func (kg *testkeyKeyGenerator) SuffixRange() (low, high []byte) {
+	a := kg.uniformSuffixInt()
+	b := kg.uniformSuffixInt()
+	if a > b {
+		a, b = b, a
+	} else if a == b {
+		b++
+	}
+	return testkeys.Suffix(a), testkeys.Suffix(b)
 }
 
 // UniformSuffix returns a suffix in the same range as SkewedSuffix but with a
@@ -115,16 +146,16 @@ func (kg *keyGenerator) IncMaxSuffix() []byte {
 // mix of older and newer keys. The suffix can be empty.
 //
 // May return a nil suffix.
-func (kg *keyGenerator) UniformSuffix() []byte {
-	if suffix := kg.UniformSuffixInt(); suffix != 0 {
+func (kg *testkeyKeyGenerator) UniformSuffix() []byte {
+	if suffix := kg.uniformSuffixInt(); suffix != 0 {
 		return testkeys.Suffix(suffix)
 	}
 	return nil
 }
 
-// UniformSuffixInt is a variant of UniformSuffix which returns the unencoded
+// uniformSuffixInt is a helper of UniformSuffix which returns the unencoded
 // suffix as an integer.
-func (kg *keyGenerator) UniformSuffixInt() int64 {
+func (kg *testkeyKeyGenerator) uniformSuffixInt() int64 {
 	maxVal := kg.cfg.writeSuffixDist.Max()
 	return kg.rng.Int64N(int64(maxVal))
 }
@@ -132,7 +163,7 @@ func (kg *keyGenerator) UniformSuffixInt() int64 {
 // randKey returns a random key (either a previously known key or a new key).
 //
 // If bounds is not nil, the key will be inside the bounds.
-func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRange) []byte {
+func (kg *testkeyKeyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRange) []byte {
 	var knownKeys [][]byte
 	if bounds == nil {
 		knownKeys = kg.keyManager.knownKeys()
@@ -192,7 +223,7 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 	}
 
 	if bounds == nil {
-		suffix := kg.SkewedSuffixInt(0.01)
+		suffix := kg.skewedSuffixInt(0.01)
 		for {
 			key := kg.generateKeyWithSuffix(4, 12, suffix)
 			if !kg.keyManager.prefixExists(kg.prefix(key)) {
@@ -214,7 +245,7 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 		if cmpSuffix(startSuffix, endSuffix) >= 0 {
 			panic(fmt.Sprintf("invalid bounds [%q, %q)", bounds.Start, bounds.End))
 		}
-		suffix = kg.SkewedSuffixInt(0.01)
+		suffix = kg.skewedSuffixInt(0.01)
 		for i := 0; !(cmpSuffix(startSuffix, suffix) <= 0 && cmpSuffix(suffix, endSuffix) < 0); i++ {
 			if i > 10 {
 				// This value is always >= startSuffix and < endSuffix.
@@ -223,11 +254,11 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 			}
 			// The suffix we want must exist in the current suffix range, we don't
 			// want to keep increasing it here.
-			suffix = kg.SkewedSuffixInt(0)
+			suffix = kg.skewedSuffixInt(0)
 		}
 	} else {
 		prefix = testkeys.RandomPrefixInRange(startPrefix, endPrefix, kg.rng)
-		suffix = kg.SkewedSuffixInt(0.01)
+		suffix = kg.skewedSuffixInt(0.01)
 		if kg.equal(prefix, startPrefix) {
 			// We can't use a suffix which sorts before startSuffix.
 			for i := 0; cmpSuffix(suffix, startSuffix) < 0; i++ {
@@ -235,7 +266,7 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 					suffix = startSuffix
 					break
 				}
-				suffix = kg.SkewedSuffixInt(0)
+				suffix = kg.skewedSuffixInt(0)
 			}
 		}
 	}
@@ -244,7 +275,8 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 		key = append(key, testkeys.Suffix(suffix)...)
 	}
 	if kg.cmp(key, bounds.Start) < 0 || kg.cmp(key, bounds.End) >= 0 {
-		panic(fmt.Sprintf("invalid randKey %q; bounds: [%q, %q) %v %v", key, bounds.Start, bounds.End, kg.cmp(key, bounds.Start), kg.cmp(key, bounds.End)))
+		panic(fmt.Sprintf("invalid randKey %q; bounds: [%q, %q) %v %v",
+			key, bounds.Start, bounds.End, kg.cmp(key, bounds.Start), kg.cmp(key, bounds.End)))
 	}
 	// We might (rarely) produce an existing key here, that's ok.
 	kg.keyManager.addNewKey(key)
@@ -253,7 +285,9 @@ func (kg *keyGenerator) randKey(newKeyProbability float64, bounds *pebble.KeyRan
 
 // generateKeyWithSuffix generates a key with a random prefix and the given
 // suffix. If the given suffix is 0, the key will not have a suffix.
-func (kg *keyGenerator) generateKeyWithSuffix(minPrefixLen, maxPrefixLen int, suffix int64) []byte {
+func (kg *testkeyKeyGenerator) generateKeyWithSuffix(
+	minPrefixLen, maxPrefixLen int, suffix int64,
+) []byte {
 	prefix := randBytes(kg.rng, minPrefixLen, maxPrefixLen)
 	if suffix == 0 {
 		return prefix
@@ -275,24 +309,24 @@ func cmpSuffix(s1, s2 int64) int {
 	}
 }
 
-func (kg *keyGenerator) cmp(a, b []byte) int {
-	return kg.keyManager.comparer.Compare(a, b)
+func (kg *testkeyKeyGenerator) cmp(a, b []byte) int {
+	return kg.keyManager.kf.Comparer.Compare(a, b)
 }
 
-func (kg *keyGenerator) equal(a, b []byte) bool {
-	return kg.keyManager.comparer.Equal(a, b)
+func (kg *testkeyKeyGenerator) equal(a, b []byte) bool {
+	return kg.keyManager.kf.Comparer.Equal(a, b)
 }
 
-func (kg *keyGenerator) split(a []byte) int {
-	return kg.keyManager.comparer.Split(a)
+func (kg *testkeyKeyGenerator) split(a []byte) int {
+	return kg.keyManager.kf.Comparer.Split(a)
 }
 
-func (kg *keyGenerator) prefix(a []byte) []byte {
+func (kg *testkeyKeyGenerator) prefix(a []byte) []byte {
 	n := kg.split(a)
 	return a[:n:n]
 }
 
-func (kg *keyGenerator) parseKey(k []byte) (prefix []byte, suffix int64) {
+func (kg *testkeyKeyGenerator) parseKey(k []byte) (prefix []byte, suffix int64) {
 	n := kg.split(k)
 	if n == len(k) {
 		return k, 0

internal/metamorphic/reduce_test.go
@@ -16,6 +16,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/cockroachdb/pebble/internal/testkeys"
 	"github.com/cockroachdb/pebble/metamorphic"
 	"github.com/stretchr/testify/require"
 )
@@ -176,7 +177,7 @@ func (r *reducer) try(t *testing.T, ops []string) bool {
 	t.Logf("  Log: %v", logFile)
 
 	// Try to generate a diagram.
-	diagram, err := metamorphic.TryToGenerateDiagram([]byte(strings.Join(ops, "\n")))
+	diagram, err := metamorphic.TryToGenerateDiagram(testkeys.Comparer, []byte(strings.Join(ops, "\n")))
 	require.NoError(t, err)
 	if diagram != "" {
 		diagramPath := filepath.Join(testRootDir, "diagram")
@@ -210,7 +211,7 @@ func (r *reducer) Run(t *testing.T) {
 	// Try to simplify the keys.
 	opsData := []byte(strings.Join(ops, "\n"))
 	for _, retainSuffixes := range []bool{false, true} {
-		newOpsData := metamorphic.TryToSimplifyKeys(opsData, retainSuffixes)
+		newOpsData := metamorphic.TryToSimplifyKeys(testkeys.Comparer, opsData, retainSuffixes)
 		o := strings.Split(strings.TrimSpace(string(newOpsData)), "\n")
 		if r.try(t, o) {
 			return

metamorphic/config.go
@@ -4,7 +4,14 @@
 
 package metamorphic
 
-import "github.com/cockroachdb/pebble/internal/randvar"
+import (
+	"math/rand/v2"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/randvar"
+	"github.com/cockroachdb/pebble/sstable"
+)
 
 // OpType is an enum of possible operation types.
 type OpType int
@@ -330,4 +337,65 @@ func mustDynamic(dyn randvar.Dynamic, err error) randvar.Dynamic {
 		panic(err)
 	}
 	return dyn
+}
+
+var knownKeyFormats = []KeyFormat{
+	TestkeysKeyFormat,
+}
+
+var keyFormatsByName = func() map[string]KeyFormat {
+	m := make(map[string]KeyFormat)
+	for _, kf := range knownKeyFormats {
+		m[kf.Name] = kf
+	}
+	return m
+}()
+
+// A KeyFormat dictates the format of key-value pairs uses by the metamorphic
+// test.
+type KeyFormat struct {
+	Name                         string
+	Comparer                     *base.Comparer
+	NewGenerator                 func(*keyManager, *rand.Rand, OpConfig) KeyGenerator
+	NewSuffixFilterMask          func() pebble.BlockPropertyFilterMask
+	NewSuffixBlockPropertyFilter func(min []byte, max []byte) sstable.BlockPropertyFilter
+}
+
+// KeyGenerator is an interface for generating keys, prefixes and suffixes.
+type KeyGenerator interface {
+	// IncMaxSuffix increases the max suffix range and returns the new maximum
+	// suffix (which is guaranteed to be larger than any previously generated
+	// suffix).
+	IncMaxSuffix() []byte
+	// RandKey returns a random key (either a previously known key, or a new
+	// key). The provided probability determines the likelihood of generating a
+	// new key.
+	RandKey(newKeyProbability float64) []byte
+	// RandKeyInRange returns a random key (either a previously known key, or a
+	// new key) in the given key range.
+	RandKeyInRange(newKeyProbability float64, kr pebble.KeyRange) []byte
+	// RandPrefix returns a random prefix key (a key with no suffix). The
+	// provided probability determines the likelihood of generating a new
+	// prefix.
+	RandPrefix(newPrefix float64) []byte
+	// SkewedSuffix generates a random suffix according to the configuration's
+	// suffix distribution. It takes a probability 0 ≤ p ≤ 1.0 indicating the
+	// probability with which the generator should increase the max suffix
+	// generated by the generator.
+	//
+	// May return a nil suffix, with the probability the configuration's suffix
+	// distribution assigns to the zero suffix.
+	SkewedSuffix(incMaxProb float64) []byte
+	// SuffixRange generates a new uniformly random range of suffixes [low,
+	// high) such that high is guaranteed to be strictly greater than low.
+	SuffixRange() (low, high []byte)
+	// UniformSuffix returns a suffix in the same range as SkewedSuffix but with
+	// a uniform distribution. This is used during reads to better exercise
+	// reading a mix of older and newer keys. The suffix can be empty.
+	//
+	// May return a nil suffix.
+	UniformSuffix() []byte
+	// UniqueKeys takes a key-generating function and uses it to generate n
+	// unique keys, returning them in sorted order.
+	UniqueKeys(n int, genFn func() []byte) [][]byte
 }

metamorphic/diagram.go
@@ -4,14 +4,18 @@
 
 package metamorphic
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/cockroachdb/pebble/internal/base"
+)
 
 // TryToGenerateDiagram attempts to generate a user-readable ASCII diagram of
 // the keys involved in the operations.
 //
 // If the diagram would be too large to be practical, returns the empty string
 // (with no error).
-func TryToGenerateDiagram(opsData []byte) (string, error) {
+func TryToGenerateDiagram(comparer *base.Comparer, opsData []byte) (string, error) {
 	ops, err := parse(opsData, parserOpts{})
 	if err != nil {
 		return "", err
@@ -29,7 +33,7 @@ func TryToGenerateDiagram(opsData []byte) (string, error) {
 	if len(keySet) == 0 {
 		return "", nil
 	}
-	keys := sortedKeys(keySet)
+	keys := sortedKeys(comparer.Compare, keySet)
 	axis1, axis2, pos := genAxis(keys)
 	if len(axis1) > 200 {
 		return "", nil

metamorphic/diagram_test.go
@@ -9,13 +9,14 @@ import (
 	"testing"
 
 	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/internal/testkeys"
 )
 
 func TestDiagram(t *testing.T) {
 	datadriven.RunTest(t, "testdata/diagram", func(t *testing.T, d *datadriven.TestData) string {
 		switch d.Cmd {
 		case "diagram":
-			res, err := TryToGenerateDiagram([]byte(d.Input))
+			res, err := TryToGenerateDiagram(testkeys.Comparer, []byte(d.Input))
 			if err != nil {
 				return err.Error()
 			}

commit de73a4b769e11dc47bedf647a4c08832d6b2ce00

metamorphic: fix time-bound filtering
In 8a1bd363 the filterMin and filterMax fields were converted to encoded
suffixes so that they could accommodate more complex representations of
timestamps, including those used by the CockroachDB key format. The commit in
8a1bd363 inadvertently inverted the meaning of filterMin and filterMax due to
the descending ordering of timestamp suffixes. This accidentally inverted the
comparisons in SkipPoint, resulting in excessive filtering.

This commit defines filterMin as the suffix that sorts smaller [the one with a
larger timestamp], and filterMax as the suffix that sorts larger [the one with
a smaller timestamp]. The ordering of these fields in serialized ops is
reversed so that parsing an ops file produced by a previous Pebble version
populates the appropriate field.

Assertions and comments are updated to clarify the ordering of filterMin,
filterMax throughput.

metamorphic/generator.go
@@ -29,7 +29,12 @@ type iterOpts struct {
 	maskSuffix UserKeySuffix
 
 	// If filterMax is != nil, this iterator will filter out any keys that have
-	// suffixes that don't fall within the range [filterMin,filterMax).
+	// suffixes that don't fall within the range (filterMin,filterMax]
+	// [according to the ordering defined by ComparePointSuffixes]. Note that
+	// suffixes are used to represent MVCC timestamps, and because MVCC
+	// timestamps are ordered in descending order, the timestamp associated with
+	// filterMin > filterMax.
+	//
 	// Additionally, the iterator will be constructed with a block-property
 	// filter that filters out blocks accordingly. Not all OPTIONS hook up the
 	// corresponding block property collector, so block-filtering may still be
@@ -1529,7 +1534,7 @@ func (g *generator) maybeMutateOptions(readerID objID, opts *iterOpts) {
 		}
 
 		// With 1/3 probability, clear existing filter.
-		if opts.filterMax != nil && g.rng.IntN(3) == 0 {
+		if opts.filterMin != nil && g.rng.IntN(3) == 0 {
 			opts.filterMax, opts.filterMin = nil, nil
 		}
 		// With 10% probability, set a filter range.

metamorphic/ops.go
@@ -1201,7 +1201,7 @@ func (o *newIterOp) run(t *Test, h historyRecorder) {
 func (o *newIterOp) formattedString(kf KeyFormat) string {
 	return fmt.Sprintf("%s = %s.NewIter(%q, %q, %d /* key types */, %q, %q, %t /* use L6 filters */, %q /* masking suffix */)",
 		o.iterID, o.readerID, kf.FormatKey(o.lower), kf.FormatKey(o.upper),
-		o.keyTypes, kf.FormatKeySuffix(o.filterMin), kf.FormatKeySuffix(o.filterMax),
+		o.keyTypes, kf.FormatKeySuffix(o.filterMax), kf.FormatKeySuffix(o.filterMin),
 		o.useL6Filters, kf.FormatKeySuffix(o.maskSuffix))
 }
 
@@ -1268,8 +1268,8 @@ func (o *newIterUsingCloneOp) run(t *Test, h historyRecorder) {
 func (o *newIterUsingCloneOp) formattedString(kf KeyFormat) string {
 	return fmt.Sprintf("%s = %s.Clone(%t, %q, %q, %d /* key types */, %q, %q, %t /* use L6 filters */, %q /* masking suffix */)",
 		o.iterID, o.existingIterID, o.refreshBatch, kf.FormatKey(o.lower),
-		kf.FormatKey(o.upper), o.keyTypes, kf.FormatKeySuffix(o.filterMin),
-		kf.FormatKeySuffix(o.filterMax), o.useL6Filters, kf.FormatKeySuffix(o.maskSuffix))
+		kf.FormatKey(o.upper), o.keyTypes, kf.FormatKeySuffix(o.filterMax),
+		kf.FormatKeySuffix(o.filterMin), o.useL6Filters, kf.FormatKeySuffix(o.maskSuffix))
 }
 
 func (o *newIterUsingCloneOp) receiver() objID { return o.existingIterID }
@@ -1365,7 +1365,7 @@ func (o *iterSetOptionsOp) run(t *Test, h historyRecorder) {
 func (o *iterSetOptionsOp) formattedString(kf KeyFormat) string {
 	return fmt.Sprintf("%s.SetOptions(%q, %q, %d /* key types */, %q, %q, %t /* use L6 filters */, %q /* masking suffix */)",
 		o.iterID, kf.FormatKey(o.lower), kf.FormatKey(o.upper),
-		o.keyTypes, kf.FormatKeySuffix(o.filterMin), kf.FormatKeySuffix(o.filterMax),
+		o.keyTypes, kf.FormatKeySuffix(o.filterMax), kf.FormatKeySuffix(o.filterMin),
 		o.useL6Filters, kf.FormatKeySuffix(o.maskSuffix))
 }
 
@@ -1393,9 +1393,13 @@ func iterOptions(kf KeyFormat, o iterOpts) *pebble.IterOptions {
 	if opts.RangeKeyMasking.Suffix != nil {
 		opts.RangeKeyMasking.Filter = kf.NewSuffixFilterMask
 	}
-	if o.filterMax != nil {
+	if o.filterMin != nil {
 		opts.PointKeyFilters = []pebble.BlockPropertyFilter{
 			kf.NewSuffixBlockPropertyFilter(o.filterMin, o.filterMax),
+		}
+		if o.filterMin != nil && o.filterMax != nil &&
+			kf.Comparer.ComparePointSuffixes(o.filterMin, o.filterMax) >= 0 {
+			panic(errors.AssertionFailedf("filterMin >= filterMax: %q >= %q", o.filterMin, o.filterMax))
 		}
 		// Enforce the timestamp bounds in SkipPoint, so that the iterator never
 		// returns a key outside the filterMin, filterMax bounds. This provides
@@ -1410,10 +1414,10 @@ func iterOptions(kf KeyFormat, o iterOpts) *pebble.IterOptions {
 				// non-timestamped keys.
 				return true
 			}
-			if kf.Comparer.ComparePointSuffixes(k[n:], o.filterMin) < 0 {
+			if o.filterMin != nil && kf.Comparer.ComparePointSuffixes(k[n:], o.filterMin) <= 0 {
 				return true
 			}
-			if kf.Comparer.ComparePointSuffixes(k[n:], o.filterMax) >= 0 {
+			if o.filterMax != nil && kf.Comparer.ComparePointSuffixes(k[n:], o.filterMax) > 0 {
 				return true
 			}
 			return false

metamorphic/parser.go
@@ -93,9 +93,9 @@ func opArgs(op op) (receiverID *objID, targetID *objID, args []interface{}) {
 	case *newIndexedBatchOp:
 		return &t.dbID, &t.batchID, nil
 	case *newIterOp:
-		return &t.readerID, &t.iterID, []interface{}{&t.lower, &t.upper, &t.keyTypes, &t.filterMin, &t.filterMax, &t.useL6Filters, &t.maskSuffix}
+		return &t.readerID, &t.iterID, []interface{}{&t.lower, &t.upper, &t.keyTypes, &t.filterMax, &t.filterMin, &t.useL6Filters, &t.maskSuffix}
 	case *newIterUsingCloneOp:
-		return &t.existingIterID, &t.iterID, []interface{}{&t.refreshBatch, &t.lower, &t.upper, &t.keyTypes, &t.filterMin, &t.filterMax, &t.useL6Filters, &t.maskSuffix}
+		return &t.existingIterID, &t.iterID, []interface{}{&t.refreshBatch, &t.lower, &t.upper, &t.keyTypes, &t.filterMax, &t.filterMin, &t.useL6Filters, &t.maskSuffix}
 	case *newSnapshotOp:
 		return &t.dbID, &t.snapID, []interface{}{&t.bounds}
 	case *newExternalObjOp:
@@ -119,7 +119,7 @@ func opArgs(op op) (receiverID *objID, targetID *objID, args []interface{}) {
 	case *iterSetBoundsOp:
 		return &t.iterID, nil, []interface{}{&t.lower, &t.upper}
 	case *iterSetOptionsOp:
-		return &t.iterID, nil, []interface{}{&t.lower, &t.upper, &t.keyTypes, &t.filterMin, &t.filterMax, &t.useL6Filters, &t.maskSuffix}
+		return &t.iterID, nil, []interface{}{&t.lower, &t.upper, &t.keyTypes, &t.filterMax, &t.filterMin, &t.useL6Filters, &t.maskSuffix}
 	case *singleDeleteOp:
 		return &t.writerID, nil, []interface{}{&t.key, &t.maybeReplaceDelete}
 	case *rangeKeyDeleteOp:

metamorphic/testkeys.go
@@ -10,6 +10,7 @@ import (
 	"math/rand/v2"
 	"slices"
 
+	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/internal/testkeys"
 	"github.com/cockroachdb/pebble/sstable"
@@ -39,15 +40,26 @@ var TestkeysKeyFormat = KeyFormat{
 		return sstable.NewTestKeysMaskingFilter()
 	},
 	NewSuffixBlockPropertyFilter: func(filterMin, filterMax []byte) sstable.BlockPropertyFilter {
-		low, err := testkeys.ParseSuffix(filterMin)
-		if err != nil {
-			panic(err)
+		var low, high int64
+		var err error
+		if filterMin != nil {
+			low, err = testkeys.ParseSuffix(filterMin)
+			if err != nil {
+				panic(err)
+			}
 		}
-		high, err := testkeys.ParseSuffix(filterMax)
-		if err != nil {
-			panic(err)
+		if filterMax != nil {
+			high, err = testkeys.ParseSuffix(filterMax)
+			if err != nil {
+				panic(err)
+			}
 		}
-		return sstable.NewTestKeysBlockPropertyFilter(uint64(low), uint64(high))
+		// The suffixes were encoded in descending order, so low should be the
+		// max timestamp and high should be the min timestamp.
+		if low < high {
+			panic(errors.AssertionFailedf("low < high: %d < %d", low, high))
+		}
+		return sstable.NewTestKeysBlockPropertyFilter(uint64(high), uint64(low))
 	},
 }
 
@@ -147,12 +159,21 @@ func (kg *testkeyKeyGenerator) IncMaxSuffix() []byte {
 func (kg *testkeyKeyGenerator) SuffixRange() (low, high []byte) {
 	a := kg.uniformSuffixInt()
 	b := kg.uniformSuffixInt()
-	if a > b {
+	// NB: Suffixes are sorted in descending order, so we need to generate the
+	// timestamps such that a > b. This ensures that the returned suffixes sort
+	// such that low < high.
+	if a < b {
 		a, b = b, a
 	} else if a == b {
-		b++
+		a++
 	}
-	return testkeys.Suffix(a), testkeys.Suffix(b)
+	if a > 0 {
+		low = testkeys.Suffix(a)
+	}
+	if b > 0 {
+		high = testkeys.Suffix(b)
+	}
+	return low, high
 }
 
 // UniformSuffix returns a suffix in the same range as SkewedSuffix but with a