4281ef6b18e203896627de0f452b09b1e242a3f4
Radu Berinde <radu@cockroachlabs.com>
2025-03-23 21:26:37 -0700

    problemspans: add data structure
    Package problemspans provides functionality for tracking and managing
    key spans that have been identified as problematic. It allows users to
    add spans with associated expiration times, check if a given key range
    overlaps any active (non-expired) spans, and remove spans when issues
    are resolved.

---
+++ internal/problemspans/set.go
@@ -0,0 +1,119 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package problemspans
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/RaduBerinde/axisds"
+	"github.com/RaduBerinde/axisds/regiontree"
+	"github.com/cockroachdb/crlib/crtime"
+	"github.com/cockroachdb/pebble/internal/base"
+)
+
+// Set maintains a set of spans with expiration times and allows checking for
+// overlap against non-expired spans.
+//
+// When the spans added to the set are not overlapping, all operations are
+// logarithmic.
+//
+// Set is not safe for concurrent use.
+type Set struct {
+	cmp   base.Compare
+	nowFn func() crtime.Mono
+
+	now crtime.Mono
+
+	// We use a region tree with key boundaries and the expirationTime as a
+	// property.
+	rt regiontree.T[axisds.Endpoint[[]byte], expirationTime]
+}
+
+// expirationTime of a problem span. 0 means that there is no problem span in a
+// region. Expiration times <= Set.now are equivalent to 0.
+type expirationTime crtime.Mono
+
+// Init must be called before a Set can be used.
+func (s *Set) Init(cmp base.Compare) {
+	s.init(cmp, crtime.NowMono)
+}
+
+func (s *Set) init(cmp base.Compare, nowFn func() crtime.Mono) {
+	*s = Set{}
+	s.cmp = cmp
+	s.nowFn = nowFn
+	propEqFn := func(a, b expirationTime) bool {
+		return a == b ||
+			crtime.Mono(a) <= s.now && crtime.Mono(b) <= s.now // Both are expired or 0.
+	}
+	endpointCmp := axisds.EndpointCompareFn(axisds.CompareFn[[]byte](cmp))
+	s.rt = regiontree.Make(endpointCmp, propEqFn)
+}
+
+func boundsToEndpoints(bounds base.UserKeyBounds) (start, end axisds.Endpoint[[]byte]) {
+	start = axisds.MakeStartEndpoint(bounds.Start, axisds.Inclusive)
+	end = axisds.MakeEndEndpoint(bounds.End.Key, axisds.InclusiveIf(bounds.End.Kind == base.Inclusive))
+	return start, end
+}
+
+// Add a span to the set. The span automatically expires after the given duration.
+func (s *Set) Add(bounds base.UserKeyBounds, expiration time.Duration) {
+	s.now = s.nowFn()
+	expTime := expirationTime(s.now + crtime.Mono(expiration))
+	start, end := boundsToEndpoints(bounds)
+	s.rt.Update(start, end, func(p expirationTime) expirationTime {
+		return max(p, expTime)
+	})
+}
+
+// Overlaps returns true if the bounds overlap with a non-expired span.
+func (s *Set) Overlaps(bounds base.UserKeyBounds) bool {
+	s.now = s.nowFn()
+	start, end := boundsToEndpoints(bounds)
+	overlaps := false
+	s.rt.Enumerate(start, end, func(start, end axisds.Endpoint[[]byte], exp expirationTime) bool {
+		overlaps = true
+		return false
+	})
+	return overlaps
+}
+
+// Excise removes a span fragment from all spans in the set. Any overlapping
+// non-expired spans are cut accordingly.
+func (s *Set) Excise(bounds base.UserKeyBounds) {
+	s.now = s.nowFn()
+	start, end := boundsToEndpoints(bounds)
+	s.rt.Update(start, end, func(p expirationTime) expirationTime {
+		return 0
+	})
+}
+
+// IsEmpty returns true if the set contains no non-expired spans.
+func (s *Set) IsEmpty() bool {
+	s.now = s.nowFn()
+	return s.rt.IsEmpty()
+}
+
+// String prints all active (non-expired) span fragments.
+func (s *Set) String() string {
+	var buf strings.Builder
+	s.now = s.nowFn()
+	s.rt.EnumerateAll(func(start, end axisds.Endpoint[[]byte], prop expirationTime) bool {
+		fmt.Fprintf(&buf, "%s  expires in: %s\n", keyEndpointIntervalFormatter(start, end), time.Duration(prop)-time.Duration(s.now))
+		return true
+	})
+	if buf.Len() == 0 {
+		return "<empty>"
+	}
+	return buf.String()
+}
+
+var keyBoundaryFormatter axisds.BoundaryFormatter[[]byte] = func(b []byte) string {
+	return string(b)
+}
+
+var keyEndpointIntervalFormatter = axisds.MakeEndpointIntervalFormatter(keyBoundaryFormatter)
---
+++ internal/problemspans/set_test.go
@@ -0,0 +1,205 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package problemspans
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"slices"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/crlib/crstrings"
+	"github.com/cockroachdb/crlib/crtime"
+	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/internal/base"
+)
+
+func TestSet(t *testing.T) {
+	now := crtime.Mono(0)
+	nowFn := func() crtime.Mono { return now }
+	var set Set
+	set.init(base.DefaultComparer.Compare, nowFn)
+
+	datadriven.RunTest(t, "testdata/set", func(t *testing.T, td *datadriven.TestData) string {
+		var nowStr string
+		if td.MaybeScanArgs(t, "now", &nowStr) {
+			var nowVal int
+			if n, err := fmt.Sscanf(nowStr, "%ds", &nowVal); err != nil || n != 1 {
+				td.Fatalf(t, "error parsing now %q: %v", nowStr, err)
+			}
+			v := crtime.Mono(time.Duration(nowVal) * time.Second)
+			if v < now {
+				td.Fatalf(t, "now cannot go backwards")
+			}
+			now = v
+		}
+
+		var out bytes.Buffer
+		switch td.Cmd {
+		case "reset":
+			set.init(base.DefaultComparer.Compare, nowFn)
+			now = 0
+
+		case "add":
+			for _, line := range crstrings.Lines(td.Input) {
+				bounds, expiration := parseSetLine(line, true /* withTime */)
+				// The test uses absolute expiration times.
+				set.Add(bounds, expiration.Sub(now))
+			}
+
+		case "excise":
+			for _, line := range crstrings.Lines(td.Input) {
+				bounds, _ := parseSetLine(line, false /* withTime */)
+				set.Excise(bounds)
+			}
+
+		case "overlap":
+			for _, line := range crstrings.Lines(td.Input) {
+				bounds, _ := parseSetLine(line, false /* withTime */)
+				res := "overlap"
+				if !set.Overlaps(bounds) {
+					res = "no overlap"
+				}
+				fmt.Fprintf(&out, "%s: %s\n", bounds, res)
+			}
+
+		case "is-empty":
+			if set.IsEmpty() {
+				out.WriteString("empty\n")
+			} else {
+				out.WriteString("not empty\n")
+			}
+		default:
+			td.Fatalf(t, "unknown command %q", td.Cmd)
+		}
+		out.WriteString("Set:\n")
+		for _, l := range crstrings.Lines(set.String()) {
+			fmt.Fprintf(&out, "  %s\n", l)
+		}
+		return out.String()
+	})
+}
+
+func parseSetLine(line string, withTime bool) (base.UserKeyBounds, crtime.Mono) {
+	var span1, span2 string
+	var timeVal int
+	if withTime {
+		n, err := fmt.Sscanf(line, "%s %s %ds", &span1, &span2, &timeVal)
+		if err != nil || n != 3 {
+			panic(fmt.Sprintf("error parsing line %q: n=%d err=%v", line, n, err))
+		}
+	} else {
+		n, err := fmt.Sscanf(line, "%s %s", &span1, &span2)
+		if err != nil || n != 2 {
+			panic(fmt.Sprintf("error parsing line %q: n=%d err=%v", line, n, err))
+		}
+	}
+	bounds := base.ParseUserKeyBounds(span1 + " " + span2)
+	return bounds, crtime.Mono(time.Duration(timeVal) * time.Second)
+}
+
+// TestSetRandomized cross-checks the Set implementation against a trivial
+// implementation.
+func TestSetRandomized(t *testing.T) {
+	now := crtime.Mono(0)
+	nowFn := func() crtime.Mono { return now }
+
+	for test := 0; test < 1000; test++ {
+		var set Set
+		set.init(base.DefaultComparer.Compare, nowFn)
+		var naive naiveSet
+
+		keys := 4 + rand.Intn(100)
+		key := func(k int) []byte {
+			return []byte(fmt.Sprintf("%04d", k))
+		}
+		for op := 0; op < 300; op++ {
+			k1, k2 := rand.Intn(keys), rand.Intn(keys)
+			if k1 > k2 {
+				k1, k2 = k2, k1
+			}
+			bounds := base.UserKeyBoundsInclusive(key(k1), key(k2))
+			if k1 < k2 && rand.Intn(2) == 0 {
+				bounds.End.Kind = base.Exclusive
+			}
+
+			if n := rand.Intn(10); n < 2 {
+				expiration := time.Duration(rand.Intn(100))
+				set.Add(bounds, expiration)
+				naive.Add(bounds, now+crtime.Mono(expiration))
+			} else if bounds.End.Kind == base.Exclusive && n == 9 {
+				set.Excise(bounds)
+				naive.Excise(bounds)
+			} else {
+				overlap := set.Overlaps(bounds)
+				expected := naive.Overlaps(bounds, now)
+				if expected != overlap {
+					t.Fatalf("expected overlap=%t, got %t for bounds %s", expected, overlap, bounds)
+				}
+			}
+
+			if rand.Intn(4) == 0 {
+				now += crtime.Mono(rand.Intn(40))
+			}
+		}
+	}
+}
+
+type naiveSpan struct {
+	bounds     base.UserKeyBounds
+	expiration crtime.Mono
+}
+
+type naiveSet struct {
+	spans []naiveSpan
+}
+
+func (ns *naiveSet) Add(bounds base.UserKeyBounds, expiration crtime.Mono) {
+	ns.spans = append(ns.spans, naiveSpan{bounds, expiration})
+}
+
+func (ns *naiveSet) Overlaps(bounds base.UserKeyBounds, now crtime.Mono) bool {
+	for _, sp := range ns.spans {
+		if sp.expiration > now && sp.bounds.Overlaps(base.DefaultComparer.Compare, &bounds) {
+			return true
+		}
+	}
+	return false
+}
+
+func (ns *naiveSet) Excise(bounds base.UserKeyBounds) {
+	if bounds.End.Kind != base.Exclusive {
+		panic("inclusive end not supported")
+	}
+	var overlapping []naiveSpan
+
+	cmp := base.DefaultComparer.Compare
+	ns.spans = slices.DeleteFunc(ns.spans, func(sp naiveSpan) bool {
+		if sp.bounds.Overlaps(cmp, &bounds) {
+			overlapping = append(overlapping, sp)
+			return true
+		}
+		return false
+	})
+	for _, sp := range overlapping {
+		if cmp(sp.bounds.Start, bounds.Start) < 0 {
+			ns.spans = append(ns.spans, naiveSpan{
+				bounds:     base.UserKeyBoundsEndExclusive(sp.bounds.Start, bounds.Start),
+				expiration: sp.expiration,
+			})
+		}
+		if bounds.End.CompareUpperBounds(cmp, sp.bounds.End) < 0 {
+			ns.spans = append(ns.spans, naiveSpan{
+				bounds: base.UserKeyBounds{
+					Start: bounds.End.Key,
+					End:   sp.bounds.End,
+				},
+				expiration: sp.expiration,
+			})
+		}
+	}
+}
