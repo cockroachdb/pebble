23ae0dd61ba8486e84bbcfd085059a237caa2e8d
Radu Berinde <radu@cockroachlabs.com>
2025-04-12 16:29:27 -0700

    metamorphic: fix key format when running a single config
    `RunOnce` takes the `KeyFormat` separately, but the one we pass is
    derived from the flags, not the one that is passed to us from e.g.
    `TestMetaCockroachKVs`. This change fixes this.
    
    Note that when running the test normally, the top-level process passes
    `--key-format` to the child process, so in that case it works out.

--- internal/metamorphic/metarunner/main.go
+++ internal/metamorphic/metarunner/main.go
@@ -27,16 +27,14 @@ func main() {
 	switch {
 	case runOnceFlags.Compare != "":
 		testRootDir, runSubdirs := runOnceFlags.ParseCompare()
-		metamorphic.Compare(t, testRootDir, runOnceFlags.Seed, runSubdirs,
-			runOnceFlags.KeyFormat(), onceOpts...)
+		metamorphic.Compare(t, testRootDir, runOnceFlags.Seed, runSubdirs, onceOpts...)
 
 	case runOnceFlags.RunDir != "":
 		// The --run-dir flag is specified either in the child process (see
 		// runOptions() below) or the user specified it manually in order to re-run
 		// a test.
 		metamorphic.RunOnce(t, runOnceFlags.RunDir, runOnceFlags.Seed,
-			filepath.Join(runOnceFlags.RunDir, "history"),
-			runOnceFlags.KeyFormat(), onceOpts...)
+			filepath.Join(runOnceFlags.RunDir, "history"), onceOpts...)
 
 	default:
 		t.Errorf("--compare or --run-dir must be used")
