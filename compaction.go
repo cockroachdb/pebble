f98387bca6dd10fc8615baa26d95f0bbe4671d85
Radu Berinde <radu@cockroachlabs.com>
2025-04-25 18:14:48 -0700

    db: log extreme values for deletion estimates
    Check `PointDeletionsBytesEstimate` and `RangeDeletionsBytesEstimate`
    for extreme values and log them (at most once every 30s). This is to
    help identify the cause of very high level scores observed in a
    cluster.

--- compaction.go
+++ compaction.go
@@ -3111,7 +3111,7 @@ func (c *compaction) makeVersionEdit(result compact.Result) (*versionEdit, error
 		// If the file didn't contain any range deletions, we can fill its
 		// table stats now, avoiding unnecessarily loading the table later.
 		maybeSetStatsFromProperties(
-			fileMeta.PhysicalMeta(), &t.WriterMeta.Properties,
+			fileMeta.PhysicalMeta(), &t.WriterMeta.Properties, c.logger,
 		)
 
 		if t.WriterMeta.HasPointKeys {
--- table_stats.go
+++ table_stats.go
@@ -8,7 +8,9 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"time"
 
+	"github.com/cockroachdb/crlib/crtime"
 	"github.com/cockroachdb/errors"
 	"github.com/cockroachdb/pebble/internal/base"
 	"github.com/cockroachdb/pebble/internal/invariants"
@@ -17,6 +19,7 @@ import (
 	"github.com/cockroachdb/pebble/internal/manifest"
 	"github.com/cockroachdb/pebble/sstable"
 	"github.com/cockroachdb/pebble/sstable/block"
+	"github.com/cockroachdb/redact"
 )
 
 // In-memory statistics about tables help inform compaction picking, but may
@@ -131,6 +134,7 @@ func (d *DB) collectTableStats() bool {
 	for _, c := range collected {
 		c.fileMetadata.Stats = c.TableStats
 		maybeCompact = maybeCompact || fileCompensation(c.fileMetadata) > 0
+		sanityCheckStats(c.fileMetadata, d.opts.Logger, "collected stats")
 		c.fileMetadata.StatsMarkValid()
 	}
 
@@ -643,7 +647,27 @@ func (d *DB) estimateReclaimedSizeBeneath(
 	return estimate, hintSeqNum, nil
 }
 
-func maybeSetStatsFromProperties(meta physicalMeta, props *sstable.Properties) bool {
+var lastSanityCheckStatsLog crtime.AtomicMono
+
+func sanityCheckStats(meta *fileMetadata, logger Logger, info string) {
+	// Values for PointDeletionsBytesEstimate and RangeDeletionsBytesEstimate that
+	// exceed this value are most likely indicative of a bug.
+	const maxDeletionBytesEstimate = 16 << 30 // 16 GiB
+
+	if meta.Stats.PointDeletionsBytesEstimate > maxDeletionBytesEstimate ||
+		meta.Stats.RangeDeletionsBytesEstimate > maxDeletionBytesEstimate {
+		if v := lastSanityCheckStatsLog.Load(); v == 0 || v.Elapsed() > 30*time.Second {
+			logger.Errorf("%s: table %s has extreme deletion bytes estimates: point=%d range=%d",
+				info, meta.FileNum,
+				redact.Safe(meta.Stats.PointDeletionsBytesEstimate),
+				redact.Safe(meta.Stats.RangeDeletionsBytesEstimate),
+			)
+			lastSanityCheckStatsLog.Store(crtime.NowMono())
+		}
+	}
+}
+
+func maybeSetStatsFromProperties(meta physicalMeta, props *sstable.Properties, logger Logger) bool {
 	// If a table contains range deletions or range key deletions, we defer the
 	// stats collection. There are two main reasons for this:
 	//
@@ -687,6 +711,7 @@ func maybeSetStatsFromProperties(meta physicalMeta, props *sstable.Properties) b
 	meta.Stats.ValueBlocksSize = props.ValueBlocksSize
 	meta.Stats.CompressionType = block.CompressionFromString(props.CompressionName)
 	meta.StatsMarkValid()
+	sanityCheckStats(meta.FileMetadata, logger, "stats from properties")
 	return true
 }
 
