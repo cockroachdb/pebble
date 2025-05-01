b4337eb22bd4827de21ff18ea78fe4c17a99da4a
Radu Berinde <radu@cockroachlabs.com>
2025-05-01 08:41:04 -0700

    sstable: fix deletion properties for virtual tables
    The independent scaling of `NumDeletions`, `NumRangeDeletions`,
    `NumSizedDeletions` for virtual tables can lead to situations where
    `NumRangeDeletions + NumSizedDeletions > NumDeletions`, which causes
    underflows in compensated size calculations.
    
    This change fixes the scaling code to scale the three "components" of
    `NumDeletions` (range, sized, other) and then sum them.
    
    We also add some safeguards around the subtractions - underflow now
    causes panics in invariant builds and is capped to 0 in non-invariant
    builds.
    
    Fixes #4670

--- internal/invariants/off.go
+++ internal/invariants/off.go
@@ -49,3 +49,17 @@ func (*Value[V]) Get() V {
 
 // Set the value; no-op in non-invariant builds.
 func (*Value[V]) Set(v V) {}
+
+// SafeSub returns a - b. If a < b, it panics in invariant builds and returns 0
+// in non-invariant builds.
+func SafeSub[T Integer](a, b T) T {
+	if a < b {
+		return 0
+	}
+	return a - b
+}
+
+// Integer is a constraint that permits any integer type.
+type Integer interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
--- table_stats.go
+++ table_stats.go
@@ -656,6 +656,13 @@ func sanityCheckStats(meta *fileMetadata, logger Logger, info string) {
 
 	if meta.Stats.PointDeletionsBytesEstimate > maxDeletionBytesEstimate ||
 		meta.Stats.RangeDeletionsBytesEstimate > maxDeletionBytesEstimate {
+		if invariants.Enabled {
+			panic(fmt.Sprintf("%s: table %s has extreme deletion bytes estimates: point=%d range=%d",
+				info, meta.FileNum,
+				redact.Safe(meta.Stats.PointDeletionsBytesEstimate),
+				redact.Safe(meta.Stats.RangeDeletionsBytesEstimate),
+			))
+		}
 		if v := lastSanityCheckStatsLog.Load(); v == 0 || v.Elapsed() > 30*time.Second {
 			logger.Errorf("%s: table %s has extreme deletion bytes estimates: point=%d range=%d",
 				info, meta.FileNum,
@@ -777,7 +784,7 @@ func pointDeletionsBytesEstimate(
 
 	// 2b. Calculate the contribution of the KV entries shadowed by ordinary DEL
 	// keys.
-	numUnsizedDels := numPointDels - props.NumSizedDeletions
+	numUnsizedDels := invariants.SafeSub(numPointDels, props.NumSizedDeletions)
 	{
 		// The shadowed keys have the same exact user keys as the tombstones
 		// themselves, so we can use the `tombstonesLogicalSize` we computed
