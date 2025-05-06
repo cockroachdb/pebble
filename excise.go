a42fc12ad2bb3909ae93b090c452b4e45b3c7187
Annie Pompa <annie@cockroachlabs.com>
2025-05-06 13:52:25 -0400

    db: ensure blob references have proper value size
    When scaling blob references' ValueSize during excise, we need to ensure
    that we round up to the nearest integer. Otherwise, a low ValueSize we want to
    scale could easily be rounded to 0.

--- excise.go
+++ excise.go
@@ -6,6 +6,7 @@ package pebble
 
 import (
 	"context"
+	"math"
 	"slices"
 
 	"github.com/cockroachdb/errors"
@@ -445,9 +446,18 @@ func determineExcisedTableSize(
 func determineExcisedTableBlobReferences(
 	originalBlobReferences manifest.BlobReferences, originalSize uint64, excisedTable *tableMetadata,
 ) {
+	if len(originalBlobReferences) == 0 {
+		return
+	}
 	newBlobReferences := make(manifest.BlobReferences, len(originalBlobReferences))
 	for i, bf := range originalBlobReferences {
-		bf.ValueSize = bf.ValueSize * excisedTable.Size / originalSize
+		adjustedSize := math.Ceil(float64(bf.ValueSize*excisedTable.Size) / float64(originalSize))
+		if invariants.Enabled && adjustedSize < 1 {
+			panic(errors.AssertionFailedf("invalid adjusted blob reference value size %.2f, "+
+				"original size %d, ratio of excised to original %d", adjustedSize, bf.ValueSize,
+				excisedTable.Size/originalSize))
+		}
+		bf.ValueSize = uint64(adjustedSize)
 		newBlobReferences[i] = bf
 	}
 	excisedTable.BlobReferences = newBlobReferences
