8eaafbe147747881242b4b31c9edcf97adcbcbdf
Radu Berinde <radu@cockroachlabs.com>
2025-05-12 10:14:09 -0700

    tool: add db analyze-data command
    Add a command that finds all sst files in a store directory and
    samples files to run compression experiments on.
    
    The list of files is refreshed and partial results are written out
    every 10 seconds.
    
    Sample stdout output on a ycsb store (random data):
    
    ```
    Found 223 files, total size 1.2GB.
    Limiting read bandwidth to 50MB/s.
    
    Kind   Size Range  Test CR  Samples  Size                  Snappy        MinLZ1        MinLZ2        ZSTD1          ZSTD3          ZSTD5         ZSTD7
    data   <24KB       <1.1     142      13.4KB ± 47%  CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%       1.0 ± 0%       1.0 ± 0%      1.0 ± 0%
                                                       Comp    2.0us ± 97%   1.8us ± 40%   6.8us ± 37%   24.1us ± 61%   19.1us ± 39%   28.5us ± 42%  28.6us ± 43%
                                                       Decomp  0.8us ± 45%   0.4us ± 55%   0.5us ± 60%   0.9us ± 44%    1.0us ± 51%    0.9us ± 49%   0.9us ± 49%
    data   24-48KB     <1.1     39165    32.2KB ± 6%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%       1.0 ± 0%       1.0 ± 0%      1.0 ± 0%
                                                       Comp    2.5us ± 299%  2.6us ± 99%   8.3us ± 53%   21.1us ± 121%  26.4us ± 89%   44.6us ± 78%  45.1us ± 61%
                                                       Decomp  1.6us ± 203%  1.0us ± 90%   1.1us ± 187%  1.7us ± 88%    1.7us ± 244%   1.7us ± 398%  1.7us ± 56%
    index  <24KB       <1.1     14       0.0KB ± 12%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      0.8 ± 1%       0.8 ± 1%       0.8 ± 1%      0.8 ± 1%
                                                       Comp    0.3us ± 28%   0.9us ± 11%   4.1us ± 20%   1.8us ± 103%   2.7us ± 33%    3.2us ± 30%   3.2us ± 34%
                                                       Decomp  0.2us ± 34%   0.1us ± 25%   0.1us ± 52%   0.6us ± 53%    0.4us ± 30%    0.5us ± 80%   0.4us ± 37%
    index  <24KB       1.1-1.5  184      3.4KB ± 62%   CR      1.4 ± 4%      1.4 ± 4%      1.4 ± 4%      1.6 ± 7%       1.6 ± 7%       1.7 ± 8%      1.7 ± 8%
                                                       Comp    5.9us ± 68%   6.5us ± 64%   14.1us ± 52%  30.1us ± 45%   30.9us ± 66%   54.6us ± 71%  111.4us ± 68%
                                                       Decomp  1.5us ± 79%   4.4us ± 58%   3.4us ± 64%   15.7us ± 48%   11.0us ± 54%   10.9us ± 52%  10.8us ± 53%
    index  <24KB       1.5-2.5  24       4.9KB ± 34%   CR      1.5 ± 1%      1.5 ± 0%      1.5 ± 0%      1.8 ± 0%       1.8 ± 0%       1.8 ± 0%      1.8 ± 0%
                                                       Comp    8.2us ± 43%   10.6us ± 67%  18.1us ± 41%  36.9us ± 38%   35.8us ± 38%   69.0us ± 42%  151.2us ± 44%
                                                       Decomp  2.1us ± 47%   5.6us ± 42%   4.4us ± 42%   20.1us ± 44%   13.8us ± 40%   13.3us ± 44%  14.2us ± 51%
    other  <24KB       <1.1     87       1.0KB ± 59%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 2%       1.0 ± 2%       1.0 ± 2%      1.0 ± 2%
                                                       Comp    0.5us ± 33%   1.1us ± 34%   4.3us ± 33%   38.2us ± 159%  22.4us ± 160%  16.5us ± 47%  18.3us ± 91%
                                                       Decomp  0.2us ± 34%   0.1us ± 44%   0.1us ± 48%   0.4us ± 73%    0.3us ± 41%    0.3us ± 55%   0.3us ± 35%
    other  <24KB       1.5-2.5  11       0.1KB ± 0%    CR      1.7 ± 14%     2.0 ± 14%     2.2 ± 14%     1.7 ± 12%      1.6 ± 12%      1.7 ± 10%     1.8 ± 11%
                                                       Comp    0.5us ± 34%   1.1us ± 14%   4.1us ± 12%   2.5us ± 25%    2.4us ± 13%    4.5us ± 19%   5.4us ± 20%
                                                       Decomp  0.4us ± 22%   0.7us ± 55%   0.5us ± 26%   1.3us ± 54%    0.7us ± 28%    1.0us ± 55%   0.8us ± 23%
    
    Sampled 100.00% (1.2GB)
    ```

---
+++ tool/db_analyze_data.go
@@ -0,0 +1,282 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package tool
+
+import (
+	"cmp"
+	"context"
+	"fmt"
+	"io"
+	"math"
+	"math/rand/v2"
+	"os"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/humanize"
+	"github.com/cockroachdb/pebble/sstable/compressionanalyzer"
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/cockroachdb/tokenbucket"
+	"github.com/spf13/cobra"
+)
+
+// Minimum samples required per analyzer bucket to report the bucket at all.
+const minSamples = 10
+
+func (d *dbT) runAnalyzeData(cmd *cobra.Command, args []string) {
+	stdout, stderr := cmd.OutOrStdout(), cmd.OutOrStderr()
+	isTTY := isTTY(stdout)
+
+	dir := args[0]
+	if err := d.initOptions(dir); err != nil {
+		fmt.Fprintf(stderr, "error initializing options: %s\n", err)
+		return
+	}
+	rng := rand.New(rand.NewPCG(rand.Uint64(), rand.Uint64()))
+	files, err := makeFileSet(d.opts.FS, dir, rng)
+	if err != nil {
+		fmt.Fprintf(stderr, "error loading file list: %s\n", err)
+		return
+	}
+	if files.Done() {
+		fmt.Fprintf(stderr, "no sstables found\n")
+		return
+	}
+	totalSize := files.TotalSize()
+	// We do not recalculate the target size every time we refresh the file list.
+	// If the database is growing rapidly, we might not be able to keep up.
+	targetSize := totalSize
+	if d.analyzeData.samplePercent > 0 && d.analyzeData.samplePercent < 100 {
+		targetSize = (totalSize*int64(d.analyzeData.samplePercent) + 99) / 100
+	}
+	var readLimiter *tokenbucket.TokenBucket
+	if d.analyzeData.readMBPerSec > 0 {
+		readLimiter = &tokenbucket.TokenBucket{}
+		rate := tokenbucket.TokensPerSecond(d.analyzeData.readMBPerSec) * (1 << 20)
+		burst := tokenbucket.Tokens(rate * 0.1)
+		readLimiter.Init(rate, burst)
+	}
+	if isTTY {
+		fmt.Fprintf(stdout, "Found %d files, total size %s.\n", len(files.files), humanize.Bytes.Int64(totalSize))
+		if d.analyzeData.readMBPerSec > 0 {
+			fmt.Fprintf(stdout, "Limiting read bandwidth to %s/s.\n", humanize.Bytes.Int64(int64(d.analyzeData.readMBPerSec)<<20))
+		} else {
+			fmt.Fprintf(stdout, "No read bandwidth limiting.\n")
+		}
+		if d.analyzeData.samplePercent > 0 && d.analyzeData.samplePercent < 100 {
+			fmt.Fprintf(stdout, "Stopping after samping %d%% of the data", d.analyzeData.samplePercent)
+			if d.analyzeData.timeout > 0 {
+				fmt.Fprintf(stdout, " or after %s", d.analyzeData.timeout)
+			}
+			fmt.Fprintf(stdout, ".\n")
+		} else if d.analyzeData.timeout > 0 {
+			fmt.Fprintf(stdout, "Stopping after %s.", d.analyzeData.timeout)
+		}
+	}
+
+	startTime := time.Now()
+	lastReportTime := startTime
+
+	analyzer := compressionanalyzer.NewFileAnalyzer(readLimiter, d.opts.MakeReaderOptions())
+	nLinesToErase := 0
+	var sampled int64
+	for {
+		shouldStop := files.Done() || sampled >= targetSize ||
+			(d.analyzeData.timeout > 0 && time.Since(startTime) > d.analyzeData.timeout)
+		// Every 10 seconds, we:
+		//  - print the current results and progress (if on a tty);
+		//  - write the output CSV file (so we get some results even if the command
+		//    is interrupted);
+		//  - refresh the list of files.
+		if time.Since(lastReportTime) > 10*time.Second || shouldStop {
+			if isTTY || shouldStop {
+				if nLinesToErase > 0 {
+					fmt.Fprint(stdout, strings.Repeat("\033[1A\033[2K\r", nLinesToErase))
+				}
+				partialResults := analyzer.Buckets().String(minSamples)
+				fmt.Fprintf(stdout, "\n%s\n", partialResults)
+				percentage := min(float64(sampled*100)/float64(totalSize), 100)
+				if files.Done() {
+					percentage = 100
+				}
+				fmt.Fprintf(stdout, "Sampled %.2f%% (%s)\n", percentage, humanize.Bytes.Int64(sampled))
+				nLinesToErase = strings.Count(partialResults, "\n") + 3
+			}
+			if err := analyzeSaveCSVFile(analyzer, d.analyzeData.outputCSVFile); err != nil {
+				fmt.Fprintf(stderr, "error writing CSV file: %s\n", err)
+				return
+			}
+			if shouldStop {
+				return
+			}
+			if err := files.Refresh(); err != nil {
+				fmt.Fprintf(stderr, "error loading file list: %s\n", err)
+				return
+			}
+			lastReportTime = time.Now()
+		}
+		// Sample a file and analyze it.
+		path, size := files.Sample()
+		err := analyzer.SSTable(context.Background(), d.opts.FS, path)
+		if err != nil {
+			// We ignore errors from files that are deleted from under us.
+			if !errors.Is(err, os.ErrNotExist) {
+				fmt.Fprintf(stderr, "error reading file %s: %s\n", path, err)
+			}
+			continue
+		}
+		sampled += size
+	}
+}
+
+func analyzeSaveCSVFile(a *compressionanalyzer.FileAnalyzer, path string) error {
+	csv := a.Buckets().ToCSV(minSamples)
+	return os.WriteFile(path, []byte(csv), 0o666)
+}
+
+// We avoid files that are very large to prevent excessive memory usage. Note
+// that we have seen cases where large files contain a giant top index block, so
+// even getting the block layout of the file would use a lot of memory.
+const analyzeMaxFileSize = 512 * 1024 * 1024
+
+type fileSet struct {
+	fs  vfs.FS
+	dir string
+	rng *rand.Rand
+
+	files     []fileInSet
+	sampleIdx []int
+}
+
+type fileInSet struct {
+	filename    string
+	fileType    base.FileType
+	size        int64
+	samplingKey float64
+	sampled     bool
+}
+
+func makeFileSet(fs vfs.FS, dir string, rng *rand.Rand) (fileSet, error) {
+	s := fileSet{
+		fs:  fs,
+		dir: dir,
+		rng: rng,
+	}
+	return s, s.Refresh()
+}
+
+// generateSamplingKey generates a key for a file of a given size. When sorted
+// by increasing keys, the files correspond to a random weighted sampling
+// without replacement. This is the Efraimidis–Spirakis algorithm.
+func samplingKey(rng *rand.Rand, size int64) float64 {
+	return -math.Log(rng.Float64()) / float64(size)
+}
+
+func (s *fileSet) Refresh() error {
+	filenames, err := s.fs.List(s.dir)
+	if err != nil {
+		return err
+	}
+	slices.Sort(filenames)
+	oldFiles := slices.Clone(s.files)
+	s.files = s.files[:0]
+	now := time.Now()
+
+	newFile := func(filename string) {
+		// New file.
+		fileType, _, ok := base.ParseFilename(s.fs, filename)
+		if !ok || fileType != base.FileTypeTable {
+			return
+		}
+		fileInfo, err := s.fs.Stat(s.fs.PathJoin(s.dir, filename))
+		if err != nil {
+			// Files can get deleted from under us, so we tolerate errors.
+			return
+		}
+		// We ignore files that are less than 15 seconds old. This is to avoid
+		// trying to read a file that is still being written.
+		if now.Sub(fileInfo.ModTime()) < 15*time.Second {
+			return
+		}
+		size := fileInfo.Size()
+		if size == 0 || size > analyzeMaxFileSize {
+			return
+		}
+		s.files = append(s.files, fileInSet{
+			filename:    filename,
+			fileType:    fileType,
+			size:        size,
+			samplingKey: samplingKey(s.rng, size),
+		})
+	}
+
+	// Go through the two lists of sorted files.
+	for i, j := 0, 0; i < len(filenames) || j < len(oldFiles); {
+		switch {
+		case i < len(filenames) && (j == len(oldFiles) || filenames[i] < oldFiles[j].filename):
+			// New file.
+			newFile(filenames[i])
+			i++
+		case j < len(oldFiles) && (j == len(filenames) || filenames[i] > oldFiles[j].filename):
+			// File deleted.
+			j++
+		default:
+			// Existing file.
+			s.files = append(s.files, oldFiles[j])
+			i++
+			j++
+		}
+	}
+	// Generate the samples.
+	s.sampleIdx = s.sampleIdx[:0]
+	for i := range s.files {
+		if !s.files[i].sampled {
+			s.sampleIdx = append(s.sampleIdx, i)
+		}
+	}
+	slices.SortFunc(s.sampleIdx, func(i, j int) int {
+		return cmp.Compare(s.files[i].samplingKey, s.files[j].samplingKey)
+	})
+	return nil
+}
+
+func (s *fileSet) TotalSize() int64 {
+	var sum int64
+	for i := range s.files {
+		sum += s.files[i].size
+	}
+	return sum
+}
+
+func (s *fileSet) Done() bool {
+	return len(s.sampleIdx) == 0
+}
+
+// Sample returns a random file from the set (which was not previously sampled),
+// weighted by size.
+func (s *fileSet) Sample() (path string, size int64) {
+	idx := s.sampleIdx[0]
+	s.sampleIdx = s.sampleIdx[1:]
+	s.files[idx].sampled = true
+	return s.fs.PathJoin(s.dir, s.files[idx].filename), s.files[idx].size
+}
+
+func isTTY(out io.Writer) bool {
+	f, ok := out.(*os.File)
+	if !ok {
+		return false
+	}
+
+	// 2. Stat the file and check ModeCharDevice
+	fi, err := f.Stat()
+	if err != nil {
+		return false
+	}
+
+	return fi.Mode()&os.ModeCharDevice != 0
+}
