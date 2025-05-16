886faf44959cffb632c037f0e4467f9709001d59
Radu Berinde <radu@cockroachlabs.com>
2025-05-16 14:38:15 -0700

    tool: add db analyze-data command
    Add a command that finds all sst files in a store directory and
    samples files to run compression experiments on.
    
    The list of files is refreshed and partial results are written out
    every 10 seconds.
    
    Sample stdout output on a ycsb store (random data):
    
    ```
    Found 223 files, total size 1.2GB.
    Limiting read bandwidth to 50MB/s.
    
    Kind   Size Range  Test CR  Samples  Size                  Snappy        MinLZ1        MinLZ2        ZSTD1          ZSTD3          ZSTD5         ZSTD7
    data   <24KB       <1.1     142      13.4KB ± 47%  CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%       1.0 ± 0%       1.0 ± 0%      1.0 ± 0%
                                                       Comp    2.0us ± 97%   1.8us ± 40%   6.8us ± 37%   24.1us ± 61%   19.1us ± 39%   28.5us ± 42%  28.6us ± 43%
                                                       Decomp  0.8us ± 45%   0.4us ± 55%   0.5us ± 60%   0.9us ± 44%    1.0us ± 51%    0.9us ± 49%   0.9us ± 49%
    data   24-48KB     <1.1     39165    32.2KB ± 6%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%       1.0 ± 0%       1.0 ± 0%      1.0 ± 0%
                                                       Comp    2.5us ± 299%  2.6us ± 99%   8.3us ± 53%   21.1us ± 121%  26.4us ± 89%   44.6us ± 78%  45.1us ± 61%
                                                       Decomp  1.6us ± 203%  1.0us ± 90%   1.1us ± 187%  1.7us ± 88%    1.7us ± 244%   1.7us ± 398%  1.7us ± 56%
    index  <24KB       <1.1     14       0.0KB ± 12%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      0.8 ± 1%       0.8 ± 1%       0.8 ± 1%      0.8 ± 1%
                                                       Comp    0.3us ± 28%   0.9us ± 11%   4.1us ± 20%   1.8us ± 103%   2.7us ± 33%    3.2us ± 30%   3.2us ± 34%
                                                       Decomp  0.2us ± 34%   0.1us ± 25%   0.1us ± 52%   0.6us ± 53%    0.4us ± 30%    0.5us ± 80%   0.4us ± 37%
    index  <24KB       1.1-1.5  184      3.4KB ± 62%   CR      1.4 ± 4%      1.4 ± 4%      1.4 ± 4%      1.6 ± 7%       1.6 ± 7%       1.7 ± 8%      1.7 ± 8%
                                                       Comp    5.9us ± 68%   6.5us ± 64%   14.1us ± 52%  30.1us ± 45%   30.9us ± 66%   54.6us ± 71%  111.4us ± 68%
                                                       Decomp  1.5us ± 79%   4.4us ± 58%   3.4us ± 64%   15.7us ± 48%   11.0us ± 54%   10.9us ± 52%  10.8us ± 53%
    index  <24KB       1.5-2.5  24       4.9KB ± 34%   CR      1.5 ± 1%      1.5 ± 0%      1.5 ± 0%      1.8 ± 0%       1.8 ± 0%       1.8 ± 0%      1.8 ± 0%
                                                       Comp    8.2us ± 43%   10.6us ± 67%  18.1us ± 41%  36.9us ± 38%   35.8us ± 38%   69.0us ± 42%  151.2us ± 44%
                                                       Decomp  2.1us ± 47%   5.6us ± 42%   4.4us ± 42%   20.1us ± 44%   13.8us ± 40%   13.3us ± 44%  14.2us ± 51%
    other  <24KB       <1.1     87       1.0KB ± 59%   CR      1.0 ± 0%      1.0 ± 0%      1.0 ± 0%      1.0 ± 2%       1.0 ± 2%       1.0 ± 2%      1.0 ± 2%
                                                       Comp    0.5us ± 33%   1.1us ± 34%   4.3us ± 33%   38.2us ± 159%  22.4us ± 160%  16.5us ± 47%  18.3us ± 91%
                                                       Decomp  0.2us ± 34%   0.1us ± 44%   0.1us ± 48%   0.4us ± 73%    0.3us ± 41%    0.3us ± 55%   0.3us ± 35%
    other  <24KB       1.5-2.5  11       0.1KB ± 0%    CR      1.7 ± 14%     2.0 ± 14%     2.2 ± 14%     1.7 ± 12%      1.6 ± 12%      1.7 ± 10%     1.8 ± 11%
                                                       Comp    0.5us ± 34%   1.1us ± 14%   4.1us ± 12%   2.5us ± 25%    2.4us ± 13%    4.5us ± 19%   5.4us ± 20%
                                                       Decomp  0.4us ± 22%   0.7us ± 55%   0.5us ± 26%   1.3us ± 54%    0.7us ± 28%    1.0us ± 55%   0.8us ± 23%
    
    Sampled 100.00% (1.2GB)
    ```

---
+++ tool/db_analyze_data_test.go
@@ -0,0 +1,83 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package tool
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand/v2"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/stretchr/testify/require"
+)
+
+// TestFileSetSampling is a smoke test of the fileSet sampling implementation.
+// It verifies that a large file is usually chosen when sampling from a set of
+// files with one large file and multiple small files.
+func TestFileSetSampling(t *testing.T) {
+	const (
+		largeFileSize = 10000
+		smallFileSize = 1
+		numSmallFiles = 10
+		iterations    = 10000
+	)
+	// The probability of choosing a small file should be ~0.001. Verify that it doesn't happen
+
+	// Create an in-memory filesystem.
+	memFS := vfs.NewMem()
+	createWithSize := func(name string, size int) {
+		f, err := memFS.Create(name, vfs.WriteCategoryUnspecified)
+		require.NoError(t, err)
+		n, err := f.Write(bytes.Repeat([]byte("a"), size))
+		require.NoError(t, err)
+		require.Equal(t, size, n)
+		require.NoError(t, f.Close())
+	}
+
+	// Create a large file.
+	largeFileName := fmt.Sprintf("%s.sst", base.DiskFileNum(123456))
+	createWithSize(largeFileName, largeFileSize)
+	for i := 0; i < numSmallFiles; i++ {
+		createWithSize(fmt.Sprintf("%s.sst", base.DiskFileNum(i)), smallFileSize)
+	}
+	rng := rand.New(rand.NewPCG(rand.Uint64(), rand.Uint64()))
+	smallFileChosen := 0
+	for i := 0; i < iterations; i++ {
+		fs, err := makeFileSet(fsWrapper{memFS}, "", rng)
+		require.NoError(t, err)
+		file, _ := fs.Sample()
+		if file != largeFileName {
+			smallFileChosen++
+		}
+	}
+	// We expect a small file to be chosen ~0.001 of the time. The probability of
+	// observing this 10x more often than expected in 10,000 trials is negligible,
+	// less than (e^90 / 10^10)^10 < 10^-60 by Chernoff bound.
+	expected := float64(smallFileSize*numSmallFiles) / float64(smallFileSize*numSmallFiles+largeFileSize)
+	actual := float64(smallFileChosen) / float64(iterations)
+	require.Less(t, actual, expected*10)
+}
+
+// fsWrapper stubs out Stat and changes the ModTime so files appear older.
+type fsWrapper struct {
+	*vfs.MemFS
+}
+
+func (f fsWrapper) Stat(name string) (vfs.FileInfo, error) {
+	info, err := f.MemFS.Stat(name)
+	return fileInfoWrapper{info}, err
+}
+
+// fileInfoWrapper stubs out ModTime so the file appears older.
+type fileInfoWrapper struct {
+	vfs.FileInfo
+}
+
+func (f fileInfoWrapper) ModTime() time.Time {
+	return time.Now().Add(-time.Hour)
+}
