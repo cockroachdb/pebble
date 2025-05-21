545660e6048556fbd502e2ef9530f9405286a1b8
Radu Berinde <radu@cockroachlabs.com>
2025-05-21 08:24:53 -0700

    metamorphic: fix code around WAL recovery directories

--- options.go
+++ options.go
@@ -2091,6 +2091,8 @@ func (o *Options) CheckCompatibility(previousOptions string) error {
 			}
 		case "Options.wal_dir", "WAL Failover.secondary_dir":
 			switch {
+			case value == "":
+				return nil
 			case o.WALDir == value:
 				return nil
 			case o.WALFailover != nil && o.WALFailover.Secondary.Dirname == value:
