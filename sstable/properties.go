0c27ea7573fd5e576d2e86cc934e88ee70a49d66
Xin Hao Zhang <xzhang@cockroachlabs.com>
2025-05-22 18:02:43 -0400

    sstable: add AttributePointKeys
    AttributePointKeys is set if Properties.NumDataBlocks > 0.

--- sstable/properties.go
+++ sstable/properties.go
@@ -535,6 +535,9 @@ func (p *Properties) toAttributes() Attributes {
 	if p.NumValuesInBlobFiles > 0 {
 		attributes.Add(AttributeBlobValues)
 	}
+	if p.NumDataBlocks > 0 {
+		attributes.Add(AttributePointKeys)
+	}
 
 	return attributes
 }
