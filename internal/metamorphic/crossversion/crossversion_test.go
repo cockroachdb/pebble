b80f7fefd174d8d9efb3935e3a2e1079a41cdf26
Radu Berinde <radu@cockroachlabs.com>
2025-05-22 13:41:37 -0700

    metamorphic: sanity check initial-state and previous-ops flags
    Error out if `--initial-state` is used without `--previous-ops`.

--- internal/metamorphic/crossversion/crossversion_test.go
+++ internal/metamorphic/crossversion/crossversion_test.go
@@ -187,6 +187,9 @@ func runCrossVersion(
 		// All the initial states described the same state and all of this
 		// version's metamorphic runs used the same seed, so all of the
 		// resulting histories should be identical.
+		if len(histories) == 0 {
+			t.Fatal("no subrun histories")
+		}
 		if h, diff := metamorphic.CompareHistories(t, histories); h > 0 {
 			fatalf(t, &fatalOnce, rootDir, "Metamorphic test divergence between %q and %q:\nDiff:\n%s",
 				nextInitialStates[0].desc, nextInitialStates[h].desc, diff)
@@ -245,9 +248,10 @@ func runVersion(
 				if streamOutput {
 					out = io.MultiWriter(out, os.Stderr)
 				}
-				t.Logf("  Running test with version %s with initial state %s.",
-					vers.SHA, s)
-				if err := r.run(ctx, out); err != nil {
+				t.Logf("  Running test with version %s with initial state %s (dir=%s initial=%s).",
+					vers.SHA, s, r.dir, s.path)
+
+				if err := r.run(ctx, t, out); err != nil {
 					fatalf(t, fatalOnce, rootDir, "Metamorphic test failed: %s\nOutput:%s\n", err, buf.String())
 				}
 
@@ -311,7 +315,7 @@ type metamorphicTestRun struct {
 	testBinaryPath string
 }
 
-func (r *metamorphicTestRun) run(ctx context.Context, output io.Writer) error {
+func (r *metamorphicTestRun) run(ctx context.Context, t *testing.T, output io.Writer) error {
 	args := []string{
 		"-test.run", "TestMeta$",
 		"-seed", strconv.FormatUint(r.seed, 10),
@@ -342,8 +346,9 @@ func (r *metamorphicTestRun) run(ctx context.Context, output io.Writer) error {
 	cmd.Stdout = output
 
 	// Print the command itself before executing it.
+	fmt.Println(output, cmd)
 	if testing.Verbose() {
-		fmt.Fprintln(output, cmd)
+		t.Logf("running: %s", cmd.String())
 	}
 
 	return cmd.Run()
