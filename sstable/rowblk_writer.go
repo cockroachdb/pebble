6fce9c535a090ddca49c404c8f1ea5498e1a2270
Radu Berinde <radu@cockroachlabs.com>
2025-05-31 19:19:17 -0700

    base: add NoFilterPolicy
    This is cleaner than special-casing `nil` and will allow
    differentiating between an unset value and a value that is
    specifically set to `NoFilterPolicy`.

--- sstable/rowblk_writer.go
+++ sstable/rowblk_writer.go
@@ -1739,7 +1739,7 @@ func newRowWriter(writable objstorage.Writable, o WriterOptions) *RawRowWriter {
 		return w
 	}
 
-	if o.FilterPolicy != nil {
+	if o.FilterPolicy != base.NoFilterPolicy {
 		switch o.FilterType {
 		case TableFilter:
 			w.filter = newTableFilterWriter(o.FilterPolicy)
