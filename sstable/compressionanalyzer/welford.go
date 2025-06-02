5985de16e944915299c15a6750497ab6d732eb99
Radu Berinde <radu@cockroachlabs.com>
2025-06-02 09:37:04 -0700

    compressionanalyzer: sample per byte instead of per block
    We record compression ratio and block compression/decompression time
    per block. This is ok when blocks are about the same size but are less
    useful when there is larger variance (as in >128KB buckets).
    
    We want the compression ratio to be the ratio between the total
    decompressed size and the total compressed size, not the average
    compression ratio among blocks. Similarly, we want to measure
    performance in terms of CPU time per byte.
    
    We switch to using frequency-weighted sampling (where frequency is the
    size of the block) and we now show compression/decompression
    performance in MB/s. Note that the standard deviation for the latter
    is derived from the CPU time per second metric, it doesn't apply
    directly to the MB/s figure.

--- sstable/compressionanalyzer/welford.go
+++ sstable/compressionanalyzer/welford.go
@@ -29,36 +29,62 @@ func (w *Welford) Count() int64 {
 }
 
 // Mean returns the current running mean.
-// If no values have been added, returns NaN.
+// If no values have been added, returns 0.
 func (w *Welford) Mean() float64 {
-	if w.count == 0 {
-		return math.NaN()
-	}
 	return w.mean
 }
 
-// Variance returns the population variance (M2/n). Returns NaN if no values.
-func (w *Welford) Variance() float64 {
-	if w.count == 0 {
-		return math.NaN()
-	}
-	return w.m2 / float64(w.count)
-}
-
-// SampleVariance returns the sample variance (M2/(n-1)). Returns NaN if fewer than 2 values.
+// SampleVariance returns the sample variance (M2/(n-1)). Returns 0 if fewer than 2 values.
 func (w *Welford) SampleVariance() float64 {
 	if w.count < 2 {
-		return math.NaN()
+		return 0
 	}
 	return w.m2 / float64(w.count-1)
-}
-
-// StandardDeviation returns the population standard deviation.
-func (w *Welford) StandardDeviation() float64 {
-	return math.Sqrt(w.Variance())
 }
 
 // SampleStandardDeviation returns the sample standard deviation.
 func (w *Welford) SampleStandardDeviation() float64 {
 	return math.Sqrt(w.SampleVariance())
+}
+
+// WeightedWelford maintains running statistics for mean and variance using
+// an extension of Welford's algorithm which allows for weighted samples; see
+// https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Weighted_incremental_algorithm
+type WeightedWelford struct {
+	wSum  float64
+	w2Sum float64
+	mean  float64
+	s     float64
+}
+
+// Add incorporates a new data point x with the given frequency into the running
+// statistics.
+func (ww *WeightedWelford) Add(x float64, frequency uint64) {
+	w := float64(frequency)
+	ww.wSum += w
+	ww.w2Sum += w * w
+	delta := x - ww.mean
+	ww.mean += delta * (w / ww.wSum)
+	delta2 := x - ww.mean
+	ww.s += w * delta * delta2
+}
+
+// Mean returns the current running mean.
+// If no values have been added, returns 0.
+func (ww *WeightedWelford) Mean() float64 {
+	return ww.mean
+}
+
+// SampleVariance returns the sample variance (M2/(n-1)). Returns 0 if the sum
+// of added frequencies is less than 2.
+func (ww *WeightedWelford) SampleVariance() float64 {
+	if ww.wSum < 2 {
+		return 0
+	}
+	return ww.s / (ww.wSum - 1)
+}
+
+// SampleStandardDeviation returns the sample standard deviation.
+func (ww *WeightedWelford) SampleStandardDeviation() float64 {
+	return math.Sqrt(ww.SampleVariance())
 }
