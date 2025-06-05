274154bd6dca306eafd2c5dc469bba35c36c415b
Jackson Owens <jackson@cockroachlabs.com>
2025-06-05 15:45:42 -0400

    tool: refactor blob file mappings
    Refactor the tool package's interfaces surrounding blob references in
    preparation for blob file replacement.

---
+++ tool/blob_files.go
@@ -0,0 +1,103 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package tool
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/manifest"
+	"github.com/cockroachdb/pebble/objstorage/objstorageprovider"
+	"github.com/cockroachdb/pebble/record"
+	"github.com/cockroachdb/pebble/sstable"
+	"github.com/cockroachdb/pebble/sstable/blob"
+	"github.com/cockroachdb/pebble/sstable/block"
+	"github.com/cockroachdb/pebble/vfs"
+)
+
+// blobFileMappings contains state for retrieving a value separated into a blob
+// file.
+//
+// An sstable alone does not contain sufficient information to read a value from
+// a blob file. It requires the manifest to map the ReferenceIDs encoded within
+// the sstable to physical blob files. The blobFileMappings type contains
+// sufficient information to perform this mapping.
+type blobFileMappings struct {
+	references map[base.TableNum]*manifest.BlobReferences
+	fetcher    blob.ValueFetcher
+	provider   debugReaderProvider
+}
+
+// LoadValueBlobContext returns a TableBlobContext that configures a sstable
+// iterator to fetch the value stored in a blob file, using the mappings in this
+// blobFileMappings.
+func (m *blobFileMappings) LoadValueBlobContext(tableNum base.TableNum) sstable.TableBlobContext {
+	return sstable.TableBlobContext{
+		ValueFetcher: &m.fetcher,
+		References:   m.references[tableNum],
+	}
+}
+
+// Close releases any resources held by the blobFileMappings.
+func (m *blobFileMappings) Close() error {
+	return errors.CombineErrors(m.fetcher.Close(), m.provider.objProvider.Close())
+}
+
+// newBlobFileMappings builds a blobFileMappings from a list of manifests.
+//
+// Possibly benign errors are printed to stderr. For example, if a process exits
+// during a manifest rotation, we may encounter an error reading a manifest, but
+// we'll still have sufficient information to read separated values from all
+// tables in the current version of the LSM.
+//
+// The returned blobFileMappings must be closed to release resources.
+func newBlobFileMappings(
+	stderr io.Writer, fs vfs.FS, dir string, manifests []fileLoc,
+) (*blobFileMappings, error) {
+	settings := objstorageprovider.DefaultSettings(fs, dir)
+	provider, err := objstorageprovider.Open(settings)
+	if err != nil {
+		return nil, err
+	}
+	mappings := blobFileMappings{
+		references: make(map[base.TableNum]*manifest.BlobReferences),
+		provider:   debugReaderProvider{objProvider: provider},
+	}
+	mappings.fetcher.Init(&mappings.provider, block.ReadEnv{})
+	for _, fl := range manifests {
+		err := func() error {
+			mf, err := fs.Open(fl.path)
+			if err != nil {
+				return err
+			}
+			defer mf.Close()
+
+			rr := record.NewReader(mf, fl.DiskFileNum)
+			for {
+				r, err := rr.Next()
+				if err != nil {
+					if err != io.EOF {
+						return err
+					}
+					break
+				}
+				var ve manifest.VersionEdit
+				if err = ve.Decode(r); err != nil {
+					return err
+				}
+				for _, nf := range ve.NewTables {
+					mappings.references[nf.Meta.TableNum] = &nf.Meta.BlobReferences
+				}
+			}
+			return nil
+		}()
+		if err != nil {
+			fmt.Fprintf(stderr, "error reading manifest %s: %v\n", fl.path, err)
+		}
+	}
+	return &mappings, nil
+}
