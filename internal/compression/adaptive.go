afc5932d05012cf25699a93d6fb012eac0fdf4eb
Radu Berinde <radu@cockroachlabs.com>
2025-06-24 08:28:37 -0700

    compression: add an adaptive compressor
    Add AdaptiveCompressor, which estimates the size reduction of using a
    slower algorithm vs a faster one and chooses automatically (on a
    per-block basis).
    
    We will separately update the compression analyzer to run experiments
    with the adaptive compressors.

---
+++ internal/compression/adaptive.go
@@ -0,0 +1,104 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package compression
+
+import (
+	"math"
+	"math/rand/v2"
+	"sync"
+
+	"github.com/cockroachdb/pebble/internal/ewma"
+)
+
+// AdaptiveCompressor is a Compressor that automatically chooses between two
+// algorithms: it uses a slower but better algorithm as long as it reduces the
+// compressed size (compared to the faster algorithm) by a certain relative
+// amount. The decision is probabilistic and based on sampling a subset of
+// blocks.
+type AdaptiveCompressor struct {
+	fast Compressor
+	slow Compressor
+
+	reductionCutoff float64
+	sampleEvery     int
+
+	// estimator for the relative size reduction when choosing the slow algorithm.
+	estimator ewma.Bytes
+	rng       rand.PCG
+
+	buf []byte
+}
+
+// AdaptiveCompressorParams contains the parameters for an adaptive compressor.
+type AdaptiveCompressorParams struct {
+	// Fast and Slow are the two compression settings the adaptive compressor
+	// chooses between.
+	Fast Setting
+	Slow Setting
+	// ReductionCutoff is the relative size reduction (when using the slow
+	// algorithm vs the fast algorithm) below which we use the fast algorithm. For
+	// example, if ReductionCutoff is 0.3 then we only use the slow algorithm if
+	// it reduces the compressed size (compared to the fast algorithm) by at least
+	// 30%.
+	ReductionCutoff float64
+	// SampleEvery defines the sampling frequency: the probability we sample a
+	// block is 1.0/SampleEvery. Sampling means trying both algorithms and
+	// recording the compression ratio.
+	SampleEvery int
+	// SampleHalfLife defines the half-life of the exponentially weighted moving
+	// average. It should be a factor larger than the expected average block size.
+	SampleHalfLife int64
+	SamplingSeed   uint64
+}
+
+func NewAdaptiveCompressor(p AdaptiveCompressorParams) *AdaptiveCompressor {
+	ac := adaptiveCompressorPool.Get().(*AdaptiveCompressor)
+	ac.fast = GetCompressor(p.Fast)
+	ac.slow = GetCompressor(p.Slow)
+	ac.sampleEvery = p.SampleEvery
+	ac.reductionCutoff = p.ReductionCutoff
+	ac.estimator.Init(p.SampleHalfLife)
+	ac.rng.Seed(p.SamplingSeed, p.SamplingSeed)
+	return ac
+}
+
+var _ Compressor = (*AdaptiveCompressor)(nil)
+
+var adaptiveCompressorPool = sync.Pool{
+	New: func() any { return &AdaptiveCompressor{} },
+}
+
+func (ac *AdaptiveCompressor) Compress(dst, src []byte) ([]byte, Setting) {
+	estimate := ac.estimator.Estimate()
+	// TODO(radu): consider decreasing the sampling frequency if the estimate is
+	// far from the cutoff.
+	sampleThisBlock := math.IsNaN(estimate) || ac.rng.Uint64()%uint64(ac.sampleEvery) == 0
+	if !sampleThisBlock {
+		ac.estimator.NoSample(int64(len(src)))
+		if estimate < ac.reductionCutoff {
+			return ac.fast.Compress(dst, src)
+		} else {
+			return ac.slow.Compress(dst, src)
+		}
+	}
+	bufFast, fastSetting := ac.fast.Compress(ac.buf[:0], src)
+	ac.buf = bufFast[:0]
+	dst, slowSetting := ac.slow.Compress(dst, src)
+	reduction := 1 - float64(len(dst))/float64(len(bufFast))
+	ac.estimator.SampledBlock(int64(len(src)), reduction)
+	if reduction < ac.reductionCutoff {
+		return append(dst[:0], bufFast...), fastSetting
+	}
+	return dst, slowSetting
+}
+
+func (ac *AdaptiveCompressor) Close() {
+	ac.fast.Close()
+	ac.slow.Close()
+	if cap(ac.buf) > 256*1024 {
+		ac.buf = nil // Release large buffers.
+	}
+	adaptiveCompressorPool.Put(ac)
+}
---
+++ internal/compression/adaptive_test.go
@@ -0,0 +1,56 @@
+// Copyright 2025 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package compression
+
+import (
+	"math/rand/v2"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdaptiveCompressorRand(t *testing.T) {
+	// Test the adaptive compressor with random data and make sure it uses the
+	// fast algorithm.
+	ac := NewAdaptiveCompressor(AdaptiveCompressorParams{
+		Fast:            MinLZFastest,
+		Slow:            ZstdLevel1,
+		ReductionCutoff: 0.2,
+		SampleEvery:     10,
+		SampleHalfLife:  128 * 1024,
+		SamplingSeed:    1,
+	})
+	defer ac.Close()
+	for i := 0; i < 100; i++ {
+		data := make([]byte, 10+rand.IntN(64*1024))
+		for j := range data {
+			data[j] = byte(rand.Uint32())
+		}
+		_, setting := ac.Compress(nil, data)
+		require.Equal(t, MinLZFastest, setting)
+	}
+}
+
+func TestAdaptiveCompressorCompressible(t *testing.T) {
+	// Test the adaptive compressor with compressible data and make sure it uses
+	// the slow algorithm.
+	ac := NewAdaptiveCompressor(AdaptiveCompressorParams{
+		Fast:            None,
+		Slow:            ZstdLevel1,
+		ReductionCutoff: 0.6,
+		SampleEvery:     10,
+		SampleHalfLife:  128 * 1024,
+		SamplingSeed:    1,
+	})
+	defer ac.Close()
+	for i := 0; i < 100; i++ {
+		data := make([]byte, 10+rand.IntN(64*1024))
+		for j := range data {
+			data[j] = byte(j / 100)
+		}
+		_, setting := ac.Compress(nil, data)
+		require.Equal(t, ZstdLevel1, setting)
+	}
+}
