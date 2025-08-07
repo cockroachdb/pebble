1f989aa2309abe7b9ec024d5a3a3d7ad8f9e57d3
Radu Berinde <radu@cockroachlabs.com>
2025-08-07 18:28:47 +0300

    tool: show compression stats in sstable properties

--- tool/sstable.go
+++ tool/sstable.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"maps"
 	"os"
 	"slices"
 	"strings"
@@ -318,15 +319,13 @@ func (s *sstableT) runProperties(cmd *cobra.Command, args []string) {
 		fmt.Fprintf(tw, "merger\t%s\n", formatNull(props.MergerName))
 		fmt.Fprintf(tw, "filter\t%s\n", formatNull(props.FilterPolicyName))
 		fmt.Fprintf(tw, "compression\t%s\n", props.CompressionName)
+		if props.CompressionStats != "" {
+			fmt.Fprintf(tw, "compression stats\t%s\n", props.CompressionStats)
+		}
 		fmt.Fprintf(tw, "user properties\t\n")
 		fmt.Fprintf(tw, "  collectors\t%s\n", props.PropertyCollectorNames)
 		// Read UserProperties directly from reader.
-		keys := make([]string, 0, len(r.UserProperties))
-		for key := range r.UserProperties {
-			keys = append(keys, key)
-		}
-		slices.Sort(keys)
-		for _, key := range keys {
+		for _, key := range slices.Sorted(maps.Keys(r.UserProperties)) {
 			fmt.Fprintf(tw, "  %s\t%s\n", key, r.UserProperties[key])
 		}
 		_ = tw.Flush()
