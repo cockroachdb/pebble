5e7485cfabc32a21051c5d76ce42db15d3092c0c
Radu Berinde <radu@cockroachlabs.com>
2025-08-25 09:14:46 -0700

    manifest: rework annotation implementation
    The current annotation implementation purports to be thread-safe but
    in practice it is not: we store atomics in a slice which can get
    copied over when it is resized.
    
    We replace the dynamic slice with a static array. This requires
    assigning each annotator a unique index.
    
    The current implementation has a `valid` flag in addition to the
    `atomic.Value`. Storing a non-valid value is never necessary, so
    instead of using an extra flag we simply don't update `atomic.Value`
    unless the value is valid.
    
    We also rework the APIs to be as simple as possible and with less
    boilerplate.

--- compaction_picker.go
+++ compaction_picker.go
@@ -1560,39 +1560,42 @@ func (p *compactionPickerByScore) addScoresToPickedCompactionMetrics(
 // the criteria, it chooses whichever file has the lowest LargestSeqNum. The
 // lowest LargestSeqNum file will be the first eligible for an elision-only
 // compaction once snapshots less than or equal to its LargestSeqNum are closed.
-var elisionOnlyAnnotator = manifest.NewTableAnnotator[manifest.TableMetadata](manifest.PickFileAggregator{
-	Filter: func(f *manifest.TableMetadata) (eligible bool, cacheOK bool) {
-		if f.IsCompacting() {
-			return false, true
-		}
+var elisionOnlyAnnotator = manifest.MakePickFileAnnotator(
+	manifest.NewTableAnnotationIdx(),
+	manifest.PickFileAnnotatorFuncs{
+		Filter: func(f *manifest.TableMetadata) (eligible bool, cacheOK bool) {
+			if f.IsCompacting() {
+				return false, true
+			}
 
-		backingProps, backingPropsValid := f.TableBacking.Properties()
-		stats, statsValid := f.Stats()
-		if !backingPropsValid || !statsValid {
-			return false, false
-		}
+			backingProps, backingPropsValid := f.TableBacking.Properties()
+			stats, statsValid := f.Stats()
+			if !backingPropsValid || !statsValid {
+				return false, false
+			}
 
-		// Bottommost files are large and not worthwhile to compact just
-		// to remove a few tombstones. Consider a file eligible only if
-		// either its own range deletions delete at least 10% of its data or
-		// its deletion tombstones make at least 10% of its entries.
-		//
-		// TODO(jackson): This does not account for duplicate user keys
-		// which may be collapsed. Ideally, we would have 'obsolete keys'
-		// statistics that would include tombstones, the keys that are
-		// dropped by tombstones and duplicated user keys. See #847.
-		//
-		// Note that tables that contain exclusively range keys (i.e. no point keys,
-		// `NumEntries` and `RangeDeletionsBytesEstimate` are both zero) are excluded
-		// from elision-only compactions.
-		// TODO(travers): Consider an alternative heuristic for elision of range-keys.
-		eligible = stats.RangeDeletionsBytesEstimate*10 >= f.Size || backingProps.NumDeletions*10 > backingProps.NumEntries
-		return eligible, true
+			// Bottommost files are large and not worthwhile to compact just
+			// to remove a few tombstones. Consider a file eligible only if
+			// either its own range deletions delete at least 10% of its data or
+			// its deletion tombstones make at least 10% of its entries.
+			//
+			// TODO(jackson): This does not account for duplicate user keys
+			// which may be collapsed. Ideally, we would have 'obsolete keys'
+			// statistics that would include tombstones, the keys that are
+			// dropped by tombstones and duplicated user keys. See #847.
+			//
+			// Note that tables that contain exclusively range keys (i.e. no point keys,
+			// `NumEntries` and `RangeDeletionsBytesEstimate` are both zero) are excluded
+			// from elision-only compactions.
+			// TODO(travers): Consider an alternative heuristic for elision of range-keys.
+			eligible = stats.RangeDeletionsBytesEstimate*10 >= f.Size || backingProps.NumDeletions*10 > backingProps.NumEntries
+			return eligible, true
+		},
+		Compare: func(f1 *manifest.TableMetadata, f2 *manifest.TableMetadata) bool {
+			return f1.LargestSeqNum < f2.LargestSeqNum
+		},
 	},
-	Compare: func(f1 *manifest.TableMetadata, f2 *manifest.TableMetadata) bool {
-		return f1.LargestSeqNum < f2.LargestSeqNum
-	},
-})
+)
 
 // pickedCompactionFromCandidateFile creates a pickedCompaction from a *fileMetadata
 // with various checks to ensure that the file still exists in the expected level
