2b278238c83095c9691fe6d8b7dd6775cea68515
Xin Hao Zhang <xzhang@cockroachlabs.com>
2025-09-04 20:59:45 -0400

    internal/manifest: create virtual_backings testdata dir

--- internal/manifest/virtual_backings_test.go
+++ internal/manifest/virtual_backings_test.go
@@ -13,53 +13,55 @@ import (
 )
 
 func TestVirtualBackings(t *testing.T) {
-	bv := MakeVirtualBackings()
-	datadriven.RunTest(t, "testdata/virtual_backings", func(t *testing.T, d *datadriven.TestData) (retVal string) {
-		var nInt, size, tInt uint64
-		d.MaybeScanArgs(t, "n", &nInt)
-		d.MaybeScanArgs(t, "size", &size)
-		d.MaybeScanArgs(t, "table", &tInt)
-		n := base.DiskFileNum(nInt)
-		tableNum := base.TableNum(tInt)
+	datadriven.Walk(t, "testdata/virtual_backings", func(t *testing.T, path string) {
+		bv := MakeVirtualBackings()
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) (retVal string) {
+			var nInt, size, tInt uint64
+			d.MaybeScanArgs(t, "n", &nInt)
+			d.MaybeScanArgs(t, "size", &size)
+			d.MaybeScanArgs(t, "table", &tInt)
+			n := base.DiskFileNum(nInt)
+			tableNum := base.TableNum(tInt)
 
-		defer func() {
-			if r := recover(); r != nil {
-				retVal = fmt.Sprint(r)
+			defer func() {
+				if r := recover(); r != nil {
+					retVal = fmt.Sprint(r)
+				}
+			}()
+
+			switch d.Cmd {
+			case "add":
+				bv.AddAndRef(&TableBacking{
+					DiskFileNum: n,
+					Size:        size,
+				})
+
+			case "remove":
+				bv.Remove(n)
+
+			case "add-table":
+				m := &TableMetadata{
+					TableNum:     tableNum,
+					TableBacking: &TableBacking{DiskFileNum: n},
+					Size:         size,
+					Virtual:      true,
+				}
+				bv.AddTable(m)
+
+			case "remove-table":
+				bv.RemoveTable(n, tableNum)
+
+			case "protect":
+				bv.Protect(n)
+
+			case "unprotect":
+				bv.Unprotect(n)
+
+			default:
+				d.Fatalf(t, "unknown command %q", d.Cmd)
 			}
-		}()
 
-		switch d.Cmd {
-		case "add":
-			bv.AddAndRef(&TableBacking{
-				DiskFileNum: n,
-				Size:        size,
-			})
-
-		case "remove":
-			bv.Remove(n)
-
-		case "add-table":
-			m := &TableMetadata{
-				TableNum:     tableNum,
-				TableBacking: &TableBacking{DiskFileNum: n},
-				Size:         size,
-				Virtual:      true,
-			}
-			bv.AddTable(m)
-
-		case "remove-table":
-			bv.RemoveTable(n, tableNum)
-
-		case "protect":
-			bv.Protect(n)
-
-		case "unprotect":
-			bv.Unprotect(n)
-
-		default:
-			d.Fatalf(t, "unknown command %q", d.Cmd)
-		}
-
-		return bv.String()
+			return bv.String()
+		})
 	})
 }
