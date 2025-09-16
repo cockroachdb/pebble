224223dd053dda438257b04d1fb4e1d13d1a1718
Radu Berinde <radu@cockroachlabs.com>
2025-09-16 07:57:18 -0700

    table: support horizontal dividers
    We add a `RenderOptions` field which allows customizing where the
    horizontal dividers are put.

--- internal/ascii/table/table_test.go
+++ internal/ascii/table/table_test.go
@@ -7,6 +7,7 @@ package table
 import (
 	"fmt"
 	"slices"
+	"strconv"
 	"testing"
 
 	"github.com/cockroachdb/datadriven"
@@ -42,6 +43,16 @@ func TestTable(t *testing.T) {
 		}
 		if td.HasArg("center") {
 			align = AlignCenter
+		}
+		if hd, ok := td.Arg("horizontal-dividers"); ok {
+			opts.HorizontalDividers = make(HorizontalDividers)
+			for _, v := range hd.Vals {
+				i, err := strconv.Atoi(v)
+				if err != nil {
+					t.Fatalf("invalid horizontal-divider %q: %s", v, err)
+				}
+				opts.HorizontalDividers[i] = struct{}{}
+			}
 		}
 		switch td.Cmd {
 		case "cats-autoincrement":
