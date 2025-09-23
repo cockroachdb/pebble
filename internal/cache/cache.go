af8a16ec841327ffc2b9cbcd7b7c427e7f54953d
Radu Berinde <radu@cockroachlabs.com>
2025-09-23 08:38:12 -0700

    metrics: more granular block cache metrics
    We improve the cache metrics in two ways:
     - we break down the misses by the type of block (for the most
       important block types)
     - we also show miss rates across the last 10 minutes and the last
       hour
    
    In follow-up work, I will attempt to plumb the LSM level as well.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -17,19 +17,8 @@ import (
 
 	"github.com/cockroachdb/pebble/internal/base"
 	"github.com/cockroachdb/pebble/internal/invariants"
+	"github.com/cockroachdb/pebble/internal/metricsutil"
 )
-
-// Metrics holds metrics for the cache.
-type Metrics struct {
-	// The number of bytes inuse by the cache.
-	Size int64
-	// The count of objects (blocks or tables) in the cache.
-	Count int64
-	// The number of cache hits.
-	Hits int64
-	// The number of cache misses.
-	Misses int64
-}
 
 // Cache implements Pebble's sharded block cache. The Clock-PRO algorithm is
 // used for page replacement
@@ -77,6 +66,8 @@ type Cache struct {
 	maxSize int64
 	idAlloc atomic.Uint64
 	shards  []shard
+
+	metricsWindow *metricsutil.Window[HitsAndMisses]
 
 	// Traces recorded by Cache.trace. Used for debugging.
 	tr struct {
@@ -136,6 +127,8 @@ func NewWithShards(size int64, shards int) *Cache {
 	for i := range c.shards {
 		c.shards[i].init(size / int64(len(c.shards)))
 	}
+	c.metricsWindow = metricsutil.NewWindow[HitsAndMisses](c.hitsAndMisses)
+	c.metricsWindow.Start()
 
 	// Note: this is a no-op if invariants are disabled or race is enabled.
 	invariants.SetFinalizer(c, func(c *Cache) {
@@ -171,10 +164,19 @@ func (c *Cache) Unref() {
 	case v < 0:
 		panic(fmt.Sprintf("pebble: inconsistent reference count: %d", v))
 	case v == 0:
-		for i := range c.shards {
-			c.shards[i].Free()
-		}
+		c.destroy()
 	}
+}
+
+func (c *Cache) destroy() {
+	if c.metricsWindow != nil {
+		c.metricsWindow.Stop()
+		c.metricsWindow = nil
+	}
+	for i := range c.shards {
+		c.shards[i].Free()
+	}
+	c.shards = nil
 }
 
 func (c *Cache) NewHandle() *Handle {
@@ -205,21 +207,6 @@ func (c *Cache) Reserve(n int) func() {
 		}
 		shardN = -1
 	}
-}
-
-// Metrics returns the metrics for the cache.
-func (c *Cache) Metrics() Metrics {
-	var m Metrics
-	for i := range c.shards {
-		s := &c.shards[i]
-		s.mu.RLock()
-		m.Count += int64(s.blocks.Len())
-		m.Size += s.sizeHot + s.sizeCold
-		s.mu.RUnlock()
-		m.Hits += s.hits.Load()
-		m.Misses += s.misses.Load()
-	}
-	return m
 }
 
 // MaxSize returns the max size of the cache.
@@ -262,16 +249,16 @@ func (c *Handle) Cache() *Cache {
 // Peek retrieves the cache value for the specified file and offset, returning
 // nil if no value is present. Peek does not affect the state of the cache (it
 // does not "count" as an access as far as the cache replacement is concerned).
-func (c *Handle) Peek(fileNum base.DiskFileNum, offset uint64) *Value {
+func (c *Handle) Peek(fileNum base.DiskFileNum, offset uint64, category Category) *Value {
 	k := makeKey(c.id, fileNum, offset)
-	return c.cache.getShard(k).get(k, true /* peekOnly */)
+	return c.cache.getShard(k).get(k, category, true /* peekOnly */)
 }
 
 // Get retrieves the cache value for the specified file and offset, returning
 // nil if no value is present.
-func (c *Handle) Get(fileNum base.DiskFileNum, offset uint64) *Value {
+func (c *Handle) Get(fileNum base.DiskFileNum, offset uint64, category Category) *Value {
 	k := makeKey(c.id, fileNum, offset)
-	return c.cache.getShard(k).get(k, false /* peekOnly */)
+	return c.cache.getShard(k).get(k, category, false /* peekOnly */)
 }
 
 // GetWithReadHandle retrieves the cache value for the specified handleID, fileNum
@@ -298,7 +285,7 @@ func (c *Handle) Get(fileNum base.DiskFileNum, offset uint64) *Value {
 // While waiting, someone else may successfully read the value, which results
 // in a valid Handle being returned. This is a case where cacheHit=false.
 func (c *Handle) GetWithReadHandle(
-	ctx context.Context, fileNum base.DiskFileNum, offset uint64,
+	ctx context.Context, fileNum base.DiskFileNum, offset uint64, category Category,
 ) (
 	cv *Value,
 	rh ReadHandle,
@@ -308,7 +295,7 @@ func (c *Handle) GetWithReadHandle(
 	err error,
 ) {
 	k := makeKey(c.id, fileNum, offset)
-	cv, re := c.cache.getShard(k).getWithReadEntry(k)
+	cv, re := c.cache.getShard(k).getWithReadEntry(k, category)
 	if cv != nil {
 		return cv, ReadHandle{}, 0, 0, true, nil
 	}
