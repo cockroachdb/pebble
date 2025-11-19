40b0f5ffd8735560b2d94f55a388349412dc66a0
kruskall <99559985+kruskall@users.noreply.github.com>
2025-11-19 20:43:57 +0100

    Update zstd_nocgo.go

--- internal/compression/zstd_nocgo.go
+++ internal/compression/zstd_nocgo.go
@@ -2,7 +2,7 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build !cgo || gozstd
+//go:build !cgo || pebblegozstd
 
 package compression
 
