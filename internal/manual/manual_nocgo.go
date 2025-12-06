9681a49f74ad099ced4fcd1284d30a200c70d812
Elliot Nunn <elliotnunn@me.com>
2025-12-06 18:28:21 +0800

    manual: use sync.Pool to help the GC when CGo is off

--- internal/manual/manual_nocgo.go
+++ internal/manual/manual_nocgo.go
@@ -7,6 +7,8 @@
 package manual
 
 import (
+	"math/bits"
+	"sync"
 	"unsafe"
 
 	"github.com/cockroachdb/pebble/internal/invariants"
@@ -21,9 +23,8 @@ func New(purpose Purpose, n uintptr) Buf {
 		return Buf{}
 	}
 	recordAlloc(purpose, n)
-	slice := make([]byte, n)
 	return Buf{
-		data: unsafe.Pointer(unsafe.SliceData(slice)),
+		data: pools[sizeClass(n)].Get().(unsafe.Pointer),
 		n:    n,
 	}
 }
@@ -32,5 +33,27 @@ func New(purpose Purpose, n uintptr) Buf {
 // returned by New.
 func Free(purpose Purpose, b Buf) {
 	invariants.MaybeMangle(b.Slice())
+	if b.data == nil {
+		return
+	}
 	recordFree(purpose, b.n)
+	pools[sizeClass(b.n)].Put(b.data)
+}
+
+var pools = mkPools() // pools[n] is for allocs of size 1 << n
+
+func mkPools() [bits.UintSize]sync.Pool {
+	var pools [bits.UintSize]sync.Pool
+	for i := range pools {
+		i := i // watch out for capture bugs
+		pools[i].New = func() any {
+			return unsafe.Pointer(unsafe.SliceData(make([]byte, 1<<i)))
+		}
+	}
+	return pools
+}
+
+// sizeClass determines the smallest n such that 1 << n >= size
+func sizeClass(size uintptr) int {
+	return bits.UintSize - bits.LeadingZeros(uint(size-1))
 }
