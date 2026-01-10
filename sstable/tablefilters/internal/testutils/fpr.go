8e8084792eb62f892d652812529f8872e8bd2954
Radu Berinde <radu@cockroachlabs.com>
2026-01-09 18:43:39 -0800

    binaryfuse: add simulation, tests and benchmarks
    Observations:
    
     - Binary fuse filters take longer to build than bloom filters for
       short keys, by a factor of 2-3x. This gap is reduced significantly
       for longer keys, thanks to the much faster XXH3.
    
     - Binary fuse filters take about the same time to query for small
       keys, and significantly faster for larger keys (again, thanks to
       XXH3).
    
     - The false positive rate is not even close, especially for higher
       bit filters. The default 10 bits-per-key bloom filter has about 1
       in 88 FPR, whereas an 8-bit binary fuse filter has ~9-9.5
       bits-per-key binary and FPR 1 in 256.
    
    In a TPCC profile, `bloom.FilterPolicy.MayContain` takes 0.2% and the
    building of the filter (after accumulating the hashes) takes ~0.1%.
    Accumulating the hashes themselves also takes ~0.1%. From these
    figures, the substitution should be about a wash and thus gain the
    full benefits of the lower FPRs.

---
+++ sstable/tablefilters/internal/testutils/fpr.go
@@ -0,0 +1,57 @@
+// Copyright 2026 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package testutils
+
+import (
+	"fmt"
+	"math"
+	"math/rand/v2"
+	"runtime"
+	"sync"
+
+	"github.com/cockroachdb/crlib/crhumanize"
+	"github.com/cockroachdb/pebble/internal/metricsutil"
+)
+
+func SimulateFPR(
+	numRuns, avgSize int, runExperiment func(size int) (fpr float64),
+) (mean, stddev float64) {
+	var wg sync.WaitGroup
+	ch := make(chan struct{}, numRuns)
+	for range numRuns {
+		ch <- struct{}{}
+	}
+	close(ch)
+
+	var fprMu sync.Mutex
+	var fpr metricsutil.Welford
+
+	for range runtime.GOMAXPROCS(0) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for range ch {
+				size := avgSize - avgSize/10 + rand.IntN(avgSize*2/10)
+				falsePositiveRate := runExperiment(size)
+				fprMu.Lock()
+				fpr.Add(falsePositiveRate)
+				fprMu.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+	return fpr.Mean(), fpr.StdDev()
+}
+
+// FormatFPR formats a false positive rate as a percentage with "1 in N" ratio.
+func FormatFPR(fpr float64) string {
+	l10 := min(3, -int(math.Floor(math.Log10(fpr))))
+	return fmt.Sprintf("%.*f%% (1 in %.*f)", l10, fpr*100, 3-l10, 1.0/fpr)
+}
+
+// FormatFPRWithStdDev formats a false positive rate mean and standard deviation.
+func FormatFPRWithStdDev(mean, stdDev float64) string {
+	return fmt.Sprintf("%s ± %s", FormatFPR(mean), crhumanize.Percent(stdDev, mean))
+}
