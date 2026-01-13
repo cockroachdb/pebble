a947cb13a195b3a08045c7282a5332f58ff0ff80
Radu Berinde <radu@cockroachlabs.com>
2026-01-12 19:49:16 -0800

    tablefilters: add large MayContain benchmark
    Add a benchmark that randomly queries from a large set of filters,
    (larger than L3 caches).

--- sstable/tablefilters/bloom/bloom_test.go
+++ sstable/tablefilters/bloom/bloom_test.go
@@ -284,3 +284,12 @@ func BenchmarkMayContain(b *testing.B) {
 		})
 	}
 }
+
+// Results on n2d-standard-8 (AMD EPYC Turin, go 1.25):
+// BenchmarkMayContainLarge/procs=1-8          100000000        109.0 ns/op
+// BenchmarkMayContainLarge/procs=2-8          100000000        115.8 ns/op
+// BenchmarkMayContainLarge/procs=4-8          100000000        138.3 ns/op
+// BenchmarkMayContainLarge/procs=8-8          100000000        193.6 ns/op
+func BenchmarkMayContainLarge(b *testing.B) {
+	filtertestutils.BenchmarkMayContainLarge(b, FilterPolicy(10), Decoder)
+}
