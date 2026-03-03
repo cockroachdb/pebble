75b281c5cc859c3329a4e90308a43c60a57175e1
Jille Timmermans <jille@hexon.nl>
2026-03-03 22:31:11 +0100

    rawmalloc: Use unsafe.Slice() instead of basing the max size on the platform
    unsafe.Slice() was added in go 1.20, and according to go.mod we're using
    go 1.25.
    
    This should fix golangci-lint's complaint:
    github.com/cockroachdb/pebble/v2@v2.1.3/internal/rawalloc/rawalloc.go:23:12: invalid array length maxArrayLen (untyped int constant 1125899906842623) (typecheck)
    	return (*[maxArrayLen]byte)(ptr)[:len:cap]

--- internal/rawalloc/rawalloc.go
+++ internal/rawalloc/rawalloc.go
@@ -14,11 +14,13 @@
 
 package rawalloc
 
+import "unsafe"
+
 // New returns a new byte slice of the specified length and capacity where the
 // backing memory is uninitialized. This differs from make([]byte) which
 // guarantees that the backing memory for the slice is initialized to zero. Use
 // carefully.
 func New(len, cap int) []byte {
 	ptr := mallocgc(uintptr(cap), nil, false)
-	return (*[maxArrayLen]byte)(ptr)[:len:cap]
+	return unsafe.Slice((*byte)(ptr), cap)[:len]
 }
