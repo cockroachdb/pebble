87a7fe239a5254788eef76bea68c482732dbe353
Radu Berinde <radu@cockroachlabs.com>
2026-04-09 14:53:34 -0700

    iterv2: introduce a new iterator stack (off by default)
    We introduce a new iterator abstraction along with a new merging
    iterator, level iterator, interleaving iterator stack. This
    abstraction allows presenting the next span boundary at any point,
    which allows the merging iterator to (conceptually) divide the key
    space into "slabs". The slab approach will make the addition of new
    rangedel-type keys much easier. The boundary also naturally allows
    "stopping points" in the level iterator without a special contract
    with the merging iterator.
    
    The new stack can be used by setting `iterv2.Enabled`. The stack
    functions correctly (it passed a few hours of the metamorphic test).
    It is still a prototype in that it does not yet have many of the
    optimizations the existing stack has (like try-seek-using-next). The
    integration of the v2 stack also needs some optimization in terms of
    allocations.
    
    Most of the code inside the iterators was written by me. Claude helped
    with comments and with limited bits and pieces but it was generating
    much more complicated code for the core logic. In all fairness, I also
    had to go back and rewrite most of it a few times.
    
    Suggestion on what to review:
     - `iterv2.Iter` semantics;
     - `iterv2.TestIter` implementation. While this is only used for
       testing, it serves as a formal codification of the semantics (with
       a fairly simple implementation). All the new iterators are tested
       against the `TestIter`, so it's less important to review the
       actual iterator implementations;
     - `iterv2.InterleavingIter` struct and `Init()` comments;
     - `levelIterV2` struct comment;
     - `mergingIterV2` struct comment;
     - `slabState` interface (`BuildForward`).

---
+++ internal/iterv2/interleaving_iter_rand_test.go
@@ -0,0 +1,115 @@
+// Copyright 2026 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package iterv2
+
+import (
+	"math/rand/v2"
+	"testing"
+
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/invalidating"
+	"github.com/cockroachdb/pebble/internal/keyspan"
+	"github.com/cockroachdb/pebble/internal/testkeys"
+)
+
+func TestInterleavingIterRandom(t *testing.T) {
+	// To reproduce a failure:
+	// runRandomTest(t, <seed>)
+	for range 200 {
+		runRandomTest(t, rand.Uint64())
+	}
+}
+
+func runRandomTest(t *testing.T, seed uint64) {
+	const numOps = 500
+
+	rng := rand.New(rand.NewPCG(seed, seed))
+
+	cmp := testkeys.Comparer
+
+	cfg := RandKeyConfig(rng)
+	points := RandPointKeys(rng, cfg, 50)
+	spans := RandSpans(rng, cfg, 10)
+
+	var startKey, endKey []byte
+	switch rng.IntN(3) {
+	case 0:
+		// Sometimes nil.
+	case 1:
+		// Sometimes a loose bound.
+		startKey = []byte("a")
+	case 2:
+		// Sometimes a tight bound.
+		if len(points) > 0 {
+			startKey = points[0].K.UserKey
+		}
+		if len(spans) > 0 {
+			if startKey == nil || cmp.Compare(startKey, spans[0].Start) > 0 {
+				startKey = spans[0].Start
+			}
+		}
+	}
+	switch rng.IntN(3) {
+	case 0:
+		// Sometimes nil.
+	case 1:
+		// Sometimes a loose bound.
+		endKey = []byte("z~")
+	case 2:
+		// Sometimes a tight bound.
+		if len(points) > 0 {
+			endKey = cmp.ImmediateSuccessor(nil, cmp.Split.Prefix(points[len(points)-1].K.UserKey))
+		}
+		if len(spans) > 0 {
+			if endKey == nil || cmp.Compare(endKey, spans[len(spans)-1].End) < 0 {
+				endKey = spans[len(spans)-1].End
+			}
+		}
+	}
+
+	lower, upper := RandBounds(rng, cfg, startKey, endKey)
+
+	var pointIter base.InternalIterator
+	pointIter = base.NewFakeIterWithCmp(cmp.Compare, points)
+	if lower != nil || upper != nil {
+		pointIter.SetBounds(lower, upper)
+	}
+	if rand.IntN(2) == 0 {
+		pointIter = invalidating.NewIter(pointIter)
+	}
+
+	var spanIter keyspan.FragmentIterator
+	if len(spans) == 0 && rng.IntN(2) == 0 {
+		// Use nil span iterator.
+	} else {
+		spanIter = keyspan.NewIter(cmp.Compare, spans)
+		if rand.IntN(2) == 0 {
+			spanIter = keyspan.NewInvalidatingIter(spanIter)
+		}
+	}
+	var interleavingIter InterleavingIter
+	interleavingIter.Init(
+		cmp,
+		pointIter,
+		spanIter,
+		startKey,
+		endKey,
+		lower,
+		upper,
+	)
+
+	defer func() {
+		if t.Failed() {
+			t.Logf("seed: %d", seed)
+			t.Logf("cfg: %+v", cfg)
+			t.Logf("start, end: %q %q", startKey, endKey)
+			t.Logf("lower, upper: %q %q", lower, upper)
+			t.Logf("points: %v", points)
+			t.Logf("spans: %v", spans)
+		}
+	}()
+
+	CheckIter(t, rng, cmp, cfg, AllTestOps, points, spans, &interleavingIter, startKey, endKey, lower, upper, numOps)
+}
